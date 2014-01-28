cba638dc389038837aa5521e8d841c644212349c
Etienne Rossignon <etienne.rossignon@gadz.org>
2014-01-28 13:21:20 +0100

    improve encode and decode

--- test/test_encode_decode.js
+++ test/test_encode_decode.js
@@ -2,192 +2,157 @@ var should = require("should");
 var ec = require("../lib/encode_decode");
 var opcua = require("../lib/nodeopcua");
 
+function test_encode_decode(obj,encode_func,decode_func,expectedLength,verify_buffer_func)
+{
+    var binaryStream = new opcua.BinaryStream();
+    binaryStream.length.should.equal(0);
+
+    encode_func(obj,binaryStream);
+    binaryStream.length.should.equal(expectedLength);
+
+    if (verify_buffer_func) {
+        verify_buffer_func(binaryStream._buffer);
+    }
+    binaryStream.rewind();
+
+    var obj_verif = decode_func(binaryStream);
+    binaryStream.length.should.equal(expectedLength);
+
+    if (obj !== undefined) {
+        obj_verif.should.eql(obj);
+
+    } else {
+        should.not.exists(obj_verif);
+    }
+}
 
 describe("testing built-in type encoding",function() {
 
 
     it("should encode and decode a boolean as a single byte",function(){
 
-        var binaryStream = new opcua.BinaryStream();
-        binaryStream.length.should.equal(0);
-
-        ec.encodeBoolean(true,binaryStream);
-        binaryStream.length.should.equal(1);
-
-        binaryStream.rewind();
-        var boolValue =ec.decodeBoolean(binaryStream);
-        boolValue.should.equal(true);
-
+        test_encode_decode(true ,ec.encodeBoolean,ec.decodeBoolean,1);
+        test_encode_decode(false,ec.encodeBoolean,ec.decodeBoolean,1);
 
     });
 
     it("should encode and decode a Integer (4 bytes)",function(){
 
-        var binaryStream = new opcua.BinaryStream();
-        binaryStream.length.should.equal(0);
 
-        ec.encodeInt32(1000000000,binaryStream);
-        binaryStream.length.should.equal(4);
+        test_encode_decode(1000000000 ,ec.encodeInt32,ec.decodeInt32,4,function (buffer) {
+            // should be little endian
+            buffer.readUInt8(0).should.equal(0x00);
+            buffer.readUInt8(1).should.equal(0xCA);
+            buffer.readUInt8(2).should.equal(0x9A);
+            buffer.readUInt8(3).should.equal(0x3B);
+        });
+        test_encode_decode(-100000000 ,ec.encodeInt32,ec.decodeInt32,4);
 
-        // should be little endian
-        //xx console.log(binaryStream._buffer.slice(0,4));//toJSON());
-        binaryStream._buffer.readUInt8(0).should.equal(0x00);
-        binaryStream._buffer.readUInt8(1).should.equal(0xCA);
-        binaryStream._buffer.readUInt8(2).should.equal(0x9A);
-        binaryStream._buffer.readUInt8(3).should.equal(0x3B);
-
-        binaryStream.rewind();
-        var check_value =ec.decodeInt32(binaryStream);
-        check_value.should.equal(1000000000);
     });
 
     it("should encode and decode a Floating Point (4 bytes)",function(){
-        // I EEE-754
-        var binaryStream = new opcua.BinaryStream();
-        binaryStream.length.should.equal(0);
 
         var value = -6.5;
-        ec.encodeFloat(value,binaryStream);
+        // I EEE-754
+        test_encode_decode(value ,ec.encodeFloat,ec.decodeFloat,4,function (buffer) {
+            // should be little endian
+            buffer.readUInt8(0).should.equal(0x00);
+            buffer.readUInt8(1).should.equal(0x00);
+            buffer.readUInt8(2).should.equal(0xD0);
+            buffer.readUInt8(3).should.equal(0xC0);
+        });
 
-        binaryStream.length.should.equal(4);
-        // should be little endian
-        binaryStream._buffer.readUInt8(0).should.equal(0x00);
-        binaryStream._buffer.readUInt8(1).should.equal(0x00);
-        binaryStream._buffer.readUInt8(2).should.equal(0xD0);
-        binaryStream._buffer.readUInt8(3).should.equal(0xC0);
-
-        binaryStream.rewind();
-        var check_value =ec.decodeFloat(binaryStream);
-        check_value.should.equal(value);
     });
 
     it("should encode and decode a Double Point (8 bytes)",function(){
         // I EEE-754
-        var binaryStream = new opcua.BinaryStream();
-        binaryStream.length.should.equal(0);
 
         var value = -6.5;
-        ec.encodeDouble(value,binaryStream);
+        // I EEE-754
+        test_encode_decode(value ,ec.encodeDouble,ec.decodeDouble,8,function (buffer) {
+            // should be little endian
+            buffer.readUInt8(0).should.equal(0x00);
+            buffer.readUInt8(1).should.equal(0x00);
+            buffer.readUInt8(2).should.equal(0x00);
+            buffer.readUInt8(3).should.equal(0x00);
+            buffer.readUInt8(4).should.equal(0x00);
+            buffer.readUInt8(5).should.equal(0x00);
+            buffer.readUInt8(6).should.equal(0x1a);
+            buffer.readUInt8(7).should.equal(0xc0);
+        });
 
-        binaryStream.length.should.equal(8);
-        // should be little endian
-        //xx console.log(binaryStream._buffer.slice(0,8));//toJSON());
-        binaryStream._buffer.readUInt8(0).should.equal(0x00);
-        binaryStream._buffer.readUInt8(1).should.equal(0x00);
-        binaryStream._buffer.readUInt8(2).should.equal(0x00);
-        binaryStream._buffer.readUInt8(3).should.equal(0x00);
-        binaryStream._buffer.readUInt8(4).should.equal(0x00);
-        binaryStream._buffer.readUInt8(5).should.equal(0x00);
-        binaryStream._buffer.readUInt8(6).should.equal(0x1a);
-        binaryStream._buffer.readUInt8(7).should.equal(0xc0);
-
-        binaryStream.rewind();
-        var check_value =ec.decodeDouble(binaryStream);
-        check_value.should.equal(value);
     });
 
     it("should encode and decode a null string" ,function() {
 
-        var binaryStream = new opcua.BinaryStream();
-        binaryStream.length.should.equal(0);
-
         var value = undefined;
-        ec.encodeUAString(value,binaryStream);
 
-        binaryStream.length.should.equal(4);
 
-        binaryStream._buffer.readUInt8(0).should.equal(0xff);
-        binaryStream._buffer.readUInt8(1).should.equal(0xff);
-        binaryStream._buffer.readUInt8(2).should.equal(0xff);
-        binaryStream._buffer.readUInt8(3).should.equal(0xff);
-
-        binaryStream.rewind();
-        var check_value =ec.decodeUAString(binaryStream);
-        should.not.exists(check_value);
+        test_encode_decode(value ,ec.encodeUAString,ec.decodeUAString,4,function (buffer) {
+            // should be little endian
+            buffer.readUInt8(0).should.equal(0xff);
+            buffer.readUInt8(1).should.equal(0xff);
+            buffer.readUInt8(2).should.equal(0xff);
+            buffer.readUInt8(3).should.equal(0xff);
+        });
 
     });
 
     it("should encode and decode a normal string" ,function() {
 
-        var binaryStream = new opcua.BinaryStream();
-        binaryStream.length.should.equal(0);
+        var value = "Hello";
 
-        var value ="Hello";
-        ec.encodeUAString(value,binaryStream);
-
-        binaryStream.length.should.equal(4 + 5);
-        // should be little endian
-        binaryStream._buffer.readUInt8(0).should.equal(0x05);
-        binaryStream._buffer.readUInt8(1).should.equal(0x00);
-        binaryStream._buffer.readUInt8(2).should.equal(0x00);
-        binaryStream._buffer.readUInt8(3).should.equal(0x00);
-        binaryStream._buffer.readUInt8(4).should.equal('H'.charCodeAt(0));
-        binaryStream._buffer.readUInt8(5).should.equal('e'.charCodeAt(0));
-        binaryStream._buffer.readUInt8(6).should.equal('l'.charCodeAt(0));
-        binaryStream._buffer.readUInt8(7).should.equal('l'.charCodeAt(0));
-        binaryStream._buffer.readUInt8(8).should.equal('o'.charCodeAt(0));
-
-        binaryStream.rewind();
-        var check_value =ec.decodeUAString(binaryStream);
-        check_value.should.equal(value);
+        test_encode_decode(value ,ec.encodeUAString,ec.decodeUAString,9,function (buffer) {
+            // should be little endian
+            buffer.readUInt8(0).should.equal(0x05);
+            buffer.readUInt8(1).should.equal(0x00);
+            buffer.readUInt8(2).should.equal(0x00);
+            buffer.readUInt8(3).should.equal(0x00);
+            buffer.readUInt8(4).should.equal('H'.charCodeAt(0));
+            buffer.readUInt8(5).should.equal('e'.charCodeAt(0));
+            buffer.readUInt8(6).should.equal('l'.charCodeAt(0));
+            buffer.readUInt8(7).should.equal('l'.charCodeAt(0));
+            buffer.readUInt8(8).should.equal('o'.charCodeAt(0));
+        });
 
     });
 
     it("should encode and decode a DateTime" ,function() {
 
-        var binaryStream = new opcua.BinaryStream();
-        binaryStream.length.should.equal(0);
-
         var value = new Date();
-        ec.encodeDateTime(value,binaryStream);
 
-        binaryStream.length.should.equal(8);
-
-        binaryStream.rewind();
-        var checked_value = ec.decodeDateTime(binaryStream);
-        checked_value.should.eql(value);
+        test_encode_decode(value ,ec.encodeDateTime,ec.decodeDateTime,8,function (buffer) {
+            // todo
+        });
 
     });
 
     it("should encode and decode a GUID" ,function() {
 
-        var binaryStream = new opcua.BinaryStream();
-        binaryStream.length.should.equal(0);
-
         var value = "72962B91-FA75-4AE6-8D28-B404DC7DAF63";
-        ec.encodeGUID(value,binaryStream);
 
-        binaryStream.length.should.equal(16);
+        test_encode_decode(value ,ec.encodeGUID,ec.decodeGUID,16,function (buffer) {
+            buffer.readUInt8(0).should.equal(0x91);
+            buffer.readUInt8(1).should.equal(0x2B);
+            buffer.readUInt8(2).should.equal(0x96);
+            buffer.readUInt8(3).should.equal(0x72);
 
+            buffer.readUInt8(4).should.equal(0x75);
+            buffer.readUInt8(5).should.equal(0xFA);
 
-        binaryStream.rewind();
-        // should be little endian
-        binaryStream.readUInt8().should.equal(0x91);
-        binaryStream.readUInt8().should.equal(0x2B);
-        binaryStream.readUInt8().should.equal(0x96);
-        binaryStream.readUInt8().should.equal(0x72);
+            buffer.readUInt8(6).should.equal(0xE6);
+            buffer.readUInt8(7).should.equal(0x4A);
 
-        binaryStream.readUInt8().should.equal(0x75);
-        binaryStream.readUInt8().should.equal(0xFA);
+            buffer.readUInt8(8).should.equal(0x8D);
+            buffer.readUInt8(9).should.equal(0x28);
 
-        binaryStream.readUInt8().should.equal(0xE6);
-        binaryStream.readUInt8().should.equal(0x4A);
-
-        binaryStream.readUInt8().should.equal(0x8D);
-        binaryStream.readUInt8().should.equal(0x28);
-
-        binaryStream.readUInt8().should.equal(0xB4);
-        binaryStream.readUInt8().should.equal(0x04);
-        binaryStream.readUInt8().should.equal(0xDC);
-        binaryStream.readUInt8().should.equal(0x7D);
-        binaryStream.readUInt8().should.equal(0xAF);
-        binaryStream.readUInt8().should.equal(0x63);
-
-        binaryStream.rewind();
-        var checked_value = ec.decodeGUID(binaryStream);
-        checked_value.should.eql(value);
-
-
+            buffer.readUInt8(10).should.equal(0xB4);
+            buffer.readUInt8(11).should.equal(0x04);
+            buffer.readUInt8(12).should.equal(0xDC);
+            buffer.readUInt8(13).should.equal(0x7D);
+            buffer.readUInt8(14).should.equal(0xAF);
+            buffer.readUInt8(15).should.equal(0x63);
+        });
     });
 
 
@@ -196,106 +161,101 @@ describe("testing built-in type encoding",function() {
         var buf = new Buffer(256);
         buf.write("THIS IS MY BUFFER");
 
-        var binaryStream = new opcua.BinaryStream();
-        binaryStream.length.should.equal(0);
+        test_encode_decode(buf,ec.encodeByteString,ec.decodeByteString,256+4,function (buffer) {
 
-        ec.encodeByteString(buf,binaryStream);
-        binaryStream.length.should.equal(256+4);
-
-        binaryStream.rewind();
-        var check_buf = ec.decodeByteString(binaryStream);
-        check_buf.length.should.equal(buf.length);
-        check_buf.toString('hex').should.equal(buf.toString('hex'));
+            buffer.readUInt32LE(0).should.equal(256);
+        });
+        //xx check_buf.toString('hex').should.equal(buf.toString('hex'));
 
 
     });
 
     it("should encode and decode a two byte NodeId" ,function() {
-        // standard binary encoding
-        var binaryStream = new opcua.BinaryStream();
-        binaryStream.length.should.equal(0);
 
         var nodeId = ec.makeNodeId(25);
-        ec.encodeNodeId(nodeId,binaryStream);
+        nodeId.identifierType.should.eql(ec.NodeIdType.NUMERIC);
 
-        binaryStream.length.should.equal(2);
-        binaryStream._buffer.readUInt8(0).should.equal(0);
-        binaryStream._buffer.readUInt8(1).should.equal(25);
-
-        binaryStream.rewind();
-
-        var check_nodeId = ec.decodeNodeId(binaryStream);
-        binaryStream.length.should.equal(2);
-
-        check_nodeId.should.eql(nodeId);
+        test_encode_decode(
+            nodeId,
+            ec.encodeNodeId,
+            ec.decodeNodeId,
+            2,
+            function verify_buffer(buffer){
+                buffer.readUInt8(0).should.equal(0);
+                buffer.readUInt8(1).should.equal(25); // namespace
+            }
+        );
 
     });
 
 
     it("should encode and decode a four byte NodeId" ,function() {
-        var binaryStream = new opcua.BinaryStream();
-        binaryStream.length.should.equal(0);
 
         var nodeId = ec.makeNodeId(258);
-
         nodeId.identifierType.should.eql(ec.NodeIdType.NUMERIC);
-
-        ec.encodeNodeId(nodeId,binaryStream);
-
-        binaryStream.length.should.equal(4);
-        binaryStream._buffer.readUInt8(0).should.equal(1);
-        binaryStream._buffer.readUInt8(1).should.equal(0); // namespace
-        binaryStream._buffer.readUInt16LE(2).should.equal(258);
-
-
-        binaryStream.rewind();
-
-        var check_nodeId = ec.decodeNodeId(binaryStream);
-        binaryStream.length.should.equal(4);
-
-        check_nodeId.should.eql(nodeId);
-
+        test_encode_decode(
+            nodeId,
+            ec.encodeNodeId,
+            ec.decodeNodeId,
+            4,
+            function verify_buffer(buffer){
+                buffer.readUInt8(0).should.equal(1);
+                buffer.readUInt8(1).should.equal(0); // namespace
+                buffer.readUInt16LE(2).should.equal(258);
+            }
+        );
     });
 
-    it("should encode a Numeric NodeId" ,function() {
-    });
-    it("should encode a long NodeId" ,function() {
+    it("should encode and decode a Numeric NodeId" ,function() {
 
-
-        // standard binary encoding
-        var binaryStream = new opcua.BinaryStream();
-        binaryStream.length.should.equal(0);
-
-        var value = new ec.makeNodeId("SomeStuff",2500);
-
-        value.identifierType.should.eql(ec.NodeIdType.STRING);
-
-        ec.encodeNodeId(value,binaryStream);
-
-        binaryStream._buffer.readUInt8(0).should.equal(3);
-
-    });
-
-    it("should encode a Guid NodeId" ,function() {
-    });
-
-    it("should encode a Opaque NodeId" ,function() {
+        var nodeId = ec.makeNodeId(545889,2500);
+        nodeId.identifierType.should.eql(ec.NodeIdType.NUMERIC);
+        test_encode_decode(
+            nodeId,
+            ec.encodeNodeId,
+            ec.decodeNodeId,
+            7);
     });
 
 
-    it("should encode a Expanded NodeId" ,function() {
+    it("should encode and decode a String NodeId" ,function() {
 
-        var binaryStream = new opcua.BinaryStream();
-        binaryStream.length.should.equal(0);
+        var nodeId = ec.makeNodeId("SomeStuff",2500);
+        nodeId.identifierType.should.eql(ec.NodeIdType.STRING);
 
-        var e1 = ec.makeExpandedNodeId(10);
-        ec.encodeExpandedNodeId(e1,binaryStream);
+        test_encode_decode(
+            nodeId,
+            ec.encodeNodeId,
+            ec.decodeNodeId,
+            4 + 9 + 2 + 1);
 
-        binaryStream.rewind();
-        var e1_verif = ec.decodeExpandedNodeId(binaryStream);
+    });
 
-        e1_verif.should.eql(e1);
+    xit("should encode and decode a Guid NodeId" ,function() {
+        // todo
+    });
 
+    xit("should encode and decode a Opaque NodeId" ,function() {
+        // todo
+    });
+
+    it("should encode and decode a Expanded NodeId  - TwoBytes" ,function() {
+
+        test_encode_decode(
+            ec.makeExpandedNodeId(10),
+            ec.encodeExpandedNodeId,
+            ec.decodeExpandedNodeId,
+            2)
+        ;
+    });
+    it("should encode and decode a Expanded NodeId  - FourBytes" ,function() {
+
+        test_encode_decode(
+            ec.makeExpandedNodeId(32000),
+            ec.encodeExpandedNodeId,
+            ec.decodeExpandedNodeId,
+            4)
+        ;
     });
 
 
