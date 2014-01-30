9c6240800f5d84afc6262b07dab626bcaed99009
Etienne Rossignon <etienne.rossignon@gadz.org>
2014-01-30 08:59:44 +0100

    improve interceptor

--- bin/opcua_interceptor.js
+++ bin/opcua_interceptor.js
@@ -4,14 +4,13 @@ var hexy = require("hexy");
 var opcua = require("../lib/nodeopcua");
 var MessageBuilder = require("../lib/secure_channel_service").MessageBuilder;
 var packet_analyzer = require("../lib/packet_analyzer").packet_analyzer;
-
+var messageHeaderToString = require("../lib/secure_channel_service").messageHeaderToString;
 require("colors");
 
 var remote_port = 4841;
 hostname = "localhost";
 
 var my_port = remote_port + 1;
-
 
 
 var TrafficAnalyser = function(id)
@@ -32,7 +31,7 @@ TrafficAnalyser.prototype.add = function(data)
     var messageBuild = new MessageBuilder();
     messageBuild.on("raw_buffer",function(fullMessage){
 
-        console.log(hexy.hexy(fullMessage));
+        console.log(hexy.hexy(fullMessage , { width: 32}));
 
         packet_analyzer(fullMessage);
 
@@ -41,23 +40,29 @@ TrafficAnalyser.prototype.add = function(data)
 
 
 
-    if (this.id%2) {
-        console.log( JSON.stringify(messageHeader,null,"").red.bold);
-    } else {
-        console.log( JSON.stringify(messageHeader,null,"").yellow.bold);
-    }
+
 
     switch(messageHeader.msgType) {
 
         case "HEL":
-            break;
         case "ACK":
+            if (this.id%2) {
+                console.log( JSON.stringify(messageHeader,null,"").red.bold);
+            } else {
+                console.log( JSON.stringify(messageHeader,null,"").yellow.bold);
+            }
             break;
 
         case "OPN": // open secure channel
         case "CLO": // close secure channel
         case "MSG": // message
             // decode secure message
+            if (this.id%2) {
+                console.log( messageHeaderToString(data).red.bold);
+            } else {
+                console.log( messageHeaderToString(data).yellow.bold);
+            }
+
             messageBuild.feed(data);
             break;
         case "ERR":
