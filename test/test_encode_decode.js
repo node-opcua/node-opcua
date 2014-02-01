e6a7fd99709fa4e355faac210620ebf9fd4104fc
Etienne Rossignon <etienne.rossignon@gadz.org>
2014-02-01 13:57:23 +0100

    fix UtcTime encoding

--- test/test_encode_decode.js
+++ test/test_encode_decode.js
@@ -2,12 +2,11 @@ var should = require("should");
 var ec = require("../lib/encode_decode");
 var opcua = require("../lib/nodeopcua");
 
-function test_encode_decode(obj,encode_func,decode_func,expectedLength,verify_buffer_func)
-{
+function test_encode_decode(obj, encode_func, decode_func, expectedLength, verify_buffer_func) {
     var binaryStream = new opcua.BinaryStream();
     binaryStream.length.should.equal(0);
 
-    encode_func(obj,binaryStream);
+    encode_func(obj, binaryStream);
     binaryStream.length.should.equal(expectedLength);
 
     if (verify_buffer_func) {
@@ -26,35 +25,35 @@ function test_encode_decode(obj,encode_func,decode_func,expectedLength,verify_bu
     }
 }
 
-describe("testing built-in type encoding",function() {
+describe("testing built-in type encoding", function () {
 
 
-    it("should encode and decode a boolean as a single byte",function(){
+    it("should encode and decode a boolean as a single byte", function () {
 
-        test_encode_decode(true ,ec.encodeBoolean,ec.decodeBoolean,1);
-        test_encode_decode(false,ec.encodeBoolean,ec.decodeBoolean,1);
+        test_encode_decode(true, ec.encodeBoolean, ec.decodeBoolean, 1);
+        test_encode_decode(false, ec.encodeBoolean, ec.decodeBoolean, 1);
 
     });
 
-    it("should encode and decode a Integer (4 bytes)",function(){
+    it("should encode and decode a Integer (4 bytes)", function () {
 
 
-        test_encode_decode(1000000000 ,ec.encodeInt32,ec.decodeInt32,4,function (buffer) {
+        test_encode_decode(1000000000, ec.encodeInt32, ec.decodeInt32, 4, function (buffer) {
             // should be little endian
             buffer.readUInt8(0).should.equal(0x00);
             buffer.readUInt8(1).should.equal(0xCA);
             buffer.readUInt8(2).should.equal(0x9A);
             buffer.readUInt8(3).should.equal(0x3B);
         });
-        test_encode_decode(-100000000 ,ec.encodeInt32,ec.decodeInt32,4);
+        test_encode_decode(-100000000, ec.encodeInt32, ec.decodeInt32, 4);
 
     });
 
-    it("should encode and decode a Floating Point (4 bytes)",function(){
+    it("should encode and decode a Floating Point (4 bytes)", function () {
 
         var value = -6.5;
         // I EEE-754
-        test_encode_decode(value ,ec.encodeFloat,ec.decodeFloat,4,function (buffer) {
+        test_encode_decode(value, ec.encodeFloat, ec.decodeFloat, 4, function (buffer) {
             // should be little endian
             buffer.readUInt8(0).should.equal(0x00);
             buffer.readUInt8(1).should.equal(0x00);
@@ -64,12 +63,12 @@ describe("testing built-in type encoding",function() {
 
     });
 
-    it("should encode and decode a Double Point (8 bytes)",function(){
+    it("should encode and decode a Double Point (8 bytes)", function () {
         // I EEE-754
 
         var value = -6.5;
         // I EEE-754
-        test_encode_decode(value ,ec.encodeDouble,ec.decodeDouble,8,function (buffer) {
+        test_encode_decode(value, ec.encodeDouble, ec.decodeDouble, 8, function (buffer) {
             // should be little endian
             buffer.readUInt8(0).should.equal(0x00);
             buffer.readUInt8(1).should.equal(0x00);
@@ -83,12 +82,12 @@ describe("testing built-in type encoding",function() {
 
     });
 
-    it("should encode and decode a null string" ,function() {
+    it("should encode and decode a null string", function () {
 
         var value = undefined;
 
 
-        test_encode_decode(value ,ec.encodeUAString,ec.decodeUAString,4,function (buffer) {
+        test_encode_decode(value, ec.encodeUAString, ec.decodeUAString, 4, function (buffer) {
             // should be little endian
             buffer.readUInt8(0).should.equal(0xff);
             buffer.readUInt8(1).should.equal(0xff);
@@ -98,11 +97,11 @@ describe("testing built-in type encoding",function() {
 
     });
 
-    it("should encode and decode a normal string" ,function() {
+    it("should encode and decode a normal string", function () {
 
         var value = "Hello";
 
-        test_encode_decode(value ,ec.encodeUAString,ec.decodeUAString,9,function (buffer) {
+        test_encode_decode(value, ec.encodeUAString, ec.decodeUAString, 9, function (buffer) {
             // should be little endian
             buffer.readUInt8(0).should.equal(0x05);
             buffer.readUInt8(1).should.equal(0x00);
@@ -117,21 +116,21 @@ describe("testing built-in type encoding",function() {
 
     });
 
-    it("should encode and decode a DateTime" ,function() {
+    it("should encode and decode a DateTime", function () {
 
-        var value = new Date();
-
-        test_encode_decode(value ,ec.encodeDateTime,ec.decodeDateTime,8,function (buffer) {
+        var value = new Date(2014,0,2,15,0);
+        test_encode_decode(value, ec.encodeDateTime, ec.decodeDateTime, 8, function (buffer) {
             // todo
         });
 
     });
 
-    it("should encode and decode a GUID" ,function() {
+
+    it("should encode and decode a GUID", function () {
 
         var value = "72962B91-FA75-4AE6-8D28-B404DC7DAF63";
 
-        test_encode_decode(value ,ec.encodeGUID,ec.decodeGUID,16,function (buffer) {
+        test_encode_decode(value, ec.encodeGUID, ec.decodeGUID, 16, function (buffer) {
             buffer.readUInt8(0).should.equal(0x91);
             buffer.readUInt8(1).should.equal(0x2B);
             buffer.readUInt8(2).should.equal(0x96);
@@ -156,12 +155,12 @@ describe("testing built-in type encoding",function() {
     });
 
 
-    it("should encode and decode a ByteString" ,function() {
+    it("should encode and decode a ByteString", function () {
 
         var buf = new Buffer(256);
         buf.write("THIS IS MY BUFFER");
 
-        test_encode_decode(buf,ec.encodeByteString,ec.decodeByteString,256+4,function (buffer) {
+        test_encode_decode(buf, ec.encodeByteString, ec.decodeByteString, 256 + 4, function (buffer) {
 
             buffer.readUInt32LE(0).should.equal(256);
         });
@@ -170,7 +169,7 @@ describe("testing built-in type encoding",function() {
 
     });
 
-    it("should encode and decode a two byte NodeId" ,function() {
+    it("should encode and decode a two byte NodeId", function () {
 
         var nodeId = ec.makeNodeId(25);
         nodeId.identifierType.should.eql(ec.NodeIdType.NUMERIC);
@@ -180,7 +179,7 @@ describe("testing built-in type encoding",function() {
             ec.encodeNodeId,
             ec.decodeNodeId,
             2,
-            function verify_buffer(buffer){
+            function verify_buffer(buffer) {
                 buffer.readUInt8(0).should.equal(0);
                 buffer.readUInt8(1).should.equal(25); // namespace
             }
@@ -189,7 +188,7 @@ describe("testing built-in type encoding",function() {
     });
 
 
-    it("should encode and decode a four byte NodeId" ,function() {
+    it("should encode and decode a four byte NodeId", function () {
 
         var nodeId = ec.makeNodeId(258);
         nodeId.identifierType.should.eql(ec.NodeIdType.NUMERIC);
@@ -198,7 +197,7 @@ describe("testing built-in type encoding",function() {
             ec.encodeNodeId,
             ec.decodeNodeId,
             4,
-            function verify_buffer(buffer){
+            function verify_buffer(buffer) {
                 buffer.readUInt8(0).should.equal(1);
                 buffer.readUInt8(1).should.equal(0); // namespace
                 buffer.readUInt16LE(2).should.equal(258);
@@ -206,57 +205,119 @@ describe("testing built-in type encoding",function() {
         );
     });
 
-    it("should encode and decode a Numeric NodeId" ,function() {
+    it("should encode and decode a Numeric NodeId", function () {
 
-        var nodeId = ec.makeNodeId(545889,2500);
+        var nodeId = ec.makeNodeId(545889, 2500);
         nodeId.identifierType.should.eql(ec.NodeIdType.NUMERIC);
         test_encode_decode(
             nodeId,
             ec.encodeNodeId,
             ec.decodeNodeId,
-            7);
+            7
+        );
     });
 
 
-    it("should encode and decode a String NodeId" ,function() {
+    it("should encode and decode a String NodeId", function () {
 
-        var nodeId = ec.makeNodeId("SomeStuff",2500);
+        var nodeId = ec.makeNodeId("SomeStuff", 2500);
         nodeId.identifierType.should.eql(ec.NodeIdType.STRING);
 
         test_encode_decode(
             nodeId,
             ec.encodeNodeId,
             ec.decodeNodeId,
-            4 + 9 + 2 + 1);
+            4 + 9 + 2 + 1
+        );
 
     });
 
-    xit("should encode and decode a Guid NodeId" ,function() {
+    xit("should encode and decode a Guid NodeId", function () {
         // todo
     });
 
-    xit("should encode and decode a Opaque NodeId" ,function() {
+    xit("should encode and decode a Opaque NodeId", function () {
         // todo
     });
 
-    it("should encode and decode a Expanded NodeId  - TwoBytes" ,function() {
+    it("should encode and decode a Expanded NodeId  - TwoBytes", function () {
 
         test_encode_decode(
             ec.makeExpandedNodeId(10),
             ec.encodeExpandedNodeId,
             ec.decodeExpandedNodeId,
-            2)
-        ;
+            2
+        );
     });
-    it("should encode and decode a Expanded NodeId  - FourBytes" ,function() {
+    it("should encode and decode a Expanded NodeId  - FourBytes", function () {
 
         test_encode_decode(
             ec.makeExpandedNodeId(32000),
             ec.encodeExpandedNodeId,
             ec.decodeExpandedNodeId,
-            4)
-        ;
+            4
+        );
     });
 
 
 });
+
+
+describe("check OPCUA Date Type ", function () {
+
+    it("should convert date in 2014 ",function(){
+
+        var date    = new Date(2014,0,1);
+        var nano   = ec.dateToHundredNanoSecondFrom1601(date);
+        var date2  = ec.hundredNanoSecondFrom1601ToDate(nano);
+
+        date2.toString().should.equal(date.toString());
+
+    });
+    it("dateToHundredNanoSecondFrom1601 should return 0 for 1st of January 1601",function(){
+
+        var date    = new Date(1601,0,1,1,0);
+        var nano   = ec.dateToHundredNanoSecondFrom1601(date);
+        nano.should.equal(0);
+    });
+
+    it("dateToHundredNanoSecondFrom1601 should return xx nanos for 2st of January 1601",function(){
+        var date   = new Date(1601,0,2,1,0);
+        var nano   = ec.dateToHundredNanoSecondFrom1601(date);
+        nano.should.equal(24*60*60*1000*10000);
+    });
+
+    it("hundredNanoSecondFrom1601ToDate and dateToHundredNanoSecondFrom1601 ",function(){
+
+        var date    = new Date(1601,0,2,1,0);
+
+        var nano    = ec.dateToHundredNanoSecondFrom1601(date);
+
+        var date2   = ec.hundredNanoSecondFrom1601ToDate(nano);
+
+        date2.toString().should.equal(date.toString());
+
+    });
+
+
+    it("should decode 92c253d3 0cf7ce01 DateTime as  Dec 12, 2013 08:36:09.747317000 ", function () {
+
+        var buf = new Buffer(8);
+        buf.writeUInt32BE(0x92c253d3, 0);
+        buf.writeUInt32BE(0x0cf7ce01, 4);
+        buf.readUInt8(0).should.equal(0x92);
+        buf.readUInt8(1).should.equal(0xc2);
+        buf.readUInt8(2).should.equal(0x53);
+        buf.readUInt8(7).should.equal(0x01);
+
+        var stream = new opcua.BinaryStream(buf);
+        var date = ec.decodeDateTime(stream);
+
+        stream.rewind();
+        ec.encodeDateTime(new Date(2013,11,12,9,36,9),stream);
+
+    });
+    //
+    //  =>
+});
+
