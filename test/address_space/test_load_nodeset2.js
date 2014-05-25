5ff16a5c80d5d8679d84e992d2a19b24461e2d42
Etienne Rossignon <etienne.rossignon@gadz.org>
2014-05-25 12:01:52 +0200

    add new test to verify large nodeset xml file loading

--- test/address_space/test_load_nodeset2.js
+++ test/address_space/test_load_nodeset2.js
@@ -1,19 +1,57 @@
 var generate_address_space = require("../../lib/address_space/load_nodeset2").generate_address_space;
 var AddressSpace = require("../../lib/address_space/address_space").AddressSpace;
-
+var should = require("should");
 
 describe("testing NodeSet XML file loading",function(){
 
+
+    var address_space;
+
+    beforeEach(function(){
+
+        address_space = new AddressSpace();
+        Object.keys(address_space._aliases).length.should.equal(0);
+        Object.keys(address_space._objectMap).length.should.equal(0);
+        Object.keys(address_space._variableTypeMap).length.should.equal(0);
+        Object.keys(address_space._referenceTypeMap).length.should.equal(0);
+        Object.keys(address_space._dataTypeMap).length.should.equal(0);
+        Object.keys(address_space._objectTypeMap).length.should.equal(0);
+    });
+
     it("should load a nodeset xml file",function(done){
 
-        var address_space = new AddressSpace();
 
         var xml_file = __dirname + "/../../lib/server/mini.Node.Set2.xml";
 
+        require("fs").existsSync(xml_file).should.be.eql(true);
+
         generate_address_space(address_space,xml_file,function(err){
+
+            Object.keys(address_space._aliases).length.should.be.greaterThan(10);
+            Object.keys(address_space._objectMap).length.should.be.greaterThan(10);
+            Object.keys(address_space._variableTypeMap).length.should.be.greaterThan(3);
+            Object.keys(address_space._referenceTypeMap).length.should.be.greaterThan(10);
+            Object.keys(address_space._dataTypeMap).length.should.be.greaterThan(2);
+            Object.keys(address_space._objectTypeMap).length.should.be.greaterThan(1);
             done(err);
         })
     });
 
+    it("should load a large nodeset xml file",function(done){
+
+        var xml_file = __dirname + "/../../nodesets/Opc.Ua.NodeSet2.xml";
+        require("fs").existsSync(xml_file).should.be.eql(true);
+
+        generate_address_space(address_space,xml_file,function(err){
+
+            Object.keys(address_space._aliases).length.should.be.greaterThan(10);
+            Object.keys(address_space._objectMap).length.should.be.greaterThan(10);
+            Object.keys(address_space._variableTypeMap).length.should.be.greaterThan(10);
+            Object.keys(address_space._referenceTypeMap).length.should.be.greaterThan(10);
+            Object.keys(address_space._dataTypeMap).length.should.be.greaterThan(10);
+            Object.keys(address_space._objectTypeMap).length.should.be.greaterThan(10);
+            done(err);
+        })
+    });
 
 });
