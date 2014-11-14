13198c636aa03d1a89953607b829a000a15017f6
Etienne Rossignon <etienne.rossignon@gadz.org>
2014-11-14 07:16:21 +0100

    #17 redo default client certificate

--- certificates/makecert.sh
+++ certificates/makecert.sh
@@ -1,3 +1,5 @@
 openssl req -x509 -days 365 -nodes -newkey rsa:1024 -keyout key.pem -out cert.pem -config cert.cnf
 openssl rsa -in key.pem -pubout > public_key.pub
 
+openssl req -x509 -days 365 -nodes -newkey rsa:1024 -keyout client_key.pem -out client_cert.pem -config cert.cnf
+
