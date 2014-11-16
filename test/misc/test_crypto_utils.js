91ed3d6e265375da76c239a33ece49abd72410a3
Etienne Rossignon <etienne.rossignon@gadz.org>
2014-11-16 08:57:45 +0100

    #17 start derivedKeys

--- test/misc/test_crypto_utils.js
+++ test/misc/test_crypto_utils.js
@@ -63,15 +63,25 @@ describe("test derived key making",function() {
 
         var min_length = 256;
         var buf = crypto_utils.makePseudoRandomBuffer(secret,seed,min_length);
-        buf.length.should.be.greaterThan(min_length);
+        buf.length.should.be.equal(min_length);
         console.log(hexDump(buf));
     });
+
+    it('Should compute key using keysize, client and server keys.', function(done) {
+        // see https://github.com/leandrob/node-psha1/blob/master/test/lib.index.js#L4
+        var secret = new Buffer('GS5olVevYMI4vW1Df/7FUpHcJJopTszp6sodlK4/rP8=',"base64");
+        var seed   = new Buffer('LmF9Mjf9lYMa9YkxZDjaRFe6iMAfReKjzhLHDx376jA=',"base64");
+        var key = crypto_utils.makePseudoRandomBuffer(secret,seed, 256/8);
+        key.toString("base64").should.eql('ZMOP1NFa5VKTQ8I2awGXDjzKP+686eujiangAgf5N+Q=');
+        done();
+    });
+
     it("should create derived keys (computeDerivedKeys)",function(){
 
         var options = {
             signingKeyLength: 128,
-            encryptingKeyLength: 256,
-            encryptingBlockSize: 256,
+            encryptingKeyLength: 16,
+            encryptingBlockSize: 16,
         };
         var derivedKeys = crypto_utils.computeDerivedKeys(secret,seed,options);
 
@@ -81,36 +91,30 @@ describe("test derived key making",function() {
 
 
     });
+
+
+
     it("demonstrating how to use derived key",function(done) {
 
         var options = {
-            signingKeyLength: 32,
-            encryptingKeyLength: 128/8,
-            encryptingBlockSize: 128/8,
+            signingKeyLength: 128,
+            encryptingKeyLength: 16,
+            encryptingBlockSize: 16,
         };
         var derivedKeys = crypto_utils.computeDerivedKeys(secret,seed,options);
 
-        var algorithm = "aes-128-cbc";
-        var key = derivedKeys.encryptingKey;
-        var initVector = derivedKeys.initializationVector;
-        var cypher =     crypto.createCipheriv(algorithm,key,initVector);
-
-        cypher.setAutoPadding(false);
-
         var clear_message = make_lorem_ipsum_buffer();
 
-        var encrypted_chunks = [];
-        encrypted_chunks.push(cypher.update(clear_message));
-        // write padding
-        var paddingSize = options.encryptingBlockSize - clear_message.length % options.encryptingBlockSize;
-        var padding = new Buffer(paddingSize);
-        encrypted_chunks.push(cypher.update(padding));
-        encrypted_chunks.push(cypher.final());
-
-        var encrypted_message = Buffer.concat(encrypted_chunks);
+        var encrypted_message = crypto_utils.encryptBufferWithDerivedKeys(clear_message,derivedKeys);
 
         console.log(hexDump(encrypted_message));
         console.log(clear_message.length,encrypted_message.length);
+
+        var reconstructed_message = crypto_utils.decryptBufferWithDerivedKeys(encrypted_message,derivedKeys);
+
+        reconstructed_message = crypto_utils.removePadding(reconstructed_message);
+
+        reconstructed_message.toString("ascii").should.eql(clear_message.toString("ascii"));
 
         done();
 
