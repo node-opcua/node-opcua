5151ce7acf19265c740a3f47ac8584ca226ea01f
Etienne Rossignon <etienne.rossignon@gadz.org>
2014-12-02 19:36:23 +0100

    #17 secure policy : add SecurityPolicy.Invalid , add more tests

--- test/misc/test_security_policy.js
+++ test/misc/test_security_policy.js
@@ -14,4 +14,29 @@ describe("Security Policy",function(){
         enumValue = SecurityPolicy.fromURI("http://opcfoundation.org/UA/SecurityPolicy#Basic256Rsa15");
         enumValue.should.equal(SecurityPolicy.Basic256Rsa15);
     });
+
+    it("should return SecurityPolicy.Invalid if not supported",function(){
+
+        var enumValue = SecurityPolicy.fromURI("some invalid string");
+        enumValue.should.equal(SecurityPolicy.Invalid);
+
+    });
+    it("should turn a Security Policy Enum value into an URI",function(){
+        var uriValue = SecurityPolicy.toURI(SecurityPolicy.Basic256Rsa15);
+        uriValue.should.equal("http://opcfoundation.org/UA/SecurityPolicy#Basic256Rsa15");
+    });
+    it("should turn a Security Policy short string to an URI",function(){
+        var uriValue = SecurityPolicy.toURI("Basic256Rsa15");
+        uriValue.should.equal("http://opcfoundation.org/UA/SecurityPolicy#Basic256Rsa15");
+    });
+    it("should thrown an exception when turning an invalid SecurityPolicy into an uri",function(){
+
+       should(function() {
+           var uriValue = SecurityPolicy.toURI("<<invalid>>");
+           uriValue.should.equal("<invalid>");
+       }).throwError();
+
+    });
+
+
 });
