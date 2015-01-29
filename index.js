cb7f69d1fa130c1638ce0ad654fb06c4ff9330f5
Etienne Rossignon <etienne.rossignon@gadz.org>
2015-01-29 21:47:41 +0100

    #28 add a test to stress writing an array of double, exposed VariantArrayType

--- index.js
+++ index.js
@@ -1,48 +1,50 @@
+require("requirish")._(module);
 
 // common services
-module.exports.structures       = require("./lib/datamodel/structures");
-module.exports.parseEndpointUrl = require("./lib/nodeopcua").parseEndpointUrl;
-module.exports.resolveNodeId    = require("./lib/datamodel/nodeid").resolveNodeId;
-module.exports.makeNodeId       = require("./lib/datamodel/nodeid").makeNodeId;
-module.exports.coerceNodeId     = require("./lib/datamodel/nodeid").coerceNodeId;
-module.exports.makeExpandedNodeId       = require("./lib/datamodel/expanded_nodeid").makeExpandedNodeId;
-module.exports.coerceExpandedNodeId     = require("./lib/datamodel/expanded_nodeid").coerceExpandedNodeId;
-module.exports.StatusCodes      = require("./lib/datamodel/opcua_status_code").StatusCodes;
-module.exports.DataType         = require("./lib/datamodel/variant").DataType;
-module.exports.DataValue        = require("./lib/datamodel/datavalue").DataValue;
-module.exports.Variant          = require("./lib/datamodel/variant").Variant;
-module.exports.NodeId           = require("./lib/datamodel/nodeid").NodeId;
-module.exports.NumericRange     = require("./lib/datamodel/numeric_range").NumericRange;
-module.exports.AccessLevelFlag  = require("./lib/datamodel/access_level").AccessLevelFlag;
+module.exports.structures       = require("lib/datamodel/structures");
+module.exports.parseEndpointUrl = require("lib/nodeopcua").parseEndpointUrl;
+module.exports.resolveNodeId    = require("lib/datamodel/nodeid").resolveNodeId;
+module.exports.makeNodeId       = require("lib/datamodel/nodeid").makeNodeId;
+module.exports.coerceNodeId     = require("lib/datamodel/nodeid").coerceNodeId;
+module.exports.makeExpandedNodeId       = require("lib/datamodel/expanded_nodeid").makeExpandedNodeId;
+module.exports.coerceExpandedNodeId     = require("lib/datamodel/expanded_nodeid").coerceExpandedNodeId;
+module.exports.StatusCodes      = require("lib/datamodel/opcua_status_code").StatusCodes;
+module.exports.DataType         = require("lib/datamodel/variant").DataType;
+module.exports.DataValue        = require("lib/datamodel/datavalue").DataValue;
+module.exports.Variant          = require("lib/datamodel/variant").Variant;
+module.exports.VariantArrayType = require("lib/datamodel/datavalue").VariantArrayType;
+module.exports.NodeId           = require("lib/datamodel/nodeid").NodeId;
+module.exports.NumericRange     = require("lib/datamodel/numeric_range").NumericRange;
+module.exports.AccessLevelFlag  = require("lib/datamodel/access_level").AccessLevelFlag;
 
 // services
-module.exports.browse_service   = require("./lib/services/browse_service");
-module.exports.read_service     = require("./lib/services/read_service");
-module.exports.call_service     = require("./lib/services/call_service");
-module.exports.session_service  = require("./lib/services/session_service");
-module.exports.get_endpoints_service  = require("./lib/services/get_endpoints_service");
+module.exports.browse_service   = require("lib/services/browse_service");
+module.exports.read_service     = require("lib/services/read_service");
+module.exports.call_service     = require("lib/services/call_service");
+module.exports.session_service  = require("lib/services/session_service");
+module.exports.get_endpoints_service  = require("lib/services/get_endpoints_service");
 
-module.exports.utils     = require("./lib/misc/utils");
+module.exports.utils     = require("lib/misc/utils");
 
 module.exports.AttributeIds     = module.exports.read_service.AttributeIds;
 module.exports.AttributeNameById= module.exports.read_service.AttributeNameById;
-module.exports.VariableIds      = require("./lib/opcua_node_ids").VariableIds;
-module.exports.MethodIds        = require("./lib/opcua_node_ids").MethodIds;
-module.exports.ObjectIds        =require("./lib/opcua_node_ids").ObjectIds;
+module.exports.VariableIds      = require("lib/opcua_node_ids").VariableIds;
+module.exports.MethodIds        = require("lib/opcua_node_ids").MethodIds;
+module.exports.ObjectIds        =require("lib/opcua_node_ids").ObjectIds;
 
 // client services
-module.exports.OPCUAClient        = require("./lib/client/opcua_client").OPCUAClient;
-module.exports.NodeCrawler        = require("./lib/client/node_crawler").NodeCrawler;
-module.exports.ClientSubscription = require("./lib/client/client_subscription").ClientSubscription;
-module.exports.ClientSession       = require("./lib/client/opcua_client").ClientSession;
+module.exports.OPCUAClient        = require("lib/client/opcua_client").OPCUAClient;
+module.exports.NodeCrawler        = require("lib/client/node_crawler").NodeCrawler;
+module.exports.ClientSubscription = require("lib/client/client_subscription").ClientSubscription;
+module.exports.ClientSession       = require("lib/client/opcua_client").ClientSession;
 
 // Server services
-module.exports.OPCUAServer      = require("./lib/server/opcua_server").OPCUAServer;
-module.exports.ServerEngine     = require("./lib/server/server_engine").ServerEngine;
-module.exports.generate_address_space = require("./lib/address_space/load_nodeset2").generate_address_space;
-module.exports.AddressSpace     = require("./lib/address_space/address_space").AddressSpace;
+module.exports.OPCUAServer      = require("lib/server/opcua_server").OPCUAServer;
+module.exports.ServerEngine     = require("lib/server/server_engine").ServerEngine;
+module.exports.generate_address_space = require("lib/address_space/load_nodeset2").generate_address_space;
+module.exports.AddressSpace     = require("lib/address_space/address_space").AddressSpace;
 
-module.exports.SecurityPolicy = require("./lib/misc/security_policy").SecurityPolicy;
+module.exports.SecurityPolicy = require("lib/misc/security_policy").SecurityPolicy;
 
 //
 module.exports.MessageSecurityMode = module.exports.get_endpoints_service.MessageSecurityMode;
--- test/simulation/test_simulation_address_space.js
+++ test/simulation/test_simulation_address_space.js
@@ -3,16 +3,16 @@ var address_space_for_conformance_testing  = require("lib/simulation/address_spa
 var build_address_space_for_conformance_testing = address_space_for_conformance_testing.build_address_space_for_conformance_testing;
 
 var address_space = require("lib/address_space/address_space");
-var generate_address_space = require("lib/address_space/load_nodeset2").generate_address_space;
-var AddressSpace = address_space.AddressSpace;
-var should  = require("should");
 var nodeid = require("lib/datamodel/nodeid");
-var ReferenceType = address_space.ReferenceType;
 var server_engine = require("lib/server/server_engine");
 var makeNodeId = nodeid.makeNodeId;
 var DataType = require("lib/datamodel/variant").DataType;
+var VariantArrayType = require("lib/datamodel/variant").VariantArrayType;
 var WriteValue = require("lib/services/write_service").WriteValue;
+var AttributeIds = require("lib/datamodel/attributeIds").AttributeIds;
+var StatusCodes  = require("lib/datamodel/opcua_status_code").StatusCodes;
 
+var should  = require("should");
 var assert = require("better-assert");
 
 var namespaceIndex = 411; // namespace for conformance testing nodes
@@ -24,82 +24,123 @@ describe("testing address space for conformance testing",function() {
 
     this.timeout(40000);
 
-    beforeEach(function() {
+    before(function(done) {
 
         engine = new server_engine.ServerEngine();
+        engine.initialize(null,function() {
+            build_address_space_for_conformance_testing(engine,{ mass_variables: true});
+
+            // address space variable change for conformance testing are changing randomly
+            // let wait a little bit to make sure variables have changed at least once
+            setTimeout(done,500);
+        });
     });
-    afterEach(function() {
+    after(function() {
         engine = null;
     });
 
-    it("should build an address space for conformance testing",function(done) {
+    it("should check that AccessLevel_CurrentRead_NotCurrentWrite Int32 can be read but not written",function() {
 
+        var nodeId = makeNodeId("AccessLevel_CurrentRead_NotCurrentWrite", namespaceIndex);
+        var value = engine.readSingleNode(nodeId,AttributeIds.Value);
 
-        engine.initialize(null,function(){
-            build_address_space_for_conformance_testing(engine);
+        value.statusCode.should.eql(StatusCodes.Good);
+        value.value.dataType.should.eql(DataType.Int32);
+        value.value.arrayType.should.eql(VariantArrayType.Scalar);
+        value.value.value.should.eql(36);
 
-            // check that AccessLevel_CurrentRead_NotCurrentWrite Int32 can be read but not written
-
-
-            // change one value
-            var Scalar_Static_Int32_NodeId = makeNodeId("Scalar_Static_Int32", namespaceIndex);
-            engine.writeSingleNode(new WriteValue({
-                nodeId: Scalar_Static_Int32_NodeId,
-                attributeId: 13,
-                value: {
-                    value : {
-                        dataType: DataType.Int32,
-                        value: 1000
-                    }
+        // now write it again
+        var result = engine.writeSingleNode(new WriteValue({
+            nodeId: nodeId,
+            attributeId: AttributeIds.Value,
+            value: {
+                value : {
+                    dataType: DataType.Int32,
+                    value: 1000
                 }
-            }));
+            }
+        }));
 
+        result.should.eql(StatusCodes.BadNotWritable , " writing on AccessLevel_CurrentRead_NotCurrentWrite should raise BadNotWritable ");
 
-
-
-            // address space variable change for conformance testing are changing randomly
-            // let wait a little bit to make sure variables have changed at least once
-            setTimeout(done,500);
-            // done();
-        });
     });
 
-    it("should build an address space for conformance testing with options.mass_variables",function(done) {
+    it("XX should be able to write a array of double on Scalar_Static_Array_Double",function() {
 
-        engine.initialize(null,function(){
-            build_address_space_for_conformance_testing(engine,{ mass_variables: true});
+        var nodeId = makeNodeId("Scalar_Static_Array_Double", namespaceIndex);
+        var value = engine.readSingleNode(nodeId,AttributeIds.Value);
 
-            // browseName Interval
-            // browseName Enabled
-            var intervalNodeId = makeNodeId("Scalar_Simulation_Interval", namespaceIndex);
-            // change interval to 200 ms
-            engine.writeSingleNode(new WriteValue({
-                nodeId: intervalNodeId,
-                attributeId: 13,
-                value: {
-                   value : {
-                       dataType: DataType.UInt16,
-                       value: 250
-                   }
+        value.statusCode.should.eql(StatusCodes.Good);
+        value.value.dataType.should.eql(DataType.Double);
+        value.value.arrayType.should.eql(VariantArrayType.Array);
+        value.value.value.length.should.eql(10);
+
+        // now write it again
+        var result = engine.writeSingleNode(new WriteValue({
+            nodeId: nodeId,
+            attributeId: AttributeIds.Value,
+            value: {
+                value : {
+                    dataType: DataType.Double,
+                    arrayType: VariantArrayType.Array,
+                    value: [10,20,30,40,50]
                 }
-            }));
-            // set enable to true
-            var enabledNodeId = makeNodeId("Scalar_Simulation_Enabled", namespaceIndex);
-            engine.writeSingleNode(new WriteValue({
-                nodeId: enabledNodeId,
-                attributeId: 13,
-                value: {
-                    value : {
-                        dataType: DataType.Boolean,
-                        value: true
-                    }
+            }
+        }));
+
+        result.should.eql(StatusCodes.Good);
+
+    });
+
+
+    it("should write a scalar Int32 value to the  Scalar_Static_Int32_NodeId node",function() {
+
+        // change one value
+        var nodeId = makeNodeId("Scalar_Static_Int32", namespaceIndex);
+
+        engine.writeSingleNode(new WriteValue({
+            nodeId: nodeId,
+            attributeId: AttributeIds.Value,
+            value: {
+                value : {
+                    dataType: DataType.Int32,
+                    value: 1000
                 }
-            }));
+            }
+        }));
 
-            // address space variable change for conformance testing are changing randomly
-            // let wait a little bit to make sure variables have changed at least once
-            setTimeout(done,500);
+    });
 
-        });
+    it("should build an address space for conformance testing with options.mass_variables",function() {
+
+
+
+        // browseName Interval
+        // browseName Enabled
+        var intervalNodeId = makeNodeId("Scalar_Simulation_Interval", namespaceIndex);
+        // change interval to 200 ms
+        engine.writeSingleNode(new WriteValue({
+            nodeId: intervalNodeId,
+            attributeId: AttributeIds.Value,
+            value: {
+               value : {
+                   dataType: DataType.UInt16,
+                   value: 250
+               }
+            }
+        }));
+        // set enable to true
+        var enabledNodeId = makeNodeId("Scalar_Simulation_Enabled", namespaceIndex);
+        engine.writeSingleNode(new WriteValue({
+            nodeId: enabledNodeId,
+            attributeId: AttributeIds.Value,
+            value: {
+                value : {
+                    dataType: DataType.Boolean,
+                    value: true
+                }
+            }
+        }));
+
     });
 });
