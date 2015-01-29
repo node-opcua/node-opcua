2fd60d3640de77159c0527af7347b758fe65b10e
Etienne Rossignon <etienne.rossignon@gadz.org>
2015-01-29 21:55:42 +0100

    #42 permit lower case  letters in GUID strings

--- test/datamodel/test_guid.js
+++ test/datamodel/test_guid.js
@@ -4,6 +4,10 @@ var isValidGuid = require("lib/datamodel/guid").isValidGuid;
 var emptyGuid = require("lib/datamodel/guid").emptyGuid;
 describe("GUID",function(){
 
+    it("emptyGuid should be a valid GUID",function() {
+        isValidGuid(emptyGuid).should.equal(true);
+    });
+
     it("should detect strings that looks like GUID",function(){
 
         isValidGuid("72962B91-FA75-4AE6-8D28-B404DC7DAF63").should.equal(true);
@@ -14,6 +18,18 @@ describe("GUID",function(){
         isValidGuid("72962B-91FA75-4AE6-8D28-B404DC7DAF639999999").should.equal(false);
         isValidGuid(emptyGuid).should.equal(true);
 
+    });
+
+    it("should accept letters in lower case in GUID strings",function(){
+
+        isValidGuid("72962b91-fa75-4ae6-8d28-b404dc7daf63").should.equal(true);
+        isValidGuid("zz962B91-FA75-4AE6-8D28-B404DC7DAF63").should.equal(false);
+        isValidGuid("72962b-91fa75-4ae6-8d28-b404dc7daf63").should.equal(false);
+
+        isValidGuid("111111172962b-91fa75-4ae6-8d28-b404dc7daf63").should.equal(false);
+        isValidGuid("72962b-91fa75-4ae6-8d28-b404dc7daf639999999").should.equal(false);
+
 
     });
+
 });
