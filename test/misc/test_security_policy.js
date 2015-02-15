8ab7da0765e9a8e1fc9e50d2cae80b375bd0a28b
Etienne Rossignon <etienne.rossignon@gadz.org>
2015-02-15 23:48:39 +0100

    upgrade packages version fix code to new "enum" package version

--- test/misc/test_security_policy.js
+++ test/misc/test_security_policy.js
@@ -2,38 +2,39 @@ require("requirish")._(module);
 
 var should = require("should");
 
-var SecurityPolicy = require("lib/misc/security_policy").SecurityPolicy;
+var securityPolicy_m = require("lib/misc/security_policy");
+var SecurityPolicy = securityPolicy_m.SecurityPolicy;
 
 
 describe("Security Policy",function(){
 
     it("should convert a security policy uri to an enum value",function() {
 
-        var enumValue = SecurityPolicy.fromURI("http://opcfoundation.org/UA/SecurityPolicy#None");
+        var enumValue = securityPolicy_m.fromURI("http://opcfoundation.org/UA/SecurityPolicy#None");
         enumValue.should.equal(SecurityPolicy.None);
 
-        enumValue = SecurityPolicy.fromURI("http://opcfoundation.org/UA/SecurityPolicy#Basic256Rsa15");
+        enumValue = securityPolicy_m.fromURI("http://opcfoundation.org/UA/SecurityPolicy#Basic256Rsa15");
         enumValue.should.equal(SecurityPolicy.Basic256Rsa15);
     });
 
     it("should return SecurityPolicy.Invalid if not supported",function(){
 
-        var enumValue = SecurityPolicy.fromURI("some invalid string");
+        var enumValue = securityPolicy_m.fromURI("some invalid string");
         enumValue.should.equal(SecurityPolicy.Invalid);
 
     });
     it("should turn a Security Policy Enum value into an URI",function(){
-        var uriValue = SecurityPolicy.toURI(SecurityPolicy.Basic256Rsa15);
+        var uriValue = securityPolicy_m.toURI(SecurityPolicy.Basic256Rsa15);
         uriValue.should.equal("http://opcfoundation.org/UA/SecurityPolicy#Basic256Rsa15");
     });
     it("should turn a Security Policy short string to an URI",function(){
-        var uriValue = SecurityPolicy.toURI("Basic256Rsa15");
+        var uriValue = securityPolicy_m.toURI("Basic256Rsa15");
         uriValue.should.equal("http://opcfoundation.org/UA/SecurityPolicy#Basic256Rsa15");
     });
     it("should thrown an exception when turning an invalid SecurityPolicy into an uri",function(){
 
        should(function() {
-           var uriValue = SecurityPolicy.toURI("<<invalid>>");
+           var uriValue = securityPolicy_m.toURI("<<invalid>>");
            uriValue.should.equal("<invalid>");
        }).throwError();
 
