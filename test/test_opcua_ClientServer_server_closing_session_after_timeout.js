e4fca63e0610e5333c42acffa7f9926b82bc1334
Etienne Rossignon <etienne.rossignon@gadz.org>
2015-04-11 23:09:00 +0200

    detect closed session; returns serviceResult  BadSessionClosed when attempting to request a closed session

---
+++ test/test_opcua_ClientServer_server_closing_session_after_timeout.js
@@ -0,0 +1,144 @@
+require("requirish")._(module);
+
+var should = require("should");
+var assert = require("better-assert");
+var async = require("async");
+var util = require("util");
+var _ = require("underscore");
+var sinon = require("sinon");
+
+var opcua = require("index");
+
+var OPCUAServer = opcua.OPCUAServer;
+var OPCUAClient = opcua.OPCUAClient;
+var StatusCodes = opcua.StatusCodes;
+var SignatureData = require("lib/datamodel/structures").SignatureData;
+
+var port = 2000;
+
+var empty_nodeset_filename = require("path").join(__dirname, "./fixtures/fixture_empty_nodeset2.xml");
+
+describe("testing server dropping session after timeout if no activity has been recorded", function () {
+
+
+    this.timeout(20000);
+
+    var server = new OPCUAServer({
+        port: port,
+        nodeset_filename: empty_nodeset_filename
+    });
+    var serverCertificate = server.getCertificate();
+
+    var options = {
+        securityMode: opcua.MessageSecurityMode.SIGNANDENCRYPT,
+        securityPolicy: opcua.SecurityPolicy.Basic256,
+        serverCertificate: serverCertificate,
+        defaultSecureTokenLifetime: 2000
+    };
+
+    var nodeId = opcua.resolveNodeId("ns=0;i=2258");
+
+    var readRequest = new opcua.read_service.ReadRequest({
+        maxAge: 0,
+        timestampsToReturn: opcua.read_service.TimestampsToReturn.Both,
+        nodesToRead: [
+            {
+                nodeId: nodeId,
+                attributeId: opcua.read_service.AttributeIds.Value,
+            }
+        ]
+    });
+
+    var endpointUrl;
+    before(function (done) {
+        server.start(function (err) {
+            endpointUrl = server.endpoints[0].endpointDescriptions()[0].endpointUrl;
+            done(err);
+        });
+
+    });
+
+    after(function (done) {
+
+        async.series([
+            function (callback) {
+                server.shutdown(callback);
+            }
+        ], done);
+    });
+
+    it("should not be able to read a node if no session has been opened ", function (done) {
+
+        var client = new OPCUAClient(options);
+
+        async.series([
+            // given that client1 is connected, and have a session
+            function (callback) {
+                client.connect(endpointUrl, callback);
+            },
+
+            // reading should fail
+            function (callback) {
+
+                client._secureChannel.performMessageTransaction(readRequest, function (err, response) {
+                    response.responseHeader.serviceResult.should.equal(opcua.StatusCodes.BadSessionNotActivated);
+                    callback(err);
+                });
+            },
+            function (callback) {
+                client.disconnect(callback);
+            },
+
+        ], done);
+    });
+
+    it("should denied service call wikth BadSessionClosed on a timed out session", function (done) {
+        var client = new OPCUAClient(options);
+
+        var l_session = null;
+        async.series([
+            // given that client1 is connected, and have a session
+            function (callback) {
+                client.requestedSessionTimeout = 100;
+                client.connect(endpointUrl, callback);
+            },
+            function (callback) {
+
+                server.currentSessionCount.should.eql(0);
+                client.createSession(function (err, session) {
+                    if (err) {
+                        return callback(err);
+                    }
+                    l_session = session;
+                    session.timeout.should.equal(100);
+                    server.currentSessionCount.should.eql(1);
+                    callback(null);
+                });
+
+
+            },
+
+            // now wait so that session times out on the server side
+            function (callback) {
+                setTimeout(callback, 1500);
+            },
+
+            // reading should fail
+            function (callback) {
+
+                server.currentSessionCount.should.eql(0);
+                l_session.read(readRequest.nodesToRead,function(err,results){
+                    err.message.should.match(/BadSessionClosed/);
+                    callback(null);
+                });
+            },
+            function (callback) {
+
+                callback();
+                //xx client.disconnect(callback);
+            },
+
+        ], done);
+    });
+
+})
