f1cd477c6bb6e0482d34dd718496d0fcc59d9c63
Etienne Rossignon <etienne.rossignon@gadz.org>
2015-04-30 00:01:33 +0200

    #71 add benchmark test for  Enum implementation

--- test/misc/test_enum.js
+++ test/misc/test_enum.js
@@ -1,3 +1,4 @@
+"use strict"
 require("requirish")._(module);
 var Enum = require("lib/misc/enum");
 var should = require("should");
@@ -5,7 +6,7 @@ var should = require("should");
 describe("Test Enum", function () {
 
     it("should create flaggable enum from string array", function () {
-        var e = new Enum(['e1','e2','e3']);
+        var e = new Enum(['e1', 'e2', 'e3']);
         e.get('e1').value.should.equal(1);
         e.get('e2').value.should.equal(2);
         e.get('e3').value.should.equal(4);
@@ -17,7 +18,7 @@ describe("Test Enum", function () {
     });
 
     it("should create flaggable enum from flaggable map", function () {
-        var e = new Enum({'e1':1,'e2':2,'e3':4});
+        var e = new Enum({'e1': 1, 'e2': 2, 'e3': 4});
         e.get('e1').value.should.equal(1);
         e.get('e2').value.should.equal(2);
         e.get('e3').value.should.equal(4);
@@ -29,7 +30,7 @@ describe("Test Enum", function () {
     });
 
     it("should create non-flaggable enum from non-flaggable map", function () {
-        var e = new Enum({'e1':1,'e2':2,'e3':3});
+        var e = new Enum({'e1': 1, 'e2': 2, 'e3': 3});
         e.get('e1').value.should.equal(1);
         e.get('e2').value.should.equal(2);
         e.get('e3').value.should.equal(3);
@@ -42,7 +43,7 @@ describe("Test Enum", function () {
     });
 
     it("should access enum from enum item name", function () {
-        var e = new Enum({'e1':1,'e2':2,'e3':4});
+        var e = new Enum({'e1': 1, 'e2': 2, 'e3': 4});
         e.e1.value.should.equal(1);
         e.e1.key.should.equal('e1');
         e.e2.value.should.equal(2);
@@ -52,7 +53,7 @@ describe("Test Enum", function () {
     });
 
     it("EnumItem should function properly", function () {
-        var e = new Enum({'e1':1,'e2':2,'e3':4});
+        var e = new Enum({'e1': 1, 'e2': 2, 'e3': 4});
         var e1ore2 = e.get('e2 | e1');
         e1ore2.value.should.equal(3);
         e1ore2.is(e.get(3)).should.equal(true);
@@ -80,3 +81,85 @@ describe("Test Enum", function () {
 
 });
 
+var Benchmarker = require("test/helpers/benchmarker").Benchmarker;
+
+
+var EnumSlow = require("enum");
+var EnumFast = require("lib/misc/enum");
+
+describe("Benchmarking Enums", function (done) {
+
+
+    function perform_benchmark(params, checks, done) {
+
+        var bench = new Benchmarker();
+
+
+        function test_iteration(en) {
+
+            var e1 = en.SOMEDATA;
+            var e2 = en.get("OTHERDATA");
+
+            checks.forEach(function (p) {
+                p.value.should.eql(en.get(p.key).value);
+            });
+        }
+
+        bench.add('slowEnum', function () {
+
+            var en = new EnumSlow(params);
+            test_iteration(en);
+
+        })
+        .add('fastEnum', function () {
+
+            var en = new EnumFast(params);
+            test_iteration(en);
+        })
+        .on('cycle', function (message) {
+            console.log(message);
+        })
+        .on('complete', function () {
+
+            console.log(' Fastest is ' + this.fastest.name);
+            console.log(' Speed Up : x', this.speedUp);
+            this.fastest.name.should.eql("fastEnum");
+            done();
+        })
+        .run();
+
+    }
+
+    it("should verify that our enums are faster than old enums", function (done) {
+
+        var params = {SOMEDATA: 1, OTHERDATA: 2, BLAH: 3};
+        var checks = [{key: "SOMEDATA", value: 1}];
+        perform_benchmark(params, checks, done);
+    });
+
+    it("should verify that our enums are faster than old enums", function (done) {
+
+        var params = ["SOMEDATA", "OTHERDATA", "BLAH", "RED", "WHITE", "BLUE", "GREEN", "ORANGE"];
+
+        var checks = [
+            {key: "SOMEDATA", value: 1},
+            {key: "OTHERDATA", value: 2},
+            {key: "BLAH", value: 4},
+            {key: "WHITE | BLUE", value: 16 + 32}];
+        perform_benchmark(params, checks, done);
+    });
+    it("should verify that our enums are faster than old enums", function (done) {
+
+        var AccessLevelFlag = {
+            CurrentRead: 0x01,
+            CurrentWrite: 0x02,
+            HistoryRead: 0x04,
+            HistoryWrite: 0x08,
+            SemanticChange: 0x10
+        };
+        var checks = [{key: "CurrentWrite | HistoryWrite", value: 0x0A}];
+        perform_benchmark(AccessLevelFlag, checks, done);
+    });
+
+
+});
