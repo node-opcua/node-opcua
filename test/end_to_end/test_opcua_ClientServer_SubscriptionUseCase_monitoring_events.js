758e33218dc3463cf2990cca840695cebd631e0a
Etienne Rossignon <etienne.rossignon@gadz.org>
2015-07-09 22:41:17 +0200

    #90 refactor basic Monitoring event with Filters, add more tests

--- test/end_to_end/test_opcua_ClientServer_SubscriptionUseCase_monitoring_events.js
+++ test/end_to_end/test_opcua_ClientServer_SubscriptionUseCase_monitoring_events.js
@@ -17,6 +17,7 @@ var StatusCodes = opcua.StatusCodes;
 var DataType = opcua.DataType;
 var TimestampsToReturn = opcua.read_service.TimestampsToReturn;
 var MonitoringMode = opcua.subscription_service.MonitoringMode;
+var NumericRange = opcua.NumericRange;
 
 var build_server_with_temperature_device = require("test/helpers/build_server_with_temperature_device").build_server_with_temperature_device;
 var perform_operation_on_client_session = require("test/helpers/perform_operation_on_client_session").perform_operation_on_client_session;
@@ -29,7 +30,7 @@ var resourceLeakDetector = require("test/helpers/resource_leak_detector").resour
 var schema_helpers =  require("lib/misc/factories_schema_helpers");
 schema_helpers.doDebug = true;
 
-describe("testing Client-Server subscription use case, on a fake server exposing the temperature device", function () {
+describe("Client Subscription", function () {
 
     var server, client, temperatureVariableId, endpointUrl;
 
@@ -157,6 +158,129 @@ describe("testing Client-Server subscription use case, on a fake server exposing
             // now publish and check that monitored item returns
         }, done);
     });
-});
 
+    var TimestampsToReturn = opcua.read_service.TimestampsToReturn;
+
+    it("should raised a error if a filter is specified when monitoring some attributes which are not Value or EventNotifier", function (done) {
+
+        perform_operation_on_subscription(client, endpointUrl, function (session, subscription, callback) {
+
+            var readValue = {
+                nodeId: resolveNodeId("Server"),
+                attributeId: AttributeIds.BrowseName // << NOT (Value or EventNotifier)
+            };
+            var requestedParameters= {
+                samplingInterval: 10,
+                discardOldest: true,
+                queueSize: 1,
+
+                filter: new opcua.subscription_service.DataChangeFilter({}) // FILTER !
+            };
+            var monitoredItem = subscription.monitor(readValue,requestedParameters,TimestampsToReturn.Both,function(err){
+                should(err).not.eql(null);
+                err.message.should.match(/no filter expected/);
+                console.log(err.message);
+                callback();
+            });
+
+        },done);
+    });
+
+    it("should raised a error if filter is not of type EventFilter when monitoring an event", function (done) {
+
+        perform_operation_on_subscription(client, endpointUrl, function (session, subscription, callback) {
+
+            var readValue = {
+                nodeId: resolveNodeId("Server"),
+                attributeId: AttributeIds.EventNotifier // << EventNotifier
+            };
+            var requestedParameters= {
+                samplingInterval: 10,
+                discardOldest: true,
+                queueSize: 1,
+
+                filter: new opcua.subscription_service.DataChangeFilter({}) // intentionally wrong :not an EventFilter
+
+            };
+            var monitoredItem = subscription.monitor(readValue,requestedParameters,TimestampsToReturn.Both,function(err){
+                should(err).not.eql(null);
+                err.message.should.match(/Got a DataChangeFilter but a EventFilter/);
+                console.log(err.message);
+                callback();
+            });
+
+        },done);
+    });
+
+    it("should create a monitoredItem on a event with a Filter 2", function (done) {
+
+        perform_operation_on_subscription(client, endpointUrl, function (session, subscription, inner_done) {
+
+            var subscription_service =opcua.subscription_service;
+            var readValue = {
+                nodeId: resolveNodeId("Server"),
+                attributeId: AttributeIds.EventNotifier
+            };
+            var filter = new subscription_service.EventFilter({
+                selectClauses: [// SimpleAttributeOperand
+                    {
+                        typeId: "i=123", // NodeId
+
+                        browsePath: [    // QualifiedName
+                            {namespaceIndex: 1, name: "A"}, {namespaceIndex: 1, name: "B"}, {namespaceIndex: 1, name: "C"}
+                        ],
+                        attributeId: AttributeIds.Value,
+                        indexRange: new NumericRange()
+                    },
+                    {
+                        // etc...
+                    },
+                    {
+                        // etc...
+                    }
+                ],
+                whereClause: { //ContentFilter
+                    elements: [ // ContentFilterElement
+                        {
+                            filterOperator: subscription_service.FilterOperator.IsNull,
+                            filterOperands: [ //
+                                new subscription_service.ElementOperand({
+                                    index: 123
+                                }),
+                                new subscription_service.AttributeOperand({
+                                    nodeId: "i=10",
+                                    alias: "someText",
+                                    browsePath: { //RelativePath
+
+                                    },
+                                    attributeId: AttributeIds.Value
+                                })
+                            ]
+                        }
+                    ]
+                }
+            });
+
+            var requestedParameters= {
+                samplingInterval: 10,
+                discardOldest: true,
+                queueSize: 1,
+                filter: filter
+            };
+            var monitoredItem = subscription.monitor(readValue,requestedParameters,TimestampsToReturn.Both);
+
+            monitoredItem.on("changed", function (dataValue) {
+
+            });
+
+
+            setTimeout(function () {
+                inner_done();
+            }, 400);
+
+        },done);
+
+    });
+
+});
 
