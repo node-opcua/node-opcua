0708f8a1cf4d7824290de1a7b77530a34ac42285
Etienne Rossignon <etienne.rossignon@gadz.org>
2015-10-20 22:22:06 +0200

    add doc

--- schemas/AsymmetricAlgorithmSecurityHeader_schema.js
+++ schemas/AsymmetricAlgorithmSecurityHeader_schema.js
@@ -18,6 +18,18 @@ var AsymmetricAlgorithmSecurityHeader_Schema = {
         // This is a DER encoded blob.
         // This indicates what private key was used to sign the MessageChunk.
         // This field shall be null if the message is not signed.
+        // The structure of an X509 Certificate is defined in X509.
+        // The DER format for a Certificate is defined in X690
+        // The Stack shall close the channel and report an error to the Application if the SenderCertificate
+        // is too large for the buffer size supported by the transport layer.
+        // If the Certificate is signed by a CA the DER encoded CA Certificate may be appended after the Certificate
+        // in the byte array. If the CA Certificate is also signed by another CA this process is repeated until
+        // the entire Certificate chain is in the buffer or if MaxSenderCertificateSize limit is reached
+        // (the process stops after the last whole Certificate that can be added without exceeding the
+        // MaxSenderCertificateSize limit).
+        // Receivers can extract the Certificates from the byte array by using the Certificate size contained
+        // in DER header (see X509).
+        // Receivers that do not handle Certificate chains shall ignore the extra bytes.
         { name:"senderCertificate" ,             fieldType:"ByteString", defaultValue: null },
 
         // The thumbprint of the X509v3 certificate assigned to the receiving application
