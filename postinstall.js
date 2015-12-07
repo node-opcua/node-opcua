a63e6d33d42546b83e4eb329bed3c191a31b88bb
Etienne Rossignon <etienne.rossignon@gadz.org>
2015-12-07 23:19:46 +0100

    remove warning messages

--- postinstall.js
+++ postinstall.js
@@ -4,20 +4,20 @@ var child_process = require("child_process");
 function exec(cmd,callback) {
 
     console.log("Executing " ,cmd);
+    var the_code = 61;
     var child = child_process.exec(cmd,function(err) {
-        console.log("done ...");
-        callback(err);
     });
 
     //xx child.stdout.pipe(process.stdout);
 
     child.on('close', function(code) {
-        console.log('closing code: ' + code);
+        the_code = code;
+        console.log("done ... (" + the_code + ")");
+        callback();
     });
 }
 
 exec("node bin/generate_opcua_classes.js --clear ",function() {
     exec( "node bin/crypto_create_CA.js ",function() {
-        console.log("done");
     });
 });
