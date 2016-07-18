5741cf845b172550f98fc1321c6fe66f0a54b39a
Etienne Rossignon <etienne.rossignon@gadz.org>
2016-07-19 01:25:20 +0200

    add ability to set information bits on StatusCode

--- test/datamodel/test_status_code.js
+++ test/datamodel/test_status_code.js
@@ -23,7 +23,7 @@ describe("testing status code manipulation", function () {
 
     it("should create GoodWithOverflowBit", function () {
         StatusCodes.GoodWithOverflowBit.value.should.equal(0x480);
-        StatusCodes.GoodWithOverflowBit.name.should.equal("GoodWithOverflowBit");
+        StatusCodes.GoodWithOverflowBit.name.should.equal("Good#InfoTypeDataValue|Overflow");
     });
 
     it("should encode and decode a status code", function () {
@@ -31,10 +31,8 @@ describe("testing status code manipulation", function () {
         var stream = new BinaryStream(8);
         var statusCode = StatusCodes.BadNodeIdExists;
         encodeStatusCode(statusCode, stream);
-
         stream.rewind();
         var statusCode2 = decodeStatusCode(stream);
-
         statusCode2.should.eql(statusCode);
 
     });
@@ -51,5 +49,86 @@ describe("testing status code manipulation", function () {
         StatusCodes.UncertainInitialValue.name.should.equal("UncertainInitialValue");
 
     });
+
+    it("GoodWithOverflowBit",function() {
+        var statusCode2 =  StatusCodes.makeStatusCode(StatusCodes.Good,"Overflow | InfoTypeDataValue");
+        statusCode2.should.eql(StatusCodes.GoodWithOverflowBit);
+    });
+
+    it("should be possible to set SemanticChanged bit on a status code",function() {
+
+        var statusCode2 =  StatusCodes.makeStatusCode(StatusCodes.BadNodeIdExists);
+        statusCode2.set("SemanticChanged");
+        statusCode2.value.should.eql(StatusCodes.BadNodeIdExists + 0x4000);
+        statusCode2.name.should.eql("BadNodeIdExists#SemanticChanged");
+
+    });
+    it("should be possible to set the Overflow bit on a status code",function() {
+
+        var statusCode2 =  StatusCodes.makeStatusCode(StatusCodes.BadNodeIdExists);
+        statusCode2.set("Overflow");
+        statusCode2.value.should.eql(StatusCodes.BadNodeIdExists + 0x80);
+        statusCode2.name.should.eql("BadNodeIdExists#Overflow");
+
+    });
+    it("should be possible to set the Overflow and SemanticChanged bits on a status code",function() {
+
+        var statusCode = StatusCodes.makeStatusCode(StatusCodes.BadNodeIdExists);
+        statusCode.set("Overflow | SemanticChanged");
+
+
+        statusCode.value.should.eql(StatusCodes.BadNodeIdExists.value + 0x80 + 0x4000);
+        statusCode.name.should.eql("BadNodeIdExists#SemanticChanged|Overflow");
+
+        statusCode.toString().should.eql("BadNodeIdExists#SemanticChanged|Overflow (0x805e4080)");
+    });
+
+
+    it("should be possible to encode and decode a statusCode that have a extra information bit",function() {
+
+        var statusCode = StatusCodes.makeStatusCode(StatusCodes.BadNodeIdExists);
+        statusCode.set("Overflow | SemanticChanged");
+
+        var stream = new BinaryStream(8);
+        encodeStatusCode(statusCode, stream);
+        stream.rewind();
+        var statusCode2 = decodeStatusCode(stream);
+        statusCode2.should.eql(statusCode);
+
+        statusCode2.hasOverflowBit.should.equal(true);
+        statusCode2.hasSemanticChangedBit.should.equal(true);
+        statusCode2.hasStructureChangedBit.should.equal(false);
+        statusCode2.should.be.instanceOf(StatusCode);
+
+    });
+
+    it("should be possible to create a modifiable StatusCode from a ModifiableStatusCode",function() {
+
+        var statusCode = StatusCodes.makeStatusCode(StatusCodes.BadNodeIdExists);
+        statusCode.set("Overflow");
+        statusCode.hasOverflowBit.should.equal(true);
+        statusCode.hasSemanticChangedBit.should.equal(false);
+
+        var statusCode2 = StatusCodes.makeStatusCode(statusCode);
+        statusCode2.hasOverflowBit.should.equal(true);
+        statusCode2.hasSemanticChangedBit.should.equal(false);
+        statusCode2.set("SemanticChanged");
+        statusCode2.hasOverflowBit.should.equal(true);
+        statusCode2.hasSemanticChangedBit.should.equal(true);
+
+        statusCode.hasOverflowBit.should.equal(true);
+        statusCode.hasSemanticChangedBit.should.equal(false);
+
+    });
+
+    it("should fail to set a extra information bit on a standard StatusCode",function() {
+        should(function() {
+
+            var statusCode =StatusCodes.Good;
+
+            statusCode.set("Overflow"); // << set is not defined !!!
+
+        }).throwError();
+    })
 
 });
