2b3bf22126585a48d9f9687e14de5d3caf29d4c0
Etienne Rossignon <etienne.rossignon@gadz.org>
2016-11-06 22:23:48 +0100

    expose encode_decode in index

--- index.js
+++ index.js
@@ -118,6 +118,7 @@ module.exports.DataTypeIds = require("lib/opcua_node_ids").DataTypeIds;
 // filtering tools
 module.exports.constructEventFilter = require("lib/tools/tools_event_filter").constructEventFilter;
 module.exports.checkSelectClause = require("lib/tools/tools_event_filter").checkSelectClause;
-
+module.exports.buildVariantArray = require("lib/datamodel/variant_tools").buildVariantArray;
+module.exports.encode_decode = require("lib/misc/encode_decode");
 module.exports.Enum = require("lib/misc/enum");
 module.exports.factories = require("lib/misc/factories");
