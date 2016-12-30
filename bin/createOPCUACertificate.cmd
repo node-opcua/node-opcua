20f59612c956a08898e6607979df42ce936be7a0
Etienne Rossignon <etienne.rossignon@gadz.org>
2016-12-30 12:24:05 +0100

    fix certificate utility

--- bin/createOPCUACertificate.cmd
+++ bin/createOPCUACertificate.cmd
@@ -1,6 +1,6 @@
 
 SET BASE=%~dp0%
-echo applicationUri %1%
-echo output         %2%
+echo applicationUri %1
+echo output         %2
 
-node %BASE%\crypto_create_CA.js --new --applicationUri %1% --prefix %2%
+node %BASE%\crypto_create_CA.js certificate --selfSigned --applicationUri %1 -o %2
