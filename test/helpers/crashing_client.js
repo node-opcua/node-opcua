0f726e559fc6bfb7b7973dbe490cea8f9368b7cd
Etienne Rossignon <etienne.rossignon@gadz.org>
2017-01-26 08:26:48 +0100

    reduce test verbosity

--- test/helpers/crashing_client.js
+++ test/helpers/crashing_client.js
@@ -1,72 +1,76 @@
 require("babel-register");
 
-// a simple client that is design to crash in the middle of a connection
-// once a item has been monitored
 
-if (process.argv.length != 3) {
-    // throw new Error('Improper Args length')
-}
-console.log("process.argv.length ",process.argv.length);
+function main() {
 
-/*global require,console,setTimeout */
-var opcua = require("../../"); // node-opcua
-var async = require("async");
-
-
-var port = process.argv[2];
-
-
-var endpointUrl = "opc.tcp://" + require("os").hostname() + ":" + port;
-console.log("endpointUrl = ",endpointUrl);
-
-var options = {
-    requestedSessionTimeout: 101, // very short
-    keepSessionAlive: true,
-    connectionStrategy: {
-        maxRetry: 0 // << NO RETRY !
+    if (process.argv.length != 3) {
+        console.log("Invalid number of   Argument ", process.argv);
+        return;
+        throw new Error('Improper Args length')
     }
-};
+    console.log("process.argv.length ", process.argv.length);
 
-var client = new opcua.OPCUAClient(options);
+    /*global require,console,setTimeout */
+    var opcua = require("../../"); // node-opcua
+    var async = require("async");
 
-var the_session;
 
-async.series([
+    var port = process.argv[2];
 
-        // step 1 : connect to
-        function(callback)  {
-            client.connect(endpointUrl,function (err) {
-                if(err) {
-                    console.log(" cannot connect to endpoint :" , endpointUrl );
-                } else {
-                    console.log("connected !");
-                }
-                callback(err);
-            });
-        },
 
-        // step 2 : createSession
-        function(callback) {
-            client.createSession( function(err,session) {
-                if(!err) {
-                    the_session = session;
-                }
-                callback(err);
-            });
-        },
+    var endpointUrl = "opc.tcp://" + require("os").hostname() + ":" + port;
+    console.log("endpointUrl = ", endpointUrl);
 
-        // step 3 : browse
-        function(callback) {
-            setTimeout(function(){
-                console.log(" CRASHING !!!!");
-                process.exit(-1);
-            },3000);
-        },
-    ],
-    function(err) {
-        if (err) {
-            console.log(" failure ",err);
-        } else {
-            console.log("done!");
+    var options = {
+        requestedSessionTimeout: 101, // very short
+        keepSessionAlive: true,
+        connectionStrategy: {
+            maxRetry: 0 // << NO RETRY !
         }
-    }) ;
+    };
+
+    var client = new opcua.OPCUAClient(options);
+
+    var the_session;
+
+    async.series([
+
+            // step 1 : connect to
+            function (callback) {
+                client.connect(endpointUrl, function (err) {
+                    if (err) {
+                        console.log(" cannot connect to endpoint :", endpointUrl);
+                    } else {
+                        console.log("connected !");
+                    }
+                    callback(err);
+                });
+            },
+
+            // step 2 : createSession
+            function (callback) {
+                client.createSession(function (err, session) {
+                    if (!err) {
+                        the_session = session;
+                    }
+                    callback(err);
+                });
+            },
+
+            // step 3 : browse
+            function (callback) {
+                setTimeout(function () {
+                    console.log(" CRASHING !!!!");
+                    process.exit(-1);
+                }, 3000);
+            },
+        ],
+        function (err) {
+            if (err) {
+                console.log(" failure ", err);
+            } else {
+                console.log("done!");
+            }
+        });
+}
+main();
