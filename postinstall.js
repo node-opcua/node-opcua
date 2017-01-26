f908a37efa752eee06656f3c72cba3d22c76d7e1
Etienne Rossignon <etienne.rossignon@gadz.org>
2017-01-26 09:16:50 +0100

    fix scripts in package.json

--- postinstall.js
+++ postinstall.js
@@ -1,24 +1,30 @@
-var child_process = require("child_process");
+const child_process = require("child_process");
 
 
+let the_code = 61;
 function exec(cmd,callback) {
 
     console.log("Executing " ,cmd);
-    var the_code = 61;
-    var child = child_process.exec(cmd,function(err) {
-    });
-
+    const child = child_process.exec(cmd,function(err) {});
     child.stdout.pipe(process.stdout);
-
     child.on('close', function(code) {
         the_code = code;
         console.log("done ... (" + the_code + ")");
-        callback();
+        callback(code);
     });
 }
+const node = "babel-node ";
 
-//exec("babel-node bin/generate_opcua_classes.js --clear ",function() {
-    exec( "../node_modules/babel-cli/bin/babel-node bin/crypto_create_CA.js demo",function(result) {
-        console.log('certifictes generated', result);
+exec( node + " bin/generate_opcua_classes.js --clear ",function(result) {
+    if(result) {
+        console.log(" Generate OPC CLASSES has failed with error: ",result)
+    }
+    exec( node + " bin/crypto_create_CA.js demo",function(result) {
+
+        if(result) {
+            console.log(" Certificate generation has failed with error: ",result)
+        } else {
+            console.log('demo certificates generated !');
+        }
     });
-//});
+});
