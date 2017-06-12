0fde6bbcb5f6b15151023fb38f005e7a00ec864c
Etienne Rossignon <etienne.rossignon@gadz.org>
2017-06-12 21:51:17 +0200

    fixes issue #355 : exception raised in ClientMonitoredItem onChange handler causes confusion to publish engine

--- test/end_to_end/test_e2e_umbrella.js
+++ test/end_to_end/test_e2e_umbrella.js
@@ -245,6 +245,6 @@ describe("testing Client - Umbrella ", function () {
     require("./u_test_e2e_issue_233")(test);
     require("./u_test_e2e_issue_273")(test);
     require("./u_test_e2e_issue_313")(test);
+    require("./u_test_e2e_issue_355")(test);
     require("./alarms_and_conditions/u_test_e2e_conditions")(test);
 });
-
