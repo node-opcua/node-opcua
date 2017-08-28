f2b3f9edcaee9ca116a62149bc052ea6d9157e21
Etienne Rossignon <etienne.rossignon@gadz.org>
2017-08-28 08:07:21 +0200

    create test temporary folder if it doesn't exist yet

--- packages/node-opcua-debug/src/get_temp_filename.js
+++ packages/node-opcua-debug/src/get_temp_filename.js
@@ -1,10 +1,14 @@
 "use strict";
 
 var path = require("path");
+var fs = require("fs");
 function getTempFilename(tmpfile) {
     tmpfile = tmpfile || "";
-    return path.normalize(path.join(__dirname,'../../../../tmp/',tmpfile));
-
+    var temporaryFolder = path.join(__dirname,'../../../../tmp/');
+    if (!fs.existsSync(temporaryFolder)) {
+        fs.mkdirSync(temporaryFolder);
+    }
+    return path.normalize(path.join(temporaryFolder,tmpfile));
 }
 exports.getTempFilename = getTempFilename;
 
