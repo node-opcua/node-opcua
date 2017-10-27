78792561893fae27d9e5f9bc903a58058c9b7c47
Josh Mcguigan <joshmcg88@gmail.com>
2017-10-26 19:57:55 -0700

    fixed #386 client crash on terminating monitored item before initialization

--- packages/node-opcua-client/src/client_subscription.js
+++ packages/node-opcua-client/src/client_subscription.js
@@ -646,9 +646,17 @@ ClientSubscription.prototype._remove = function (monitoredItem) {
     var self = this;
     var clientHandle = monitoredItem.monitoringParameters.clientHandle;
     assert(clientHandle);
-    assert(self.monitoredItems.hasOwnProperty(clientHandle));
-    monitoredItem.removeAllListeners();
-    delete self.monitoredItems[clientHandle];
+
+    var deleteMonitoredItem = function(){
+        monitoredItem.removeAllListeners();
+        delete self.monitoredItems[clientHandle];
+    };
+
+    if(self.monitoredItems.hasOwnProperty(clientHandle)){
+        deleteMonitoredItem();
+    } else {
+        monitoredItem.on("initialized", deleteMonitoredItem);
+    }
 };
 
 ClientSubscription.prototype._delete_monitored_items = function (monitoredItems, callback) {
