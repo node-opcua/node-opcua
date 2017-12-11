c99a18e0dc2009c18ff289ff37754c798c2d190a
marcelwinh <marcelwinh@users.noreply.github.com>
2017-12-11 10:44:34 +0100

    Update qualified_name.js

--- packages/node-opcua-data-model/src/qualified_name.js
+++ packages/node-opcua-data-model/src/qualified_name.js
@@ -35,7 +35,7 @@ function stringToQualifiedName(value) {
 
     var split_array = value.split(":");
     var namespaceIndex = 0;
-    if (!isNaN(parseFloat(split_array[0])) && isFinite(split_array[0]) && Number.isInteger(parseFloat(split_array[0]))) {
+    if (!isNaN(parseFloat(split_array[0])) && isFinite(split_array[0]) && Number.isInteger(parseFloat(split_array[0])) && split_array.length > 1) {
         namespaceIndex = parseInt(split_array[0]);
         split_array.shift();
         value = split_array.join(':');
