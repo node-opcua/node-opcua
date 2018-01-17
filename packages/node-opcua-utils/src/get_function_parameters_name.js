40dab71a8a2a30ce602dce120e9011f66e27c2f8
Etienne Rossignon <etienne.rossignon@gadz.org>
2018-01-17 23:25:15 +0100

    add getFunctionParameters utility method

---
+++ packages/node-opcua-utils/src/get_function_parameters_name.js
@@ -0,0 +1,15 @@
+
+"use strict";
+
+var STRIP_COMMENTS = /((\/\/.*$)|(\/\*[\s\S]*?\*\/))/mg;
+var ARGUMENT_NAMES = /([^\s,]+)/g;
+function getFunctionParameterNames(func) {
+
+    var fnStr = func.toString().replace(STRIP_COMMENTS, '');
+    var result = fnStr.slice(fnStr.indexOf('(')+1, fnStr.indexOf(')')).match(ARGUMENT_NAMES);
+    if(result === null)
+        result = [];
+    return result;
+}
+exports.getFunctionParameterNames = getFunctionParameterNames;
+
---
+++ packages/node-opcua-utils/test/test_getFunctionParameterNames.js
@@ -0,0 +1,10 @@
+var getFunctionParameterNames = require("../src/get_function_parameters_name").getFunctionParameterNames;
+
+describe("testing getFunctionParameterNames",function() {
+    it("#getFunctionParameterNames",function() {
+        getFunctionParameterNames(getFunctionParameterNames).should.eql(['func']);
+        getFunctionParameterNames(function (a,b,c,d){}).should.eql(['a','b','c','d']);
+        getFunctionParameterNames(function (a,/*b,c,*/d){}).should.eql(['a','d']);
+        getFunctionParameterNames(function (){}) .should.eql([]);
+    });
+});
