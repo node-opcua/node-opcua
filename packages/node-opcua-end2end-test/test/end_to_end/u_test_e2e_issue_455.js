18886c8a241c832ff5ad01dc576958c9afc4d019
Etienne Rossignon <etienne.rossignon@gadz.org>
2018-03-13 22:20:04 +0100

    fixes #455 : provide a example

---
+++ packages/node-opcua-end2end-test/test/end_to_end/u_test_e2e_issue_455.js
@@ -0,0 +1,64 @@
+/*global describe, it, require*/
+var async = require("async");
+var should = require("should");
+var opcua = require("node-opcua");
+var OPCUAClient = opcua.OPCUAClient;
+var perform_operation_on_client_session = require("../../test_helpers/perform_operation_on_client_session").perform_operation_on_client_session;
+
+module.exports = function (test) {
+
+    describe("Testing bug #455 - OPCUASession#readVariableValue", function () {
+
+        it("should detect badly formed nodeId on the client level and raise an exception",function(done) {
+
+
+            var client = new OPCUAClient({});
+            var endpointUrl = test.endpointUrl;
+
+            perform_operation_on_client_session(client, endpointUrl, function (session, inner_done) {
+
+                async.series([
+
+                    function (callback) {
+
+                        // in ths test we pass a INVALID nodeID (a.k.a a string that cannot be coerced to a NodeId)
+                        // therefore this code will cause readVariableValue to raise a exception, before any
+                        // transaction is sent to the server.
+                        // The exception is the expected behavior
+                        try {
+                            session.readVariableValue("ns=2;i=INVALID_NODE_ID_THAT_SHOULD_CAUSE_EXCEPTION_IN_CLIENT", (err, dataValue) => {
+                                callback(new Error("Should Not Get Here"));
+                            });
+                        }
+                        catch(err) {
+                            callback();
+                        }
+                    },
+
+                    function (callback) {
+                        // in ths test we pass a VALID nodeID (a.k.a a string that can be coerced to a NodeId)
+                        // but this node doesn't exists in the address space of the serve.
+                        // therefore  readVariableValue will be executed, but the eerver will return a
+                        // a dataValue with no Value and a StatusCode=BadNodeIdUnknown.
+                        session.readVariableValue("ns=2;i=10000", (err, dataValue) => {
+                            dataValue.statusCode.should.eql(opcua.StatusCodes.BadNodeIdUnknown);
+                            callback();
+                        });
+                    },
+                    function (callback) {
+                        // same behavior applies on standard read transaction
+                        session.read({ nodeId: "ns=2;i=10000",attributeId: opcua.AttributeIds.Value}, (err, dataValue) => {
+                            dataValue.statusCode.should.eql(opcua.StatusCodes.BadNodeIdUnknown);
+                            callback();
+                        });
+                    },
+
+                ],inner_done)
+
+            }, done);
+        });
+
+    });
+
+};
+
