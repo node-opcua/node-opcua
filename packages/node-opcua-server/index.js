9ec499fa38fb124658e433e4b106e3380585b31b
Etienne Rossignon <etienne.rossignon@gadz.org>
2018-03-24 17:35:14 +0100

    expose server Subscription

--- packages/node-opcua-server/index.js
+++ packages/node-opcua-server/index.js
@@ -2,6 +2,7 @@ module.exports = {
     OPCUABaseServer: require("./src/base_server").OPCUABaseServer,
     OPCUAServer: require("./src/opcua_server").OPCUAServer,
     OPCUAServerEndPoint: require("./src/server_end_point").OPCUAServerEndPoint,
+    Subscription: require("./src/subscription").Subscription,
     MonitoredItem: require("./src/monitored_item").MonitoredItem,
     ServerEngine: require("./src/server_engine").ServerEngine,
     ServerSession: require("./src/server_session").ServerSession,
--- packages/node-opcua/index.js
+++ packages/node-opcua/index.js
@@ -123,10 +123,12 @@ module.exports.parseEndpointUrl = require("node-opcua-transport").parseEndpointU
 //----------------------------------------------------------------------------------------------------------------------
 // Server services
 //----------------------------------------------------------------------------------------------------------------------
-module.exports.OPCUAServer          = require("node-opcua-server").OPCUAServer;
-module.exports.ServerEngine = require("node-opcua-server").ServerEngine;
-module.exports.MonitoredItem = require("node-opcua-server").MonitoredItem;
-module.exports.ServerSession = require("node-opcua-server").ServerSession;
+module.exports.OPCUAServer    = require("node-opcua-server").OPCUAServer;
+module.exports.ServerEngine   = require("node-opcua-server").ServerEngine;
+module.exports.MonitoredItem  = require("node-opcua-server").MonitoredItem;
+module.exports.ServerSession  = require("node-opcua-server").ServerSession;
+module.exports.Subscription   = require("node-opcua-server").Subscription;
+module.exports.MonitoredItem  = require("node-opcua-server").MonitoredItem;
 
 
 module.exports.generate_address_space = require("node-opcua-address-space").generate_address_space;
