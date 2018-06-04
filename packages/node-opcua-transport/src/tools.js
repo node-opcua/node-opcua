e93ee1f633eda679c290ea69623e5ad78119feea
Sascha Englert <englert@hmi-logic.com>
2018-06-04 10:21:57 +0200

    Reworked regex to require port number and added additional tests

--- packages/node-opcua-transport/src/tools.js
+++ packages/node-opcua-transport/src/tools.js
@@ -4,22 +4,25 @@ const assert = require("node-opcua-assert").assert;
 const BinaryStream = require("node-opcua-binary-stream").BinaryStream;
 const createFastUninitializedBuffer = require("node-opcua-buffer-utils").createFastUninitializedBuffer;
 
-
 const TCPErrorMessage = require("../_generated_/_auto_generated_TCPErrorMessage").TCPErrorMessage;
 const readMessageHeader = require("node-opcua-chunkmanager").readMessageHeader;
 
-
-
 function is_valid_msg_type(msgType) {
-    assert(["HEL", "ACK", "ERR",   // Connection Layer
-        "OPN", "MSG", "CLO"    // OPC Unified Architecture, Part 6 page 36
-    ].indexOf(msgType) >= 0, "invalid message type  " + msgType);
+    assert(
+        [
+            "HEL",
+            "ACK",
+            "ERR", // Connection Layer
+            "OPN",
+            "MSG",
+            "CLO" // OPC Unified Architecture, Part 6 page 36
+        ].indexOf(msgType) >= 0,
+        "invalid message type  " + msgType
+    );
     return true;
 }
 
-
 function decodeMessage(stream, ClassName) {
-
     assert(stream instanceof BinaryStream);
     assert(ClassName instanceof Function, " expecting a function for " + ClassName);
 
@@ -39,10 +42,7 @@ function decodeMessage(stream, ClassName) {
     }
 }
 
-
-
 function packTcpMessage(msgType, encodableObject) {
-
     assert(is_valid_msg_type(msgType));
 
     const messageChunk = createFastUninitializedBuffer(encodableObject.binaryStoreSize() + 8);
@@ -51,18 +51,16 @@ function packTcpMessage(msgType, encodableObject) {
     encodeMessage(msgType, encodableObject, stream);
 
     return messageChunk;
-
 }
 
 // opc.tcp://xleuri11022:51210/UA/SampleServer
 function parseEndpointUrl(endpointUrl) {
-
-    const r = /^([a-z.]*):\/\/([a-zA-Z_\-.\-0-9]*):([0-9]*)(\/.*){0,1}/;
+    const r = /^([a-z.]*):\/\/([a-zA-Z_\-.\-0-9]*)(:[0-9]{1,5})(\/.*){0,1}/;
 
     const matches = r.exec(endpointUrl);
 
-    if(!matches) {
-        throw new Error("Invalid endpoint url ",endpointUrl);
+    if (!matches) {
+        throw new Error("Invalid endpoint url ", endpointUrl);
     }
     return {
         protocol: matches[1],
@@ -76,7 +74,6 @@ function is_valid_endpointUrl(endpointUrl) {
     return e.hasOwnProperty("hostname");
 }
 
-
 /**
  * @method encodeMessage
  * @type {{
@@ -87,7 +84,6 @@ function is_valid_endpointUrl(endpointUrl) {
  */
 
 function writeTCPMessageHeader(msgType, chunkType, total_length, stream) {
-
     if (stream instanceof Buffer) {
         stream = new BinaryStream(stream);
     }
@@ -104,7 +100,6 @@ function writeTCPMessageHeader(msgType, chunkType, total_length, stream) {
 }
 
 function encodeMessage(msgType, messageContent, stream) {
-
     //the length of the message, in bytes. (includes the 8 bytes of the message header)
     const total_length = messageContent.binaryStoreSize() + 8;
 
@@ -119,4 +114,3 @@ exports.decodeMessage = decodeMessage;
 exports.packTcpMessage = packTcpMessage;
 exports.parseEndpointUrl = parseEndpointUrl;
 exports.is_valid_endpointUrl = is_valid_endpointUrl;
-
--- packages/node-opcua-transport/test/test_parseEndpointUrl.js
+++ packages/node-opcua-transport/test/test_parseEndpointUrl.js
@@ -1,16 +1,9 @@
-
-
-
 const should = require("should");
 
 const parseEndpointUrl = require("../src/tools").parseEndpointUrl;
 
-
-describe("testing parseEndpointUrl", function () {
-
-
-    it("should parse a endpoint ", function () {
-
+describe("testing parseEndpointUrl", function() {
+    it("should parse a endpoint ", function() {
         const ep = parseEndpointUrl("opc.tcp://abcd1234:51210/UA/SampleServer");
 
         ep.protocol.should.equal("opc.tcp");
@@ -19,8 +12,7 @@ describe("testing parseEndpointUrl", function () {
         ep.address.should.equal("/UA/SampleServer");
     });
 
-    it("should parse this endpoint as well", function () {
-
+    it("should parse this endpoint as well", function() {
         const ep = parseEndpointUrl("opc.tcp://ABCD12354:51210/UA/SampleServer");
 
         ep.protocol.should.equal("opc.tcp");
@@ -29,8 +21,7 @@ describe("testing parseEndpointUrl", function () {
         ep.address.should.equal("/UA/SampleServer");
     });
 
-    it("should parse this endpoint as well", function () {
-
+    it("should parse this endpoint as well", function() {
         const ep = parseEndpointUrl("opc.tcp://portable-Precision-M4500:4841");
 
         ep.protocol.should.equal("opc.tcp");
@@ -39,12 +30,33 @@ describe("testing parseEndpointUrl", function () {
         ep.address.should.equal("");
     });
 
-    it("should raise an exception if Endpoint URL is malformed",function() {
-
+    it("should raise an exception if Endpoint URL is malformed", function() {
         should(function() {
             const ep = parseEndpointUrl("foo@baz.bar://mymachine:4841");
         }).throwError();
     });
 
+    it("should raise an exception if Endpoint URL is missing a port", function() {
+        should(function() {
+            const ep = parseEndpointUrl("opc.tcp://192.168.178.142/UA/myLittleServer");
+        }).throwError();
+    });
 
+    it("should raise an exception if Endpoint URL is missing a port number", function() {
+        should(function() {
+            const ep = parseEndpointUrl("https://192.168.178.142:");
+        }).throwError();
+    });
+
+    it("should raise an exception if Endpoint URL Port is malformed", function() {
+        should(function() {
+            const ep = parseEndpointUrl("https://192.168.178.142:87ad4");
+        }).throwError();
+    });
+
+    it("should raise an exception if Endpoint URL port is out of bounds", function() {
+        should(function() {
+            const ep = parseEndpointUrl("https://192.168.178.142:123456");
+        }).throwError();
+    });
 });
