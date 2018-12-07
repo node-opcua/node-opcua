66cef023ef45581019334eb72e9bd88a3d9656e3
Etienne Rossignon <etienne.rossignon@gadz.org>
2018-12-07 21:52:09 +0100

    backport change

--- packages/node-opcua-debug/source/display_trace.ts
+++ packages/node-opcua-debug/source/display_trace.ts
@@ -1,13 +1,13 @@
 import chalk from "chalk";
 
-export function trace_from_this_projet_only(err: Error): string {
+export function trace_from_this_projet_only(err?: Error): string {
 
     const str = [];
     str.push(chalk.cyan.bold(" display_trace_from_this_project_only = "));
     if (err) {
         str.push(err.message);
     }
-    err = err || new Error();
+    err = err || new Error("Error used to extract stack trace");
     let stack: any = err.stack;
     if (stack) {
         stack = stack.split("\n").filter((el:string) => el.match(/node-opcua/) && !el.match(/node_modules/));
@@ -19,6 +19,6 @@ export function trace_from_this_projet_only(err: Error): string {
 }
 
 
-export function display_trace_from_this_projet_only(err: Error): void {
+export function display_trace_from_this_projet_only(err?: Error): void {
     console.log(trace_from_this_projet_only(err));
 }
