f5ca9b0dace77264d3915d385936d9da3b69866b
Etienne Rossignon <etienne.rossignon@gadz.org>
2019-05-12 09:57:35 +0200

    replace var with const/let

--- packages/node-opcua-test-fixtures/fixture_OpenSecureChannelRequest.js
+++ packages/node-opcua-test-fixtures/fixture_OpenSecureChannelRequest.js
@@ -1,20 +1,20 @@
 "use strict";
 
-var secure_channel_service = require("$node-opcua/services/secure_channel_service");
-var OpenSecureChannelRequest = secure_channel_service.OpenSecureChannelRequest;
+const secure_channel_service = require("$node-opcua/services/secure_channel_service");
+const OpenSecureChannelRequest = secure_channel_service.OpenSecureChannelRequest;
 require("should");
 
 
 exports.fixture1 = (function () {
     // empty  GetEndpointsResponse
 
-    var zeroNonce = Buffer.alloc(1);
+    const zeroNonce = Buffer.alloc(1);
     zeroNonce.writeUInt8(0, 0);
 
     return new OpenSecureChannelRequest({
         clientProtocolVersion: 0,
-        requestType: s.SecurityTokenRequestType.Issue,
-        securityMode: s.MessageSecurityMode.None,
+        requestType: secure_channel_service.SecurityTokenRequestType.Issue,
+        securityMode: secure_channel_service.MessageSecurityMode.None,
         clientNonce: zeroNonce,
         requestedLifetime: 300000
     });
