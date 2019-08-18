fae457c8cb9a794cbdc35bfba80aa1dab2ce1134
Etienne Rossignon <etienne.rossignon@gadz.org>
2019-08-18 21:14:18 +0200

    Discovery Server now automatically accept new server certificates

--- packages/node-opcua-local-discovery-server/bin/local-discovery-server.js
+++ packages/node-opcua-local-discovery-server/bin/local-discovery-server.js
@@ -5,59 +5,79 @@ const path = require("path");
 const Vorpal = require("vorpal");
 const vorpal_repl = require("vorpal-repl");
 
-const server_certificate_file = path.join(__dirname,"../certificates/discoveryServer_selfsigned_cert_2048.pem");
-const server_certificate_privatekey_file  = path.join(__dirname,"../certificates/discoveryServer_key_2048.pem");
 
-const discoveryServer = new opcua.OPCUADiscoveryServer({
-    // register
-    // port: 4840,
-    certificateFile: server_certificate_file,
-    privateKeyFile: server_certificate_privatekey_file,
-
+const certificateManager = new opcua.OPCUACertificateManager({
+    automaticallyAcceptUnknownCertificate: true,
 });
-discoveryServer.start(function(err) {
 
-    if (err) {
-        console.log("Error , cannot start LDS ", err.message);
-        console.log("Make sure that a LocalDiscoveryServer is not already running");
-        return;
-
-    }
-    console.log("discovery server started on port ",discoveryServer.endpoints[0].port);
-    console.log("CTRL+C to stop");
+const server_certificate_file = path.join(__dirname, "../certificates/discoveryServer_selfsigned_cert_2048.pem");
+const server_certificate_privatekey_file = path.join(__dirname, "../certificates/discoveryServer_key_2048.pem");
 
 
-    const vorpal = new Vorpal();
-    vorpal
-        .command("info")
-        .description("display list of registered servers.")
-        .action(function(args, callback) {
+(async () => {
+    try {
 
-            this.log(discoveryServer.serverInfo);
-            // xx this.log(discoveryServer.endpoints[0]);
+        const discoveryServer = new opcua.OPCUADiscoveryServer({
+            // register
+            // port: 4840,
+            certificateFile: server_certificate_file,
+            privateKeyFile: server_certificate_privatekey_file,
+            //
+            serverCertificateManager: certificateManager
 
-            {
-                const servers = Object.keys(discoveryServer.registeredServers);
-                this.log("number of registered servers : ", servers.length);
-
-                for (const serverKey of servers) {
-                    const server = discoveryServer.registeredServers[serverKey];
-                    this.log("key =", serverKey);
-                    this.log(server.toString());
-                }
-            }
-            {
-                const server2 = Object.keys(discoveryServer.mDnsResponder.registeredServers);
-                this.log("number of mNDS registered servers : ", server2.length);
-                for (const serverKey of server2) {
-                    const server = discoveryServer.mDnsResponder.registeredServers[serverKey];
-                    this.log("key =", serverKey);
-                    this.log(server.toString());
-                }
-            }
-
-            callback();
         });
+
+        try {
+            await discoveryServer.start();
+        } catch (err) {
+            console.log("Error , cannot start LDS ", err.message);
+            console.log("Make sure that a LocalDiscoveryServer is not already running");
+            return;
+        }
+
+        console.log("discovery server started on port ", discoveryServer.endpoints[0].port);
+        console.log("CTRL+C to stop");
+        console.log("rejected Folder ",discoveryServer.serverCertificateManager.rejectedFolder);
+        console.log("trusted  Folder ",discoveryServer.serverCertificateManager.trustedFolder);
+
+
+        const vorpal = new Vorpal();
+        vorpal
+            .command("info")
+            .description("display list of registered servers.")
+            .action(function (args, callback) {
+
+                this.log(discoveryServer.serverInfo);
+                // xx this.log(discoveryServer.endpoints[0]);
+
+                {
+                    const servers = Object.keys(discoveryServer.registeredServers);
+                    this.log("number of registered servers : ", servers.length);
+
+                    for (const serverKey of servers) {
+                        const server = discoveryServer.registeredServers[serverKey];
+                        this.log("key =", serverKey);
+                        this.log(server.toString());
+                    }
+                }
+                {
+                    const server2 = Object.keys(discoveryServer.mDnsResponder.registeredServers);
+                    this.log("number of mNDS registered servers : ", server2.length);
+                    for (const serverKey of server2) {
+                        const server = discoveryServer.mDnsResponder.registeredServers[serverKey];
+                        this.log("key =", serverKey);
+                        this.log(server.toString());
+                    }
+                }
+
+                callback();
+            });
         vorpal.delimiter("local-discovery-server$").use(vorpal_repl).show();
 
-});
+    }
+    catch (err) {
+        console.log(err.message);
+        console.log(err);
+    }
+})();
+
