60a51872893574cf97b911d4fd1fa0ec5b96b962
Etienne Rossignon <etienne.rossignon@gadz.org>
2019-09-01 12:41:23 +0200

    imrpove doc

--- documentation/sample_server_with_historizing_variable.js
+++ documentation/sample_server_with_historizing_variable.js
@@ -4,6 +4,7 @@ const path = require("path");
 function construct_address_space(server) {
   const addressSpace = server.engine.addressSpace;
   const namespace = addressSpace.getOwnNamespace();
+ 
   const vessel = namespace.addObject({
       browseName: "Vessel",
       organizedBy: addressSpace.rootFolder.objects
@@ -18,7 +19,9 @@ function construct_address_space(server) {
       engineeringUnits: opcua.standardUnits.bar,
       componentOf: vessel
   });
+ 
   addressSpace.installHistoricalDataNode(vesselPressure);
+ 
   // simulate pressure change
   let t = 0;
   setInterval(function() {
@@ -32,24 +35,30 @@ function construct_address_space(server) {
 (async () => {
 
     try {
-                // Let's create an instance of OPCUAServer
-                const server = new opcua.OPCUAServer({
-                    port: 26543, // the port of the listening socket of the server
-                    resourcePath: "/UA/MyLittleServer", // this path will be added to the endpoint resource name
-                    nodeset_filename: [
-                        opcua.standard_nodeset_file,
-                    ]
-                });
+        // Let's create an instance of OPCUAServer
+        const server = new opcua.OPCUAServer({
+            port: 26543, // the port of the listening socket of the server
+            resourcePath: "/UA/MyLittleServer", // this path will be added to the endpoint resource name
+            nodeset_filename: [
+                opcua.nodesets.standard,
+            ]
+        });
+
+        await server.initialize();
+
+        console.log("certificateFile = ", server.certificateFile);
+        console.log("privateKeyFile  = ", server.privateKeyFile);
+        console.log("rejected folder = ",server.serverCertificateManager.rejectedFolder);
+        console.log("trusted  folder = ",server.serverCertificateManager.trustedFolder);
+
+        construct_address_space(server);
         
-                await server.initialize();
-        
-                construct_address_space(server);
-        
-                await server.start();
-                console.log("Server is now listening ... ( press CTRL+C to stop)");
-                console.log("port ", server.endpoints[0].port);
-                const endpointUrl = server.endpoints[0].endpointDescriptions()[0].endpointUrl;
-                console.log(" the primary server endpoint url is ", endpointUrl );
+        await server.start();
+        console.log("Server is now listening ... ( press CTRL+C to stop)");
+        console.log("port ", server.endpoints[0].port);
+        const endpointUrl = server.endpoints[0].endpointDescriptions()[0].endpointUrl;
+        console.log(" the primary server endpoint url is ", endpointUrl );
+
     } catch(err) {
         console.log("Error = ", error);
     }
