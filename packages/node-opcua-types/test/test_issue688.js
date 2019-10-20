084fafa6b9e392439ebbd8b5929ad71acdf3a6f5
Etienne Rossignon <etienne.rossignon@gadz.org>
2019-10-20 22:53:34 +0200

    issue #688 detect bad argument early

---
+++ packages/node-opcua-types/test/test_issue688.js
@@ -0,0 +1,21 @@
+
+const should =require("should");
+const { ReadValueId } = require("..");// node-opcua-types"
+
+describe("Issue 688", ()=> {
+
+    it("should throw an exception when arguement of ReadValueId are invalid", () => {
+        should(()=>{
+            const r = new ReadValueId({
+                nodeId: "someError" // << this invalid node id should cause a exception !
+            });    
+        }).throw("String cannot be coerced to a nodeId : someError");
+    });
+    it("should coerce a ReadValueId", () => {
+        should(() =>{
+            const r = new ReadValueId({
+                nodeId: "ns=1;s=OK" // << this  node id should cause a exception !
+            });    
+        }).not.throw();
+    });
+});
