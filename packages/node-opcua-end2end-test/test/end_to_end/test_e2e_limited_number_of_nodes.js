838ef7a50eaadd75beae6e524dbbb377169227fa
Etienne Rossignon <etienne.rossignon@gadz.org>
2020-06-19 11:15:14 +0200

    refactor NodeCrawler, introduce NodeCrawlerBase

--- packages/node-opcua-end2end-test/test/end_to_end/test_e2e_limited_number_of_nodes.js
+++ packages/node-opcua-end2end-test/test/end_to_end/test_e2e_limited_number_of_nodes.js
@@ -1,16 +1,20 @@
 
 
 const should = require("should");
-const assert = require("node-opcua-assert").assert;
 const async = require("async");
 const _ = require("underscore");
 
 const opcua = require("node-opcua");
 
-const OPCUAServer = opcua.OPCUAServer;
-const OPCUAClient = opcua.OPCUAClient;
-const BrowseDirection = opcua.BrowseDirection;
-const makeBrowsePath = opcua.makeBrowsePath;
+const {
+    makeNodeId, VariableIds, AttributeIds,
+    NodeCrawler,
+    assert,
+    OPCUAServer,
+    OPCUAClient,
+    BrowseDirection,
+    makeBrowsePath,
+} = require("node-opcua");
 
 assert(_.isFunction(makeBrowsePath));
 
@@ -21,7 +25,7 @@ const perform_operation_on_client_session = require("../../test_helpers/perform_
 
 const describe = require("node-opcua-leak-detector").describeWithLeakDetector;
 
-describe("testing server with low maxNodesPerRead and maxNodesPerBrowse", function () {
+describe("testing server with low maxNodesPerRead and maxNodesPerBrowse", function() {
 
 
     this.timeout(Math.max(this._timeout, 10000));
@@ -32,7 +36,7 @@ describe("testing server with low maxNodesPerRead and maxNodesPerBrowse", functi
 
 
     let endpointUrl;
-    before(function (done) {
+    before(function(done) {
 
         server = new OPCUAServer({
             port: port,
@@ -64,23 +68,23 @@ describe("testing server with low maxNodesPerRead and maxNodesPerBrowse", functi
             }
         });
         client = OPCUAClient.create();
-        server.start(function () {
+        server.start(function() {
             endpointUrl = server.endpoints[0].endpointDescriptions()[0].endpointUrl;
             done();
         });
 
     });
 
-    after(function (done) {
+    after(function(done) {
 
         async.series([
-            function (callback) {
+            function(callback) {
                 client.disconnect(callback);
             },
-            function (callback) {
+            function(callback) {
                 server.shutdown(callback);
             },
-            function (callback) {
+            function(callback) {
                 OPCUAServer.registry.count().should.eql(0);
                 callback();
             }
@@ -88,7 +92,7 @@ describe("testing server with low maxNodesPerRead and maxNodesPerBrowse", functi
         ], done);
     });
 
-    it("should be possible to customize serverCapabilities.operationLimits at construction time", function () {
+    it("should be possible to customize serverCapabilities.operationLimits at construction time", function() {
         server.engine.serverCapabilities.operationLimits.maxNodesPerRead.should.eql(10);
         server.engine.serverCapabilities.operationLimits.maxNodesPerWrite.should.eql(10);
         server.engine.serverCapabilities.operationLimits.maxNodesPerMethodCall.should.eql(10);
@@ -105,21 +109,20 @@ describe("testing server with low maxNodesPerRead and maxNodesPerBrowse", functi
 
 
     });
+    it("server should provide OperationLimits_MaxNodesPerRead node ", function(done) {
 
-    it("server should provide OperationLimits_MaxNodesPerRead node ", function (done) {
-
-        perform_operation_on_client_session(client, endpointUrl, function (session, done) {
+        perform_operation_on_client_session(client, endpointUrl, function(session, done) {
 
             server.engine.serverCapabilities.operationLimits.maxNodesPerRead.should.eql(10);
 
-            const n1 = opcua.VariableIds.Server_ServerCapabilities_OperationLimits_MaxNodesPerRead;
-            const n2 = opcua.VariableIds.Server_ServerDiagnostics_ServerDiagnosticsSummary_CurrentSessionCount;
+            const n1 = VariableIds.Server_ServerCapabilities_OperationLimits_MaxNodesPerRead;
+            const n2 = VariableIds.Server_ServerDiagnostics_ServerDiagnosticsSummary_CurrentSessionCount;
 
-            const nodeId1 = opcua.makeNodeId(n1);
+            const nodeId1 = makeNodeId(n1);
 
-            const nodeToRead = { nodeId: nodeId1, attributeId: opcua.AttributeIds.Value };
+            const nodeToRead = { nodeId: nodeId1, attributeId: AttributeIds.Value };
 
-            session.read(nodeToRead, function (err, dataValue) {
+            session.read(nodeToRead, function(err, dataValue) {
                 // console.log(results);
                 dataValue.value.value.should.eql(server.engine.serverCapabilities.operationLimits.maxNodesPerRead);
                 done(err);
@@ -130,35 +133,35 @@ describe("testing server with low maxNodesPerRead and maxNodesPerBrowse", functi
 
     });
 
-    it("server should return BadTooManyOperations when nodesToRead exceed MaxNodesPerRead in read operation ", function (done) {
+    it("server should return BadTooManyOperations when nodesToRead exceed MaxNodesPerRead in read operation ", function(done) {
 
-        perform_operation_on_client_session(client, endpointUrl, function (session, done) {
+        perform_operation_on_client_session(client, endpointUrl, function(session, done) {
 
             server.engine.serverCapabilities.operationLimits.maxNodesPerRead.should.eql(10);
 
-            const n1 = opcua.VariableIds.Server_ServerCapabilities_OperationLimits_MaxNodesPerRead;
-            const n2 = opcua.VariableIds.Server_ServerDiagnostics_ServerDiagnosticsSummary_CurrentSessionCount;
+            const n1 = VariableIds.Server_ServerCapabilities_OperationLimits_MaxNodesPerRead;
+            const n2 = VariableIds.Server_ServerDiagnostics_ServerDiagnosticsSummary_CurrentSessionCount;
 
-            const nodeId1 = opcua.makeNodeId(n1);
-            const nodeId2 = opcua.makeNodeId(n2);
+            const nodeId1 = makeNodeId(n1);
+            const nodeId2 = makeNodeId(n2);
             console.log(nodeId1.toString());
             const nodesToRead = [
-                { nodeId: nodeId1, attributeId: opcua.AttributeIds.Value },
-                { nodeId: nodeId2, attributeId: opcua.AttributeIds.Value },
-                { nodeId: nodeId1, attributeId: opcua.AttributeIds.Value },
-                { nodeId: nodeId2, attributeId: opcua.AttributeIds.Value },
-                { nodeId: nodeId1, attributeId: opcua.AttributeIds.Value },
-                { nodeId: nodeId2, attributeId: opcua.AttributeIds.Value },
-                { nodeId: nodeId1, attributeId: opcua.AttributeIds.Value },
-                { nodeId: nodeId2, attributeId: opcua.AttributeIds.Value },
-                { nodeId: nodeId1, attributeId: opcua.AttributeIds.Value },
-                { nodeId: nodeId2, attributeId: opcua.AttributeIds.Value },
-                { nodeId: nodeId1, attributeId: opcua.AttributeIds.Value },
-                { nodeId: nodeId2, attributeId: opcua.AttributeIds.Value }
+                { nodeId: nodeId1, attributeId: AttributeIds.Value },
+                { nodeId: nodeId2, attributeId: AttributeIds.Value },
+                { nodeId: nodeId1, attributeId: AttributeIds.Value },
+                { nodeId: nodeId2, attributeId: AttributeIds.Value },
+                { nodeId: nodeId1, attributeId: AttributeIds.Value },
+                { nodeId: nodeId2, attributeId: AttributeIds.Value },
+                { nodeId: nodeId1, attributeId: AttributeIds.Value },
+                { nodeId: nodeId2, attributeId: AttributeIds.Value },
+                { nodeId: nodeId1, attributeId: AttributeIds.Value },
+                { nodeId: nodeId2, attributeId: AttributeIds.Value },
+                { nodeId: nodeId1, attributeId: AttributeIds.Value },
+                { nodeId: nodeId2, attributeId: AttributeIds.Value }
             ];
             nodesToRead.length.should.be.greaterThan(server.engine.serverCapabilities.operationLimits.maxNodesPerRead);
 
-            session.read(nodesToRead, function (err, dataValues) {
+            session.read(nodesToRead, function(err, dataValues) {
                 should.exist(err);
                 err.message.should.match(/BadTooManyOperations/);
                 done();
@@ -169,11 +172,11 @@ describe("testing server with low maxNodesPerRead and maxNodesPerBrowse", functi
 
     });
 
-    it("server should return BadTooManyOperations when browseRequest exceed MaxNodesPerBrowse in browse operation ", function (done) {
+    it("server should return BadTooManyOperations when browseRequest exceed MaxNodesPerBrowse in browse operation ", function(done) {
 
         server.engine.serverCapabilities.operationLimits.maxNodesPerBrowse.should.equal(2);
 
-        perform_operation_on_client_session(client, endpointUrl, function (session, done) {
+        perform_operation_on_client_session(client, endpointUrl, function(session, done) {
             const browseRequest = [];
 
             const bad_referenceid_node = "ns=3;i=3500";
@@ -190,7 +193,7 @@ describe("testing server with low maxNodesPerRead and maxNodesPerBrowse", functi
 
             browseRequest.length.should.be.greaterThan(server.engine.serverCapabilities.operationLimits.maxNodesPerBrowse);
 
-            session.browse(browseRequest, function (err, browseResults) {
+            session.browse(browseRequest, function(err, browseResults) {
                 should.exist(err);
                 err.message.should.match(/BadTooManyOperations/);
                 done();
@@ -198,11 +201,11 @@ describe("testing server with low maxNodesPerRead and maxNodesPerBrowse", functi
         }, done);
 
     });
-    it("server should return BadTooManyOperations when translate exceed maxNodesPerTranslateBrowsePathsToNodeIds in translate operation ", function (done) {
+    it("server should return BadTooManyOperations when translate exceed maxNodesPerTranslateBrowsePathsToNodeIds in translate operation ", function(done) {
 
         server.engine.serverCapabilities.operationLimits.maxNodesPerBrowse.should.equal(2);
 
-        perform_operation_on_client_session(client, endpointUrl, function (session, done) {
+        perform_operation_on_client_session(client, endpointUrl, function(session, done) {
 
             const translateBrowsePath = [
                 makeBrowsePath("RootFolder", "/Objects/Server"),
@@ -221,7 +224,7 @@ describe("testing server with low maxNodesPerRead and maxNodesPerBrowse", functi
             server.engine.serverCapabilities.operationLimits.maxNodesPerTranslateBrowsePathsToNodeIds.should.be.greaterThan(1);
             translateBrowsePath.length.should.be.greaterThan(server.engine.serverCapabilities.operationLimits.maxNodesPerTranslateBrowsePathsToNodeIds);
 
-            session.translateBrowsePath(translateBrowsePath, function (err, results) {
+            session.translateBrowsePath(translateBrowsePath, function(err, results) {
                 should.exist(err);
                 err.message.should.match(/BadTooManyOperations/);
                 done();
@@ -230,24 +233,23 @@ describe("testing server with low maxNodesPerRead and maxNodesPerBrowse", functi
 
     });
 
-    it("crawler shall work even if server has a low limit the number of node in Read and Browse request", function (done) {
+    it("crawler shall work even if server has a low limit the number of node in Read and Browse request", function(done) {
 
-        const NodeCrawler = opcua.NodeCrawler;
         const redirectToFile = require("node-opcua-debug").redirectToFile;
 
         //xx redirectToFile("crawler_display_tree1.log",function(done){
 
         const treeify = require('treeify');
 
-        perform_operation_on_client_session(client, endpointUrl, function (the_session, callback) {
+        perform_operation_on_client_session(client, endpointUrl, function(the_session, callback) {
 
             const crawler = new NodeCrawler(the_session);
 
-            crawler.on("browsed", function (element) {
+            crawler.on("browsed", function(element) {
             });
 
             const nodeId = "ObjectsFolder";
-            crawler.read(nodeId, function (err, obj) {
+            crawler.read(nodeId, function(err, obj) {
                 if (!err) { /**/
                 }
                 crawler.dispose();
@@ -260,7 +262,7 @@ describe("testing server with low maxNodesPerRead and maxNodesPerBrowse", functi
 
     });
 
-    xit("should crawl a server cyclic-node ", function (done) {
+    xit("should crawl a server cyclic-node ", function(done) {
 
         const namespace = server.engine.addresSpaec.getOwnNamespace();
         // TODO
