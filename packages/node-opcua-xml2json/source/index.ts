c50ed49c4b8d16e7d01112bb8dc83f6ed0a7e604
Etienne Rossignon <etienne.rossignon@gadz.org>
2020-09-18 10:53:29 +0200

    fix typo

--- packages/node-opcua-xml2json/source/index.ts
+++ packages/node-opcua-xml2json/source/index.ts
@@ -1,6 +1,6 @@
 export * from "./xml2json";
 export * from "./fragment_cloner";
-export * from "./xml2Json_pojo";
+export * from "./xml2json_pojo";
 export * from "./fragment_cloner_parser";
 export * from "./extension_object_parser";
 export * from "./definition_parser";
