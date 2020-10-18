e4235faaa67e0170eaec14ccb8df896f28d614e7
Etienne Rossignon <etienne.rossignon@gadz.org>
2020-10-18 23:48:19 +0200

    simplify nodeJS.js file

--- packages/node-opcua-modeler/nodeJS.js
+++ packages/node-opcua-modeler/nodeJS.js
@@ -1,15 +1,1 @@
-/* eslint-disable no-prototype-builtins */
-"use strict";
-let __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-let __exportStar = (this && this.__exportStar) || function(m, exports) {
-    for (let p in m) if (p !== "default" && !exports.hasOwnProperty(p)) __createBinding(exports, m, p);
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-__exportStar(require("./distNodeJS"), exports);
-//# sourceMappingURL=index.js.map
+module.exports = require("./distNodeJS");
