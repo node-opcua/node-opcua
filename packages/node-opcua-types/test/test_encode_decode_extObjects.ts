e3264db3fdee0e0623b37be7deea0098ccfb429a
Etienne Rossignon <etienne.rossignon@gadz.org>
2021-02-23 22:24:30 +0100

    fix variant initialization

---
+++ packages/node-opcua-types/test/test_encode_decode_extObjects.ts
@@ -0,0 +1,30 @@
+import { BinaryStream } from "node-opcua-binary-stream";
+import { ExtensionObject } from "node-opcua-extension-object";
+import { DataType } from "node-opcua-variant";
+import { PubSubConfigurationDataType, PubSubConnectionDataType, UABinaryFileDataType } from "../dist";
+import "should";
+
+describe("Testing encoding/decoding of complex ExtensionObjects", () => {
+    it("should encode and decode a large extension object encodeExtensionObject/decodeExtensionObject", async () => {
+        const value = new PubSubConfigurationDataType({
+            connections: [new PubSubConnectionDataType({})]
+        });
+        const binaryFile = new UABinaryFileDataType({
+            body: { dataType: DataType.ExtensionObject, value }
+        });
+
+        const stream = new BinaryStream(binaryFile.binaryStoreSize());
+        binaryFile.encode(stream);
+
+        const reloaded = new UABinaryFileDataType();
+        stream.rewind();
+
+        reloaded.decode(stream);
+
+        const a = binaryFile.toJSON();
+        const b = reloaded.toJSON();
+
+        a.should.eql(b);
+        // console.log(a);
+    });
+});
