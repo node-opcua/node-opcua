c6b51c81ffe02b9f39436a61c6fdad3826d146bb
Etienne Rossignon <etienne.rossignon@gadz.org>
2021-04-14 11:59:41 +0200

    improve server configuration

--- packages/node-opcua-server-configuration/source/index.ts
+++ packages/node-opcua-server-configuration/source/index.ts
@@ -7,3 +7,4 @@ export * from "./push_certificate_manager_helpers";
 export * from "./clientTools/push_certificate_management_client";
 export * from "./server/install_push_certitifate_management";
 export * from "./server/push_certificate_manager_server_impl";
+export * from "./standard_certificate_types";
--- packages/node-opcua-server-configuration/source/trust_list_impl.ts
+++ packages/node-opcua-server-configuration/source/trust_list_impl.ts
@@ -1,28 +1,29 @@
 /**
  * @module node-opcua-server-configuration
  */
+import { StatusCode, StatusCodes } from "node-opcua-status-code";
 import { ITrustList } from "./trust_list";
 
 export class TrustList implements ITrustList {
 
-    public async closeAndUpdate(
-      fileHandle: number
-    ): Promise<boolean> {
+  public async closeAndUpdate(
+    applyChangesRequired: boolean
+  ): Promise<boolean> {
 
-        return false;
-    }
+    return false;
+  }
 
-    public async addCertificate(
-      certificate: Buffer,
-      isTrustedCertificate: boolean
-    ): Promise<void> {
-        return;
-    }
+  public async addCertificate(
+    certificate: Buffer,
+    isTrustedCertificate: boolean
+  ): Promise<StatusCode> {
+    return StatusCodes.BadNotImplemented;
+  }
 
-    public async removeCertificate(
-      thumbprint: string,
-      isTrustedCertificate: boolean
-    ): Promise<void> {
-        return;
-    }
+  public async removeCertificate(
+    thumbprint: string,
+    isTrustedCertificate: boolean
+  ): Promise<StatusCode> {
+    return StatusCodes.BadNotImplemented;
+  }
 }
