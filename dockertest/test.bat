b1296b54bdc5ef8c452abd851806f90816bda80e
Etienne Rossignon <etienne.rossignon@gadz.org>
2021-05-22 17:03:45 +0200

    add docker test

---
+++ dockertest/test.bat
@@ -0,0 +1,4 @@
+
+
+docker run -it  -v c:/sterfive/NodeOPCUA/node-opcua:/node-opcua --cpus=0.5 --mount source=data,target=/data node1 /bin/bash
+
---
+++ dockertest/testFast.bat
@@ -0,0 +1,4 @@
+
+
+docker run -it  -v c:/sterfive/NodeOPCUA/node-opcua:/node-opcua --cpus=20 --mount source=data,target=/data node1 /bin/bash
+
