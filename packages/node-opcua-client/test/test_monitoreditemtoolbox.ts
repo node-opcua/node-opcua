e27071eba77fcf6bd70c3cf5e65b1067dad7310b
Martin Baum <martin.baum@treestructure.net>
2021-06-03 18:00:40 +0200

    add testcase for monitoreditemgroup to report an error of createMonitoredItems fail

---
+++ packages/node-opcua-client/test/test_monitoreditemtoolbox.ts
@@ -0,0 +1,44 @@
+import * as should  from "should";
+import * as sinon from "sinon";
+import {TimestampsToReturn} from "node-opcua-data-value";
+import {SinonSandbox} from "sinon";
+import {ClientSubscriptionImpl} from "../source/private/client_subscription_impl";
+import { ClientSessionImpl } from "../source/private/client_session_impl";
+const {ClientMonitoredItemGroup, ClientSubscription } = require("..");
+
+describe('Testing the Monitored Items Group', () => {
+
+    let sandbox: SinonSandbox;
+    let monitoredItemGroup: any;
+    let fakeSubscription: any
+
+    before(() => {
+        sandbox = sinon.createSandbox();
+
+        fakeSubscription = sandbox.createStubInstance(ClientSubscriptionImpl);
+        fakeSubscription._wait_for_subscription_to_be_ready
+            .callsFake((cb:any) => setTimeout(() => (cb(null)), 500))
+        const fakeSession = sandbox.createStubInstance(ClientSessionImpl)
+        fakeSession.createMonitoredItems.yields(new Error('something bad happened'))
+        sandbox.stub(fakeSubscription, 'session').value(fakeSession)
+
+        monitoredItemGroup = ClientMonitoredItemGroup.create(
+            fakeSubscription,
+            [],
+            {},
+            TimestampsToReturn.Both
+        );
+    });
+
+    it('should transmit an error object if it occurs ', (done) => {
+        monitoredItemGroup.on('terminated', (err: Error) => {
+            err.message.should.eql('something bad happened')
+            done()
+        })
+
+    })
+
+    after(() => {
+        sandbox.restore()
+    })
+})
