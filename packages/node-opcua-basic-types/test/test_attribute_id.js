a75ecdfcf0f7c87b95ed8595a071cd4a3a96a4b7
Andreas Heine <56362817+AndreasHeine@users.noreply.github.com>
2021-10-14 13:15:29 +0200

    add additional test

--- packages/node-opcua-basic-types/test/test_attribute_id.js
+++ packages/node-opcua-basic-types/test/test_attribute_id.js
@@ -4,6 +4,7 @@ describe("AttributeIds", () => {
     it("isValidAttributeId 1", () => {
 
         isValidAttributeId(-1).should.eql(false);
+        isValidAttributeId(AttributeIds.AccessLevelEx).should.eql(true);
         isValidAttributeId(AttributeIds.LAST).should.eql(true);
         isValidAttributeId(AttributeIds.LAST+1).should.eql(false);
         isValidAttributeId(AttributeIds.AccessRestrictions).should.eql(true);
