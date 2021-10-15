6ac5baee64f9af8ca40a069e64b1d6eed0c2db43
Etienne Rossignon <etienne.rossignon@gadz.org>
2021-10-15 14:55:05 +0200

    ignore explicit module boundary type

--- .eslintrc.js
+++ .eslintrc.js
@@ -37,6 +37,7 @@ module.exports = {
         "@typescript-eslint/no-empty-interface": "off",
         "@typescript-eslint/no-var-requires": "off",
         "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/explicit-module-boundary-types": "off",
         "no-constant-condition": "off",
         "import/order": [
             "error",
