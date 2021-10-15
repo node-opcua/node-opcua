f664b2215d98034231decd61ffac599878e08f0d
Etienne Rossignon <etienne.rossignon@gadz.org>
2021-10-15 22:41:14 +0200

    chore(data-value): fix eslint issues

--- packages/node-opcua-data-value/test/test_datavalue.js
+++ packages/node-opcua-data-value/test/test_datavalue.js
@@ -2,58 +2,50 @@
 Error.stackTraceLimit = 1000;
 const util = require("util");
 const should = require("should");
-const {
-    Variant,
-    DataType,
-    VariantArrayType
-} = require("node-opcua-variant");
+const { Variant, DataType, VariantArrayType } = require("node-opcua-variant");
 const { NumericRange } = require("node-opcua-numeric-range");
 const { StatusCodes } = require("node-opcua-status-code");
 const { ExtensionObject } = require("node-opcua-extension-object");
+const { encode_decode_round_trip_test } = require("node-opcua-packet-analyzer/dist/test_helpers");
 
 const { DataValue, extractRange } = require("..");
 
 require("should");
 const doDebug = process.env.DEBUG;
 
-const { encode_decode_round_trip_test } = require("node-opcua-packet-analyzer/dist/test_helpers");
 describe("DataValue", () => {
-
     it("should create a empty DataValue and encode it as a 1-Byte length block", () => {
-
         const dataValue = new DataValue();
 
-        encode_decode_round_trip_test(dataValue, function(buffer/*, id*/) {
+        encode_decode_round_trip_test(dataValue, function (buffer /*, id*/) {
             buffer.length.should.equal(1);
         });
     });
 
     it("should create a DataValue with string variant and encode/decode it nicely ", () => {
-
         const dataValue = new DataValue({
             value: new Variant({ dataType: DataType.String, value: "Hello" })
         });
-        encode_decode_round_trip_test(dataValue, function(buffer/*, id*/) {
+        encode_decode_round_trip_test(dataValue, function (buffer /*, id*/) {
             buffer.length.should.equal(1 + 1 + 4 + 5);
         });
     });
 
     it("should create a DataValue with string variant and some date and encode/decode it nicely", () => {
-
         const dataValue = new DataValue({
             value: new Variant({ dataType: DataType.String, value: "Hello" }),
             serverTimestamp: new Date(Date.UTC(1601, 0, 1, 0, 0, 1)),
             serverPicoseconds: 50000,
             sourceTimestamp: new Date(Date.UTC(1601, 0, 1, 0, 0, 2)),
-            sourcePicoseconds: 25000, // 25 nano
+            sourcePicoseconds: 25000 // 25 nano
         });
         //xx var str = dataValue.toString();
-        encode_decode_round_trip_test(dataValue, function(/*buffer, id*/) {
+        encode_decode_round_trip_test(dataValue, function (/*buffer, id*/) {
+            /** */
         });
     });
 
     it("should create a DataValue with string variant and all dates and encode/decode it nicely", () => {
-
         const dataValue = new DataValue({
             value: new Variant({ dataType: DataType.String, value: "Hello" }),
             statusCode: StatusCodes.BadCertificateHostNameInvalid,
@@ -62,12 +54,12 @@ describe("DataValue", () => {
             sourceTimestamp: new Date(Date.UTC(2018, 1, 23, 18, 54, 12, 345)),
             sourcePicoseconds: 12345670
         });
-        encode_decode_round_trip_test(dataValue, function(/*buffer, id*/) {
+        encode_decode_round_trip_test(dataValue, function (/*buffer, id*/) {
+            /** */
         });
     });
 
     it("DataValue#toString", () => {
-
         let dataValue = new DataValue({
             value: new Variant({ dataType: DataType.String, value: "Hello" }),
             statusCode: StatusCodes.BadCertificateHostNameInvalid,
@@ -105,10 +97,7 @@ describe("DataValue", () => {
         ]);
     });
 
-
-
     it("DataValue - extractRange on a Float Array", () => {
-
         const dataValue = new DataValue({
             value: new Variant({
                 dataType: DataType.Double,
@@ -123,10 +112,8 @@ describe("DataValue", () => {
         dataValue1.value.value[1].should.eql(4.0);
         dataValue1.value.dataType.should.eql(DataType.Double);
         dataValue1.value.arrayType.should.eql(VariantArrayType.Array);
-
     });
     it("DataValue - extractRange on a String", () => {
-
         const dataValue = new DataValue({
             value: new Variant({
                 dataType: DataType.String,
@@ -140,10 +127,8 @@ describe("DataValue", () => {
         dataValue1.value.value.should.eql("34");
         dataValue1.value.dataType.should.eql(DataType.String);
         dataValue1.value.arrayType.should.eql(VariantArrayType.Scalar);
-
     });
     it("DataValue - extractRange on a String with StatusCode != Good - issue #635", () => {
-
         const dataValue = new DataValue({
             statusCode: StatusCodes.BadAlreadyExists,
             value: new Variant({
@@ -161,7 +146,6 @@ describe("DataValue", () => {
         dataValue1.statusCode.should.eql(StatusCodes.BadAlreadyExists);
     });
     it("DataValue - extractRange on a String with StatusCode != Good and invalid range - issue #635", () => {
-
         const dataValue = new DataValue({
             statusCode: StatusCodes.BadAlreadyExists,
             value: new Variant({
@@ -179,9 +163,7 @@ describe("DataValue", () => {
         dataValue1.statusCode.should.eql(StatusCodes.BadIndexRangeNoData);
     });
 
-
     it("DataValue - extractRange on a ByteString", () => {
-
         const dataValue = new DataValue({
             value: new Variant({
                 dataType: DataType.ByteString,
@@ -196,7 +178,6 @@ describe("DataValue", () => {
         dataValue1.value.value[1].should.eql(4.0);
         dataValue1.value.dataType.should.eql(DataType.ByteString);
         dataValue1.value.arrayType.should.eql(VariantArrayType.Scalar);
-
     });
 
     it("DataValue - extractRange on a ByteString (null value)", () => {
@@ -215,18 +196,11 @@ describe("DataValue", () => {
     });
 
     it("DataValue - extractRange on a Array of ByteString", () => {
-
         const dataValue = new DataValue({
             value: new Variant({
                 dataType: DataType.ByteString,
                 arrayType: VariantArrayType.Array,
-                value: [
-                    Buffer.from("ABC"),
-                    Buffer.from("DEF"),
-                    Buffer.from("GHI"),
-                    Buffer.from("JKL"),
-                    null
-                ]
+                value: [Buffer.from("ABC"), Buffer.from("DEF"), Buffer.from("GHI"), Buffer.from("JKL"), null]
             })
         });
         const dataValue1 = extractRange(dataValue, new NumericRange("2:3"));
@@ -238,7 +212,6 @@ describe("DataValue", () => {
         dataValue1.value.arrayType.should.eql(VariantArrayType.Array);
     });
     it("DataValue - extractRange on a Matrix of ByteString", () => {
-
         const dataValue = new DataValue({
             value: new Variant({
                 dataType: DataType.ByteString,
@@ -248,17 +221,15 @@ describe("DataValue", () => {
                     //[
                     Buffer.from("11"),
                     Buffer.from("12"),
-                    Buffer.from("13")
-                    ,//],
+                    Buffer.from("13"), //],
                     //[
                     Buffer.from("21"),
                     Buffer.from("22"),
-                    Buffer.from("23")
-                    ,//],
+                    Buffer.from("23"), //],
                     //[
                     Buffer.from("31"),
                     Buffer.from("32"),
-                    Buffer.from("33"),
+                    Buffer.from("33")
                     //]
                 ]
             })
@@ -273,7 +244,6 @@ describe("DataValue", () => {
         dataValue1.value.dimensions.should.eql([1, 2]);
     });
     it("DataValue - extractRange on invalid range - edge case", () => {
-
         const dataValue = new DataValue({
             value: new Variant({
                 dataType: DataType.ByteString,
@@ -286,7 +256,6 @@ describe("DataValue", () => {
         const dataValue1 = extractRange(dataValue, invalidRange);
         dataValue1.statusCode.should.eql(StatusCodes.BadIndexRangeInvalid);
         should.equal(null, dataValue1.value.value);
-
     });
     describe("Cloning a DataValue", () => {
         function SomeExtensionObject(options) {
@@ -304,7 +273,6 @@ describe("DataValue", () => {
 
         function install_test(copy_construct_or_clone, copy_construct_or_clone_func) {
             it("should " + copy_construct_or_clone + " a DataValue with a simple Variant", () => {
-
                 const dv = new DataValue({
                     value: {
                         dataType: DataType.UInt32,
@@ -315,10 +283,8 @@ describe("DataValue", () => {
 
                 cloned.value.dataType.should.eql(dv.value.dataType);
                 cloned.value.value.should.eql(dv.value.value);
-
             });
             it("should " + copy_construct_or_clone + " a DataValue with a variant array", () => {
-
                 const dv = new DataValue({
                     value: {
                         dataType: DataType.UInt32,
@@ -338,21 +304,13 @@ describe("DataValue", () => {
 
                 cloned.value.value[0].should.eql(36);
                 cloned.value.value[1].should.eql(37);
-
             });
             it("should " + copy_construct_or_clone + " a DataValue with a variant array of ByteString", () => {
-
                 const dv = new DataValue({
                     value: new Variant({
                         dataType: DataType.ByteString,
                         arrayType: VariantArrayType.Array,
-                        value: [
-                            Buffer.from("ABC"),
-                            Buffer.from("DEF"),
-                            Buffer.from("GHI"),
-                            Buffer.from("JKL"),
-                            null
-                        ]
+                        value: [Buffer.from("ABC"), Buffer.from("DEF"), Buffer.from("GHI"), Buffer.from("JKL"), null]
                     })
                 });
 
@@ -371,12 +329,9 @@ describe("DataValue", () => {
                 // clone object should not have been affected !
                 cloned.value.value[0].toString().should.eql("ABC");
                 cloned.value.value[1].toString().should.eql("DEF");
-
             });
 
-
             it("should " + copy_construct_or_clone + " a DataValue with a variant containing a extension object", () => {
-
                 const extObj = new SomeExtensionObject({ a: 36 });
                 const dv = new DataValue({
                     value: {
@@ -396,7 +351,6 @@ describe("DataValue", () => {
                 cloned.value.value.a.should.equal(36);
 
                 dv.value.value.a.should.eql(1000);
-
             });
             it("should " + copy_construct_or_clone + " a DataValue with a variant containing a extension object array", () => {
                 const extObj1 = new SomeExtensionObject({ a: 36 });
@@ -407,7 +361,6 @@ describe("DataValue", () => {
                         arrayType: VariantArrayType.Array,
                         value: [extObj1, extObj2]
                     }
-
                 });
 
                 // copy construct;,
@@ -425,17 +378,14 @@ describe("DataValue", () => {
 
                 dv.value.value[0].a.should.eql(1000);
                 dv.value.value[1].a.should.eql(1001);
-
             });
         }
 
         install_test("copy construct", copy_construct);
         install_test("clone", clone);
-
     });
 
     it("should convert a DataValue to a JSON Object", () => {
-
         const dataValue = new DataValue({
             statusCode: StatusCodes.BadAggregateListMismatch,
             serverTimestamp: new Date(Date.UTC(1789, 6, 14)),
@@ -451,45 +401,40 @@ describe("DataValue", () => {
 
         const json = dataValue.toJSON();
         json.should.eql({
-            value:
-            {
-                dataType: 'UInt16',
-                arrayType: 'Array',
+            value: {
+                dataType: "UInt16",
+                arrayType: "Array",
                 value: new Uint16Array([12, 13, 14])
             },
             statusCode: { value: 2161377280 },
             sourcePicoseconds: 0,
-            serverTimestamp: '1789-07-14T00:00:00.000Z',
+            serverTimestamp: "1789-07-14T00:00:00.000Z",
             serverPicoseconds: 0
         });
         // it should be possible to reconstruct a dataValue from the json object
         const dataValue1 = new DataValue(json);
         const json1 = dataValue1.toJSON();
         json1.should.eql({
-            value:
-            {
-                dataType: 'UInt16',
-                arrayType: 'Array',
+            value: {
+                dataType: "UInt16",
+                arrayType: "Array",
                 value: new Uint16Array([12, 13, 14])
             },
             statusCode: { value: 2161377280 },
             sourcePicoseconds: 0,
-            serverTimestamp: '1789-07-14T00:00:00.000Z',
+            serverTimestamp: "1789-07-14T00:00:00.000Z",
             serverPicoseconds: 0
         });
         // console.log(dataValue1.toString());
     });
     it("should convert a DataValue with ExtensionObject to JSON", () => {
-
-
         class SomeExtensionObject extends ExtensionObject {
-
-            constructor(options/*: { name: string } */) {
+            constructor(options /*: { name: string } */) {
                 super();
                 this.name = options.a;
             }
             toJSON() {
-                return { name: this.name }
+                return { name: this.name };
             }
         }
 
@@ -505,7 +450,7 @@ describe("DataValue", () => {
                     }),
                     new SomeExtensionObject({
                         name: "2"
-                    }),
+                    })
                 ]
             }
         });
@@ -515,6 +460,4 @@ describe("DataValue", () => {
             console.log(dataValue.toString());
         }
     });
-
 });
-
--- packages/node-opcua-data-value/test/test_issue_527.js
+++ packages/node-opcua-data-value/test/test_issue_527.js
@@ -1,107 +1,94 @@
 "use strict";
-/* global describe,it*/
 Error.stackTraceLimit = 1000;
 
-const { DataValue } = require("..");
+require("should");
 const { Variant } = require("node-opcua-variant");
 const { DataType } = require("node-opcua-variant");
-const { StatusCode, StatusCodes } =require("node-opcua-status-code");
-require("should");
+const { StatusCode, StatusCodes } = require("node-opcua-status-code");
+
+const { encode_decode_round_trip_test } = require("node-opcua-packet-analyzer/dist/test_helpers/encode_decode_round_trip_test");
 
 const { make_debugLog, checkDebugFlag } = require("node-opcua-debug");
+const { DataValue } = require("..");
+
 const debugLog = make_debugLog("TEST");
 const doDebug = checkDebugFlag("TEST");
 
-const encode_decode_round_trip_test = require("node-opcua-packet-analyzer/dist/test_helpers/encode_decode_round_trip_test").encode_decode_round_trip_test;
-
 describe("DataValue with Limit bits", function () {
-
     it("should handle a DataValue with Overflow | InfoTypeDataValue ", function () {
-
         const dataValue = new DataValue({
             statusCode: StatusCode.makeStatusCode(StatusCodes.Good, "Overflow | InfoTypeDataValue")
         });
-        encode_decode_round_trip_test(dataValue, function (buffer/*, id*/) {
+        encode_decode_round_trip_test(dataValue, function (buffer /*, id*/) {
             buffer.length.should.equal(5);
         });
 
         debugLog(dataValue.toString());
-
     });
 
     it("should handle a DataValue with statusCode Good + LimitLow ", function () {
-
         const dataValue = new DataValue({
             statusCode: StatusCode.makeStatusCode(StatusCodes.Good, "LimitLow")
         });
-        encode_decode_round_trip_test(dataValue, function (buffer/*, id*/) {
+        encode_decode_round_trip_test(dataValue, function (buffer /*, id*/) {
             buffer.length.should.equal(5);
         });
 
         debugLog(dataValue.toString());
-        debugLog(" value in hexa = 0x",dataValue.statusCode.value.toString(16));
-
+        debugLog(" value in hexa = 0x", dataValue.statusCode.value.toString(16));
     });
 
     it("should handle a DataValue with statusCode Good + LimitHigh ", function () {
-
         const dataValue = new DataValue({
             statusCode: StatusCode.makeStatusCode(StatusCodes.Good, "LimitHigh")
         });
-        encode_decode_round_trip_test(dataValue, function (buffer/*, id*/) {
+        encode_decode_round_trip_test(dataValue, function (buffer /*, id*/) {
             buffer.length.should.equal(5);
             debugLog(buffer.toString("hex"));
         });
 
-        debugLog(dataValue.toString()+ "\n");
+        debugLog(dataValue.toString() + "\n");
         debugLog(dataValue);
-        debugLog(" value in hexa = 0x",dataValue.statusCode.value.toString(16));
-
-
+        debugLog(" value in hexa = 0x", dataValue.statusCode.value.toString(16));
     });
 
     it("should handle a DataValue with statusCode Good + LimitConstant ", function () {
-
         const dataValue = new DataValue({
             statusCode: StatusCode.makeStatusCode(StatusCodes.Good, "LimitConstant")
         });
-        encode_decode_round_trip_test(dataValue, function (buffer/*, id*/) {
+        encode_decode_round_trip_test(dataValue, function (buffer /*, id*/) {
             buffer.length.should.equal(5);
         });
 
         debugLog(dataValue.toString());
-        debugLog(" value in hexa = 0x",dataValue.statusCode.value.toString(16));
-
+        debugLog(" value in hexa = 0x", dataValue.statusCode.value.toString(16));
     });
 
     it("should handle a DataValue with statusCode Good + extra bits = 1024 ", function () {
-
         const dataValue = new DataValue({
             statusCode: StatusCode.makeStatusCode(StatusCodes.Good, 1024)
         });
-        encode_decode_round_trip_test(dataValue, function (buffer/*, id*/) {
+        encode_decode_round_trip_test(dataValue, function (buffer /*, id*/) {
             buffer.length.should.equal(5);
             debugLog(buffer.toString("hex"));
         });
 
         debugLog(dataValue.toString());
 
-        debugLog(" value in hexa = 0x",dataValue.statusCode.value.toString(16));
+        debugLog(" value in hexa = 0x", dataValue.statusCode.value.toString(16));
     });
     it("should handle a DataValue that has a undefined statusCode", function () {
-
         const dataValue = new DataValue({
             statusCode: StatusCode.makeStatusCode(StatusCodes.Good, 1024)
         });
 
         dataValue.statusCode = undefined;
 
-        encode_decode_round_trip_test(dataValue,  (buffer/*, id*/)=> {
+        encode_decode_round_trip_test(dataValue, (buffer /*, id*/) => {
             buffer.length.should.equal(1);
             debugLog(buffer.toString("hex"));
         });
 
         debugLog(dataValue.toString());
     });
-
 });
