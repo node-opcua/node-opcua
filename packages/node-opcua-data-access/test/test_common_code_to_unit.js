bb5cef1d2419be576808e8480879292a5af41818
Etienne Rossignon <etienne.rossignon@gadz.org>
2021-10-16 18:31:21 +0200

    chore(data-access): fix eslint issues

--- packages/node-opcua-data-access/test/test_common_code_to_unit.js
+++ packages/node-opcua-data-access/test/test_common_code_to_unit.js
@@ -1,39 +1,37 @@
 "use strict";
 const { commonCodeToUInt, standardUnits } = require("..");
 
-describe("commonCodeToUInt", function() {
-
-    it("commonCodeToUInt - CEL = °C = degree Celsius", function() {
-
+describe("commonCodeToUInt", function () {
+    it("commonCodeToUInt - CEL = °C = degree Celsius", function () {
         const unitId = commonCodeToUInt("CEL"); // °C
         unitId.should.eql(4408652);
     });
 
-    it("commonCodeToUInt - LTR = l =  liter", function() {
+    it("commonCodeToUInt - LTR = l =  liter", function () {
         const unitId = commonCodeToUInt("LTR"); // °C
         unitId.should.eql(5002322);
     });
-    it("commonCodeToUInt - BQL = Bq =  Becquerel = 27,027 x 1E-12 Ci  ", function() {
+    it("commonCodeToUInt - BQL = Bq =  Becquerel = 27,027 x 1E-12 Ci  ", function () {
         const unitId = commonCodeToUInt("BQL"); // °C
         unitId.should.eql(4346188);
     });
-    it("commonCodeToUInt - CUR = Ci = Curie = 3,7 x 1E10 Bq ", function() {
+    it("commonCodeToUInt - CUR = Ci = Curie = 3,7 x 1E10 Bq ", function () {
         const unitId = commonCodeToUInt("CUR"); // °C
         unitId.should.eql(4412754);
     });
-    it("commonCodeToUInt - A53 = eV = ElectronVolt = 1,602 177 33 1E-19 J  ", function() {
+    it("commonCodeToUInt - A53 = eV = ElectronVolt = 1,602 177 33 1E-19 J  ", function () {
         const unitId = commonCodeToUInt("A53"); // °C
         unitId.should.eql(4273459);
     });
-    it("commonCodeToUInt - B71 = MeV = megaelectronvolt = 1E6 eV  ", function() {
+    it("commonCodeToUInt - B71 = MeV = megaelectronvolt = 1E6 eV  ", function () {
         const unitId = commonCodeToUInt("B71"); // °C
         unitId.should.eql(4339505);
     });
-    it("commonCodeToUInt - STL = l = standard liter", function() {
+    it("commonCodeToUInt - STL = l = standard liter", function () {
         const unitId = commonCodeToUInt("STL"); // °C
         unitId.should.eql(5461068);
     });
-    it("commonCodeToUInt - A97 = hPa = hecto pascal", function() {
+    it("commonCodeToUInt - A97 = hPa = hecto pascal", function () {
         const unitId = commonCodeToUInt("A97"); // °C
         unitId.should.eql(4274487);
     });
@@ -43,5 +41,4 @@ describe("commonCodeToUInt", function() {
     it("commonCodeToUint - kilogram_per_squared_centimeter", () => {
         standardUnits.kilogram_per_squared_centimeter.unitId.should.eql(17461);
     });
-
 });
