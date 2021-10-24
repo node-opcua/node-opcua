9465269631246a1316f913fa28beef6ee4001cf3
Etienne Rossignon <etienne.rossignon@gadz.org>
2021-10-24 13:09:07 +0200

    chore: fix format

--- packages/node-opcua-types/test/test_issue688.js
+++ packages/node-opcua-types/test/test_issue688.js
@@ -1,21 +1,19 @@
+const should = require("should");
+const { ReadValueId } = require(".."); // node-opcua-types"
 
-const should =require("should");
-const { ReadValueId } = require("..");// node-opcua-types"
-
-describe("Issue 688", ()=> {
-
+describe("Issue 688", () => {
     it("should throw an exception when argument of ReadValueId are invalid", () => {
-        should(()=>{
+        should(() => {
             const r = new ReadValueId({
                 nodeId: "someError" // << this invalid node id should cause a exception !
-            });    
+            });
         }).throw("String cannot be coerced to a nodeId : someError");
     });
     it("should coerce a ReadValueId", () => {
-        should(() =>{
+        should(() => {
             const r = new ReadValueId({
                 nodeId: "ns=1;s=OK" // << this  node id should cause a exception !
-            });    
+            });
         }).not.throw();
     });
 });
