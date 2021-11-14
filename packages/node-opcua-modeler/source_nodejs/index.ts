1678ee51b3d032b62626305fd83dfe3565fe7563
Etienne Rossignon <etienne.rossignon@gadz.org>
2021-11-14 12:34:46 +0100

    chore: modeler export

--- packages/node-opcua-modeler/source_nodejs/index.ts
+++ packages/node-opcua-modeler/source_nodejs/index.ts
@@ -1,5 +1,5 @@
 export * from "./symbol_cvs";
 export * from "./build_model";
 export * from "./build_documentation_to_file";
-export * from "../source/index";
+export * from "..";
 export { generateAddressSpace } from "node-opcua-address-space/nodeJS"
