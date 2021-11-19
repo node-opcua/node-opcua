d81039b1f2212cc7d2978ecd06e5b1888045c136
Etienne Rossignon <etienne.rossignon@gadz.org>
2021-11-19 21:28:15 +0100

    fix: method call with enumeration #1086

--- packages/node-opcua-end2end-test/test/end_to_end/test_umbrella_e2e_seriesF.js
+++ packages/node-opcua-end2end-test/test/end_to_end/test_umbrella_e2e_seriesF.js
@@ -49,7 +49,7 @@ describe("testing Client - Umbrella-F", function() {
   require("./u_test_e2e_issue_957")(test);
  
   require("./u_test_e2e_monitored_item_ctt018").t(test);
-
+  require("./u_test_e2e_1086").t(test);
 
   // require("./u_test_e2e_modifyMonitoredItem_onEvent")(test);
 
---
+++ packages/node-opcua-end2end-test/test/end_to_end/u_test_e2e_1086.ts
@@ -0,0 +1,27 @@
+import { OPCUAClient, ClientSession, MethodIds, ObjectIds, DataType, ServerState, coerceLocalizedText, StatusCodes } from "node-opcua-client";
+export function t(test: any) {
+    describe("issue#1086 calling method with enumeration in arguments", function () {
+        it("should handle Enumeration in input arguments", async () => {
+           const client = OPCUAClient.create({});
+           await client.withSessionAsync(test.endpointUrl, async (session) =>{
+              const result = await session.call({
+                methodId: MethodIds.Server_RequestServerStateChange,
+                objectId: ObjectIds.Server,
+                inputArguments: [
+                    { dataType: DataType.Int32, value: ServerState.Shutdown },
+                    { dataType: DataType.DateTime, value: new Date() },
+                    { dataType: DataType.UInt32, value: 100 }, // second till shutdown
+                    { dataType: DataType.LocalizedText, value: coerceLocalizedText({ text: "For maintenance", locale: "en_US" }) },
+                    { dataType: DataType.Boolean, value: true }
+                ]
+              });
+              console.log(result.toString());
+              result.inputArgumentResults![1].should.eql(StatusCodes.Good);
+              result.inputArgumentResults![3].should.eql(StatusCodes.Good);
+              result.inputArgumentResults![4].should.eql(StatusCodes.Good);
+              result.inputArgumentResults![0].should.eql(StatusCodes.Good);
+              
+           });
+        });
+    });
+}
