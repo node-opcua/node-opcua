dd03c4404e4ac6581ea169f67d4ed994e311168b
Etienne Rossignon <etienne.rossignon@gadz.org>
2021-11-23 08:30:50 +0100

    chore: add note on ValueRank

--- packages/node-opcua-variant/source/verify_rank_and_dimension.ts
+++ packages/node-opcua-variant/source/verify_rank_and_dimension.ts
@@ -1,5 +1,15 @@
 import assert from "node-opcua-assert";
 
+/**
+ * ValueRank indicates whether the Value Attribute of the Variable can be an Array and how many dimensions 
+ *  * n > 1                     : the Value is an array with the specified number of dimensions.
+ *  * OneDimension (1):           The value is an array with one dimension.
+ *  * OneOrMoreDimensions (0):    The value is an array with one or more dimensions.
+ *  * Scalar (-1):                The value is not an array.
+ *  * Any (-2):                   The value can be a scalar or an array with any number of dimensions.
+ *  * ScalarOrOneDimension (-3):  The value can be a scalar or a one dimensional array.
+ */
+
 export function verifyRankAndDimensions(options: { valueRank?: number; arrayDimensions?: number[] | null }): void {
     options.arrayDimensions = options.arrayDimensions || null;
     assert(options.arrayDimensions === null || Array.isArray(options.arrayDimensions));
