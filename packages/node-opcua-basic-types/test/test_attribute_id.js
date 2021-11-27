c0560ea101fa0204874c59be52c75cd41b7a380a
Etienne Rossignon <etienne.rossignon@gadz.org>
2021-11-27 22:37:24 +0100

    chore: remove AttributeIds.ALL

--- packages/node-opcua-basic-types/test/test_attribute_id.js
+++ packages/node-opcua-basic-types/test/test_attribute_id.js
@@ -2,12 +2,9 @@ const { AttributeIds, isValidAttributeId } = require("..");
 
 describe("AttributeIds", () => {
     it("isValidAttributeId 1", () => {
-
         isValidAttributeId(-1).should.eql(false);
         isValidAttributeId(AttributeIds.AccessLevelEx).should.eql(true);
-        isValidAttributeId(AttributeIds.LAST).should.eql(true);
-        isValidAttributeId(AttributeIds.LAST+1).should.eql(false);
+        isValidAttributeId(AttributeIds.AccessLevelEx + 1).should.eql(false);
         isValidAttributeId(AttributeIds.AccessRestrictions).should.eql(true);
-
-    })
-})
+    });
+});
