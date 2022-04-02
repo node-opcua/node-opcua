cc5dcdf67f1e63e636d84d07fb65590052b8a4f0
Etienne Rossignon <etienne.rossignon@gadz.org>
2022-04-02 17:31:06 +0200

    chore(certificate-manager): refactor and replace String.substr

--- packages/node-opcua-certificate-manager/source/make_subject.ts
+++ packages/node-opcua-certificate-manager/source/make_subject.ts
@@ -1,7 +1,7 @@
 import { OPCUACertificateManager } from "./certificate_manager";
 
 export function makeSubject(applicationName: string, hostname: string): string{
-    const commonName = `${applicationName}@${hostname}`.substr(0, 63);
-    const dc = `${hostname}`.substr(63);
+    const commonName = `${applicationName}@${hostname}`.substring(0, 63);
+    const dc = `${hostname}`.substring(63);
     return `/CN=${commonName}` + `/DC=${dc}` + OPCUACertificateManager.defaultCertificateSubject;
 }
