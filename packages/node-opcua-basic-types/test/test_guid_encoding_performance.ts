ce0179f71a30e484f93635951f1bc315bb0b0d61
Etienne Rossignon <etienne.rossignon@gadz.org>
2022-04-02 23:12:40 +0200

    chore(basic-type): refactor and replace String.substr

--- packages/node-opcua-basic-types/test/test_guid_encoding_performance.ts
+++ packages/node-opcua-basic-types/test/test_guid_encoding_performance.ts
@@ -9,21 +9,21 @@ function write_UInt16Old(stream: OutputBinaryStream, guid: string, starts: numbe
     const n = starts.length;
     for (let i = 0; i < n; i++) {
         const start = starts[i];
-        stream.writeUInt16(parseInt(guid.substring(start, 4), 16));
+        stream.writeUInt16(parseInt(guid.substring(start, start + 4), 16));
     }
 }
 function write_UInt32Old(stream: OutputBinaryStream, guid: string, starts: number[]) {
     const n = starts.length;
     for (let i = 0; i < n; i++) {
         const start = starts[i];
-        stream.writeUInt32(parseInt(guid.substring(start, 8), 16));
+        stream.writeUInt32(parseInt(guid.substring(start, start + 8), 16));
     }
 }
 function write_UInt8Old(stream: OutputBinaryStream, guid: string, starts: number[]) {
     const n = starts.length;
     for (let i = 0; i < n; i++) {
         const start = starts[i];
-        stream.writeUInt8(parseInt(guid.substring(start, 2), 16));
+        stream.writeUInt8(parseInt(guid.substring(start, start + 2), 16));
     }
 }
 export function encodeGuidOld(guid: Guid, stream: OutputBinaryStream): void {
@@ -121,7 +121,7 @@ describe("Map vs Object", () => {
                 counter = (counter + 1) % 120000;
             })
             .add("with Object", () => {
-                Object.prototype.hasOwnProperty.call(_map,counter.toString());
+                Object.prototype.hasOwnProperty.call(_map, counter.toString());
                 counter = (counter + 1) % 120000;
             })
             .on("cycle", (message) => {
