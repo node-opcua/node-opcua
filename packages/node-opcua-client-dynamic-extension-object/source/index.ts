92914bc9bf1927feab7bd1b6083f8733fc471843
Etienne Rossignon <etienne.rossignon@gadz.org>
2022-05-29 15:06:07 +0200

    refactor: move promote_opaque_structure_in_notification_data

--- packages/node-opcua-client-dynamic-extension-object/source/index.ts
+++ packages/node-opcua-client-dynamic-extension-object/source/index.ts
@@ -9,3 +9,4 @@ export * from "./get_extension_object_constructor";
 export * from "./get_extra_data_type_manager";
 export * from "./resolve_dynamic_extension_object";
 export * from "./convert_data_type_definition_to_structuretype_schema";
+export * from "./promote_opaque_structure_in_notification_data";
