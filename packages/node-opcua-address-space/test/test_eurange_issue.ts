33077c31dfca6402978ddcfdc51db2d24adb0630
Etienne Rossignon <etienne.rossignon@sterfive.com>
2022-06-17 19:45:28 +0200

    chore(address-space) make sure ObjectType instantiation replicates default value in child variables

--- packages/node-opcua-address-space/test/test_eurange_issue.ts
+++ packages/node-opcua-address-space/test/test_eurange_issue.ts
@@ -25,13 +25,13 @@ describe("Testing EURange Issue", async function (this: any) {
         addressSpace.dispose();
     });
 
-    it("should instatiate a VariableType that has a  valid Range as a value, and the same value should appear in the instiated variable", () => {
-        const type = addressSpace.findVariableType("MyType", 1) as UAVariableType;
+    it("should instantiate a VariableType that has a  valid Range as a value, and the same value should appear in the instantiated variable", () => {
+        const variableType = addressSpace.findVariableType("MyVariableType", 1)!;
 
-        const dataValueFromType = type.readAttribute(null, AttributeIds.Value);
+        const dataValueFromType = variableType.readAttribute(null, AttributeIds.Value);
         console.log(dataValueFromType.toString());
 
-        const variable = type.instantiate({
+        const variable = variableType.instantiate({
             browseName: "Type",
             componentOf: addressSpace.rootFolder.objects.server
         });
@@ -39,6 +39,20 @@ describe("Testing EURange Issue", async function (this: any) {
         console.log(dataValue.toString());
 
         dataValue.value.toString().should.eql(dataValueFromType.value.toString());
+    });
+    it("should instantiate a Object that has a property containing valid Range as a value, and the same value should appear in the instantiated object", () => {
+        const objectType = addressSpace.findObjectType("MyObjectType", 1)!;
 
+        const dataValueFromType = objectType.getChildByName("Range", 1)!.readAttribute(null, AttributeIds.Value);
+        console.log(dataValueFromType.toString());
+
+        const object = objectType.instantiate({
+            browseName: "MyObject",
+            componentOf: addressSpace.rootFolder.objects.server
+        });
+        const dataValue = object.getChildByName("Range", 1)!.readAttribute(null, AttributeIds.Value);
+        console.log(dataValue.toString());
+
+        dataValue.value.toString().should.eql(dataValueFromType.value.toString());
     });
 });
