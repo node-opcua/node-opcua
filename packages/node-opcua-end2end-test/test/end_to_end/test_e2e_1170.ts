366e1392a960f0825ee07589d4c178145becf885
JeanArhancet <jean.arhancetebehere@gmail.com>
2022-08-25 13:46:52 +0200

    test: implement test 1170

---
+++ packages/node-opcua-end2end-test/test/end_to_end/test_e2e_1170.ts
@@ -0,0 +1,79 @@
+const async = require("async");
+const should = require("should");
+const opcua = require("node-opcua");
+const OPCUAClient = opcua.OPCUAClient;
+const UserTokenPolicy = opcua.UserTokenPolicy;
+const { perform_operation_on_client_session } = require("../../test_helpers/perform_operation_on_client_session");
+// require("node-opcua-service-session").UserNameIdentityToken;
+
+const describe = require("node-opcua-leak-detector").describeWithLeakDetector;
+describe("Testing bug #1170", function() {
+
+    const port = 1170;
+    let server = null;
+    let endpointUrl = "";
+
+
+    before(async () => {
+
+
+        server = new opcua.OPCUAServer({
+            port
+        });
+
+        await server.start();
+
+        let endpoints = server._get_endpoints();
+        endpointUrl = endpoints[0].endpointUrl;
+
+
+    });
+    after(function(done) {
+        server.shutdown(done);
+    });
+
+    it("should productURI have the same value", (done) => {
+
+        // user1/password1
+
+        const client = opcua.OPCUAClient.create({
+            endpointMustExist: false,
+            requestedSessionTimeout: 60000
+        });
+
+        let the_session;
+        async.series([
+
+            function client_connect(callback) {
+                client.connect(endpointUrl, function(err) {
+                    if (err) {
+                        console.log(" cannot connect to endpoint :", endpointUrl);
+                    } else {
+                        console.log("connected !");
+                    }
+                    callback(err);
+                });
+      
+            },
+
+            function client_create_session(callback) {
+                
+                client.createSession({}, function(err, session) {
+                    if (!err) {
+                        the_session = session;
+                        should(client.endpoint.server.productUri).eql("NodeOPCUA-Server");
+                    }
+                    callback(err);
+                });
+            },
+
+            function(callback) {
+                the_session.close(callback);
+            },
+            function(callback) {
+                client.disconnect(callback);
+            }
+        ], done);
+    });
+});
+
