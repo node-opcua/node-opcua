214d4724c0eb6b322b3f39de07985514ca5dccbe
Etienne Rossignon <etienne.rossignon@sterfive.com>
2022-11-12 11:54:33 +0100

    move addressspace for compliance to external module

--- packages/node-opcua/source/index.ts
+++ packages/node-opcua/source/index.ts
@@ -120,10 +120,4 @@ export * from "node-opcua-service-filter";
 // filtering tools
 export * from "node-opcua-transport";
 
-export { OPCUADiscoveryServer } from "node-opcua-server-discovery";
-
-export { build_address_space_for_conformance_testing } from "node-opcua-address-space-for-conformance-testing";
-export { install_optional_cpu_and_memory_usage_node } from "node-opcua-vendor-diagnostic";
-
 export * from "node-opcua-aggregates";
-export { makeBoiler } from "node-opcua-address-space/testHelpers";
