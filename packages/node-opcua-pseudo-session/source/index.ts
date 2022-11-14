a58679ee3164d96b12e62984a4e3f7f0e5c85482
Etienne Rossignon <etienne.rossignon@sterfive.com>
2022-11-14 21:55:32 +0100

    pseudo session: add getChildByBrowseName and IBasicSessionEx

--- packages/node-opcua-pseudo-session/source/index.ts
+++ packages/node-opcua-pseudo-session/source/index.ts
@@ -2,6 +2,7 @@
  * @module node-opcua-pseudo-session
  */
 export * from "./basic_session_interface";
+export * from "./basic_session_ex";
 export * from "./browse_all";
 export * from "./read_operational_limits";
 export * from "./create_monitored_items_limit";
@@ -9,3 +10,4 @@ export * from "./basic_session_with_subscription";
 export * from "./find_basic_datatype";
 export * from "./get_builtin_datatype";
 export * from "./find_structure_datatype";
+export * from "./get_child_by_browse_name";
