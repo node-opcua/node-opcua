26120b9111e78f88123ee7a959b9c5c38e67f04e
Jacob <jacobq@gmail.com>
2022-11-21 10:59:14 -0600

    fix: uncomment server-stuff exports (fix #1216)

--- packages/node-opcua/source/server-stuff.ts
+++ packages/node-opcua/source/server-stuff.ts
@@ -7,8 +7,6 @@ export * from "node-opcua-server";
 
 export { OPCUADiscoveryServer } from "node-opcua-server-discovery";
 
-/**
- * export { build_address_space_for_conformance_testing } from "node-opcua-address-space-for-conformance-testing";
- * export { install_optional_cpu_and_memory_usage_node } from "node-opcua-vendor-diagnostic";
- * export { makeBoiler } from "node-opcua-address-space/testHelpers";
- */
+export { build_address_space_for_conformance_testing } from "node-opcua-address-space-for-conformance-testing";
+export { install_optional_cpu_and_memory_usage_node } from "node-opcua-vendor-diagnostic";
+export { makeBoiler } from "node-opcua-address-space/testHelpers";
