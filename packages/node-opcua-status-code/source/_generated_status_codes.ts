18e52db2f18720528c3c292560069a0eaefe5447
Etienne Rossignon <etienne.rossignon@sterfive.com>
2022-12-16 08:43:34 +0100

    update status codes

--- packages/node-opcua-status-code/source/_generated_status_codes.ts
+++ packages/node-opcua-status-code/source/_generated_status_codes.ts
@@ -1253,11 +1253,11 @@ export class StatusCodes {
         value: 0xa50000,
         description: "No data exists for the requested time range or event filter."
     });
-    /** The data or event field was successfully replaced in the historical database. */
+    /** More data is available in the time range beyond the number of values requested. */
     static GoodMoreData: ConstantStatusCode = new ConstantStatusCode({
         name: "GoodMoreData",
         value: 0xa60000,
-        description: "The data or event field was successfully replaced in the historical database."
+        description: "More data is available in the time range beyond the number of values requested."
     });
     /** The requested number of Aggregates does not match the requested number of NodeIds. */
     static BadAggregateListMismatch: ConstantStatusCode = new ConstantStatusCode({
@@ -1301,6 +1301,12 @@ export class StatusCodes {
         value: 0x81130000,
         description: "The request has not been processed by the server yet."
     });
+    /** The operation is not allowed because a transaction is in progress. */
+    static BadTransactionPending: ConstantStatusCode = new ConstantStatusCode({
+        name: "BadTransactionPending",
+        value: 0x80e80000,
+        description: "The operation is not allowed because a transaction is in progress."
+    });
     /** The device identity needs a ticket before it can be accepted. */
     static BadTicketRequired: ConstantStatusCode = new ConstantStatusCode({
         name: "BadTicketRequired",
@@ -1312,6 +1318,12 @@ export class StatusCodes {
         name: "BadTicketInvalid",
         value: 0x81200000,
         description: "The device identity needs a ticket before it can be accepted."
+    });
+    /** The requested operation is not allowed, because the Node is locked by a different application. */
+    static BadLocked: ConstantStatusCode = new ConstantStatusCode({
+        name: "BadLocked",
+        value: 0x80e90000,
+        description: "The requested operation is not allowed, because the Node is locked by a different application."
     });
     /** The value does not come from the real source and has been edited by the server. */
     static GoodEdited: ConstantStatusCode = new ConstantStatusCode({
