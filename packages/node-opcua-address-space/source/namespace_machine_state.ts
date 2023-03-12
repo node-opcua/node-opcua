6d63a00ace2a3cd87fa3b4795f9c9d8e6d45b6db
Etienne Rossignon <etienne.rossignon@sterfive.com>
2023-03-12 09:00:12 +0100

    enhance StateMachine addState and addTransition

--- packages/node-opcua-address-space/source/namespace_machine_state.ts
+++ packages/node-opcua-address-space/source/namespace_machine_state.ts
@@ -1,12 +1,15 @@
 import { UAObject, UAObjectType } from "node-opcua-address-space-base";
 import { UAState } from "node-opcua-nodeset-ua";
+import { QualifiedNameLike } from "node-opcua-data-model";
+
 import { UATransitionEx } from "./interfaces/state_machine/ua_transition_ex";
 export interface INamespaceMachineState {
-    addState(component: UAObject | UAObjectType, stateName: string, stateNumber: number, isInitialState?: boolean): UAState;
+    addState(component: UAObject | UAObjectType, stateName: QualifiedNameLike, stateNumber: number, isInitialState?: boolean): UAState;
     addTransition(
         component: UAObject | UAObjectType /* StateMachine | StateMachineType,*/,
         fromState: string,
         toState: string,
-        transitionNumber: number
+        transitionNumber: number,
+        browseName?: QualifiedNameLike
     ): UATransitionEx;
 }
