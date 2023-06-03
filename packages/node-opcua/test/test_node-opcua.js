f0b4a1e899b3cf05945ffa0d6e8a71d64d807023
Etienne Rossignon <etienne.rossignon@sterfive.com>
2023-06-03 09:15:19 +0200

    fix typescript issues raised with new ts version

--- packages/node-opcua/test/test_node-opcua.js
+++ packages/node-opcua/test/test_node-opcua.js
@@ -2,6 +2,7 @@ const should = require("should");
 const opcua = require("..");
 describe("module 'node-opcua' module sanity test", function () {
     it("module 'node-opcua' should not export any null properties", function () {
+//        console.log(Object.keys(opcua).join(" "));
         Object.keys(opcua).forEach(function (x) {
             should.exist(opcua[x], x + " should be defined");
         });
