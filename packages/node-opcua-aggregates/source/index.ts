bdab5e5cc28f0c46dd0611f7d15c19cbe87836f5
Etienne Rossignon <etienne.rossignon@sterfive.com>
2023-06-05 16:16:18 +0200

    improve codecoverage

--- packages/node-opcua-aggregates/source/index.ts
+++ packages/node-opcua-aggregates/source/index.ts
@@ -15,3 +15,8 @@ export * from "./common";
 export * from "./average";
 export * from "./read_processed_details";
 export { AggregateFunction } from "node-opcua-constants";
+export { getCountData } from "./count";
+export { getPercentGoodData } from "./percent_good";
+export { getPercentBadData } from "./percent_bad";
+export { getDurationBadData } from "./duration_bad";
+export { getDurationGoodData } from "./duration_good";
