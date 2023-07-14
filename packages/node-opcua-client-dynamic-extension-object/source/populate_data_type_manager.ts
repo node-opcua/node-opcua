2e9e962e1c24bf16160ce839d1ca2985e2d0f130
Etienne Rossignon <etienne.rossignon@sterfive.com>
2023-07-14 17:57:28 +0200

    better detect <1.03 nodesets

--- packages/node-opcua-client-dynamic-extension-object/source/populate_data_type_manager.ts
+++ packages/node-opcua-client-dynamic-extension-object/source/populate_data_type_manager.ts
@@ -1,19 +1,58 @@
-import { findLastKey } from "lodash";
-import { AttributeIds, BrowseDirection, NodeClassMask } from "node-opcua-data-model";
-import { coerceNodeId, resolveNodeId } from "node-opcua-nodeid";
+import { AttributeIds, BrowseDirection, NodeClassMask, ResultMask } from "node-opcua-data-model";
+import { resolveNodeId } from "node-opcua-nodeid";
 import { IBasicSession, browseAll } from "node-opcua-pseudo-session";
-import { DataTypeIds } from "node-opcua-constants";
-
-import { StatusCodes } from "node-opcua-status-code";
+import { DataTypeIds, ObjectIds, ObjectTypeIds, VariableTypeIds } from "node-opcua-constants";
 import { DataType } from "node-opcua-variant";
 import { ReferenceDescription } from "node-opcua-types";
+import { makeBrowsePath } from "node-opcua-service-translate-browse-path";
 //
 import { ExtraDataTypeManager } from "./extra_data_type_manager";
 import { populateDataTypeManager103 } from "./private/populate_data_type_manager_103";
 import { populateDataTypeManager104 } from "./private/populate_data_type_manager_104";
 
-async function serverImplementsDataTypeDefinition(session: IBasicSession): Promise<boolean> {
-    // check if server provides DataTypeDefinition => in this case this is the prefered route,
+/**
+ * @private
+ */
+export async function serverImplementsDataTypeDefinition(session: IBasicSession): Promise<boolean> {
+    // One way to figure out is to check if the server provides DataTypeDefinition node
+    // ( see OPCUA 1.04 part 6 -)
+    // This is the preferred route, as we go along, more and more servers will implement this.
+    const browseResult1 = await browseAll(session, {
+        browseDirection: BrowseDirection.Forward,
+        includeSubtypes: true,
+        nodeClassMask: NodeClassMask.Variable,
+        nodeId: resolveNodeId(ObjectIds.OPCBinarySchema_TypeSystem),
+        resultMask: ResultMask.TypeDefinition
+    });
+
+    let count103DataType = 0;
+    for (const ref of browseResult1.references || []) {
+        const td = ref.typeDefinition;
+        if (!(td.namespace === 0 && td.value === VariableTypeIds.DataTypeDictionaryType)) continue;
+        // we have a type definition,
+        // let check if there is a deprecated property
+        const p = await session.translateBrowsePath(makeBrowsePath(ref.nodeId, "/Deprecated"));
+        if (!p.statusCode.isGood() || !p.targets || p.targets.length === 0) {
+            // the dataTypeDictionaryType is not exposing a Deprecated property
+            count103DataType++;
+            continue;
+        }
+        const deprecatedNodeId = p.targets[0].targetId;
+        // we have a deprecated property => this is a 1.03 server or 1.04
+        // => we need to check if the server provides DataTypeDefinition
+        const dataValue = await session.read({ nodeId: deprecatedNodeId, attributeId: AttributeIds.Value });
+        if (dataValue.statusCode.isGood() && dataValue.value.value === false) {
+            // this is a 1.03 server
+            count103DataType++;
+            continue;
+        }
+    }
+    if (count103DataType >= 1) {
+        // some namespace are old , we cannot assume that all namespace are 1.04
+        return false;
+    }
+
+    // check if server provides DataTypeDefinition => in this case this is the preferred route,
     // as we go along, more and more servers will implement this.
     const browseResult = await browseAll(session, {
         browseDirection: BrowseDirection.Forward,
