95439f3990c13240a29dfdf8f11150d2fcfa4bb0
Etienne Rossignon <etienne.rossignon@sterfive.com>
2023-10-29 15:56:21 +0100

    refactor readUAAnalogItem

--- packages/node-opcua-client/source/client_utils.ts
+++ packages/node-opcua-client/source/client_utils.ts
@@ -2,17 +2,13 @@
  * @module node-opcua-client
  */
 
-import { assert } from "node-opcua-assert";
 import { AttributeIds } from "node-opcua-data-model";
 import { DataValue } from "node-opcua-data-value";
 import { NodeIdLike, resolveNodeId } from "node-opcua-nodeid";
 import { ReadValueIdOptions } from "node-opcua-service-read";
 import { BrowsePath, BrowsePathResult } from "node-opcua-service-translate-browse-path";
-import { StatusCodes } from "node-opcua-status-code";
 import { Variant } from "node-opcua-variant";
-import { ClientSession } from "./client_session";
-import { ClientSessionImpl } from "./private/client_session_impl";
-import { ResponseCallback } from "node-opcua-pseudo-session";
+import { IBasicSessionAsync } from "node-opcua-pseudo-session";
 
 const hasPropertyRefId = resolveNodeId("HasProperty");
 
@@ -48,16 +44,7 @@ interface AnalogDataItemSnapshot {
  * @param nodeId
  * @param callback
  */
-export function readUAAnalogItem(session: ClientSession, nodeId: NodeIdLike): Promise<AnalogDataItemSnapshot>;
-export function readUAAnalogItem(
-    session: ClientSession,
-    nodeId: NodeIdLike,
-    callback: ResponseCallback<AnalogDataItemSnapshot>
-): void;
-export function readUAAnalogItem(session: ClientSession, nodeId: NodeIdLike, ...args: [any?, ...any[]]): any {
-    const callback = args[0] as ResponseCallback<AnalogDataItemSnapshot>;
-    assert(typeof callback === "function");
-
+export async function readUAAnalogItem(session: IBasicSessionAsync, nodeId: NodeIdLike): Promise<AnalogDataItemSnapshot> {
     const browsePath = [
         browsePathPropertyRequest(nodeId, "EngineeringUnits"),
         browsePathPropertyRequest(nodeId, "EURange"),
@@ -74,51 +61,35 @@ export function readUAAnalogItem(session: ClientSession, nodeId: NodeIdLike, ...
         valuePrecision: null
     };
 
-    session.translateBrowsePath(browsePath, (err: Error | null, browsePathResults?: BrowsePathResult[]) => {
-        if (err) {
-            return callback(err);
-        }
-        browsePathResults = browsePathResults || [];
+    const browsePathResults = await session.translateBrowsePath(browsePath);
 
-        const actions: any[] = [];
-        const nodesToRead: ReadValueIdOptions[] = [];
+    const actions: (   (readResult: DataValue) =>void)[] = [];
+    const nodesToRead: ReadValueIdOptions[] = [];
 
-        function processProperty(browsePathResult: BrowsePathResult, propertyName: string) {
-            if (browsePathResult.statusCode.isGood()) {
-                browsePathResult.targets = browsePathResult.targets || [];
-                nodesToRead.push({
-                    attributeId: AttributeIds.Value,
-                    nodeId: browsePathResult.targets[0].targetId
-                });
-                actions.push((readResult: DataValue) => ((analogItemData as any)[propertyName] = readResult.value.value));
-            }
-        }
-
-        processProperty(browsePathResults[0], "engineeringUnits");
-        processProperty(browsePathResults[1], "engineeringUnitsRange");
-        processProperty(browsePathResults[2], "instrumentRange");
-        processProperty(browsePathResults[3], "valuePrecision");
-        processProperty(browsePathResults[4], "definition");
-
-        session.read(nodesToRead, (err1: Error | null, dataValues?: DataValue[]) => {
-            if (err1) {
-                return callback(err1);
-            }
-            /* istanbul ignore next */
-            if (!dataValues) {
-                return callback(new Error("Internal Error"));
-            }
-
-            dataValues.forEach((result: DataValue, index: number) => {
-                actions[index].call(null, result);
+    function processProperty(browsePathResult: BrowsePathResult, propertyName: string) {
+        if (browsePathResult.statusCode.isGood()) {
+            browsePathResult.targets = browsePathResult.targets || [];
+            nodesToRead.push({
+                attributeId: AttributeIds.Value,
+                nodeId: browsePathResult.targets[0].targetId
             });
+            actions.push(
+                (readResult: DataValue) =>
+                    ((analogItemData as unknown as Record<string, unknown>)[propertyName] = readResult.value.value)
+            );
+        }
+    }
 
-            callback(err1, analogItemData);
-        });
+    processProperty(browsePathResults[0], "engineeringUnits");
+    processProperty(browsePathResults[1], "engineeringUnitsRange");
+    processProperty(browsePathResults[2], "instrumentRange");
+    processProperty(browsePathResults[3], "valuePrecision");
+    processProperty(browsePathResults[4], "definition");
+
+    const dataValues = await session.read(nodesToRead);
+
+    dataValues.forEach((result: DataValue, index: number) => {
+        actions[index].call(null, result);
     });
+    return analogItemData;
 }
-// tslint:disable:no-var-requires
-// tslint:disable:max-line-length
-const thenify = require("thenify");
-const opts = { multiArgs: false };
-(module as any).exports.readUAAnalogItem = thenify.withCallback((module as any).exports.readUAAnalogItem, opts);
