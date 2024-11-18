df1e0a2c41b51afc8a58fbc62e47ac6c50c5027f
Etienne Rossignon <etienne.rossignon@sterfive.com>
2024-11-18 08:37:18 +0100

    cleanup xml2json

--- packages/node-opcua-xml2json/source/index.ts
+++ packages/node-opcua-xml2json/source/index.ts
@@ -2,5 +2,4 @@ export * from "./xml2json";
 export * from "./fragment_cloner";
 export * from "./xml2json_pojo";
 export * from "./fragment_cloner_parser";
-export * from "./extension_object_parser";
 export * from "./definition_parser";
