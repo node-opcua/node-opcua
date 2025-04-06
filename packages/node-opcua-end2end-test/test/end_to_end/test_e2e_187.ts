b93490077ea88b5c70a37f4930c5cf81804ffba3
Etienne Rossignon <etienne.rossignon@sterfive.com>
2025-04-06 09:38:08 +0200

    chore: convert test in typescript

--- packages/node-opcua-end2end-test/test/end_to_end/test_e2e_187.ts
+++ packages/node-opcua-end2end-test/test/end_to_end/test_e2e_187.ts
@@ -1,25 +1,26 @@
-"use strict";
-const should = require("should");
-const chalk = require("chalk");
+import should from "should";
+import chalk from "chalk";
 
-const { OPCUAClient, OPCUAServer, SessionContext } = require("node-opcua");
+import { NodeId, OPCUAClient, OPCUAServer, SessionContext, UAObject } from "node-opcua";
+import { Boiler, makeBoiler } from "node-opcua-address-space/testHelpers";
+import { UAProxyManager } from "node-opcua-client-proxy";
+import { make_debugLog, checkDebugFlag } from "node-opcua-debug";
 const context = SessionContext.defaultContext;
 
-
-const { makeBoiler } = require("node-opcua-address-space/testHelpers");
-const { UAProxyManager } = require("node-opcua-client-proxy");
-
-const { make_debugLog, checkDebugFlag } = require("node-opcua-debug");
 const debugLog = make_debugLog("TEST");
 const doDebug = checkDebugFlag("TEST");
 
 const describe = require("node-opcua-leak-detector").describeWithLeakDetector;
-describe("testing monitoring Executable flags on methods", function () {
+describe("testing monitoring Executable flags on methods", function (this: any) {
+
     this.timeout(Math.max(60000, this.timeout()));
 
-    let server, client, endpointUrl;
+    let server: OPCUAServer;
+    let client: OPCUAClient | undefined;
+    let endpointUrl: string;
 
-    let boiler_on_server;
+    let boiler_on_server: NodeId;
+
     const port = 2006;
     before(async () => {
         const options = { port };
@@ -27,30 +28,39 @@ describe("testing monitoring Executable flags on methods", function () {
 
         await server.initialize();
 
-        boiler_on_server = makeBoiler(server.engine.addressSpace, { browseName: "Boiler#1" });
+        const uaBoiler = makeBoiler(server.engine.addressSpace!, {
+            browseName: "Boiler#1",
+            organizedBy: server.engine!.addressSpace!.rootFolder.objects
+        });
 
-        const haltMethod = boiler_on_server.simulation.getMethodByName("Halt");
-        const resetMethod = boiler_on_server.simulation.getMethodByName("Reset");
-        const startMethod = boiler_on_server.simulation.getMethodByName("Start");
-        const suspendMethod = boiler_on_server.simulation.getMethodByName("Suspend");
+        const haltMethod = uaBoiler.simulation.getMethodByName("Halt")!;
+        const resetMethod = uaBoiler.simulation.getMethodByName("Reset")!;
+        const startMethod = uaBoiler.simulation.getMethodByName("Start")!;
+        const suspendMethod = uaBoiler.simulation.getMethodByName("Suspend")!;
+        boiler_on_server = uaBoiler.nodeId;
+
         haltMethod.getExecutableFlag(context).should.eql(true);
         resetMethod.getExecutableFlag(context).should.eql(false);
         startMethod.getExecutableFlag(context).should.eql(true);
         suspendMethod.getExecutableFlag(context).should.eql(false);
 
-        boiler_on_server = boiler_on_server.nodeId;
-
         await server.start();
+
+        haltMethod.getExecutableFlag(context).should.eql(true);
+        resetMethod.getExecutableFlag(context).should.eql(false);
+        startMethod.getExecutableFlag(context).should.eql(true);
+        suspendMethod.getExecutableFlag(context).should.eql(false);
+
 
         endpointUrl = server.getEndpointUrl();
     });
 
     beforeEach(async () => {
-        client = OPCUAClient.create();
+        client = OPCUAClient.create({});
     });
 
     afterEach(async () => {
-        client = null;
+        client = undefined;
     });
 
     after(async () => {
@@ -59,7 +69,7 @@ describe("testing monitoring Executable flags on methods", function () {
 
     it("#187 ...... ", async () => {
         let proxyManager;
-
+        if (!client) return;
         await client.withSessionAsync(endpointUrl, async (session) => {
             proxyManager = new UAProxyManager(session);
             const nodeId = boiler_on_server;
@@ -67,7 +77,7 @@ describe("testing monitoring Executable flags on methods", function () {
             await proxyManager.start();
             const smType = "ProgramStateMachineType";
             const obj = await proxyManager.getStateMachineType(smType);
-            
+
             if (doDebug) {
                 debugLog("InitialState = ", obj.initialState ? obj.initialState.toString() : "<null>");
                 debugLog(
@@ -87,7 +97,7 @@ describe("testing monitoring Executable flags on methods", function () {
             if (doDebug) {
                 debugLog(" NodeId = ", nodeId.toString());
             }
-            const boiler = await proxyManager.getObject(nodeId);
+            const boiler = await proxyManager.getObject(nodeId) as any;
             if (doDebug) {
                 debugLog("Current State", boiler.simulation.currentState.toString());
             }
@@ -98,9 +108,7 @@ describe("testing monitoring Executable flags on methods", function () {
             await boiler.simulation.halt([]);
 
             await boiler.simulation.reset([]);
-            if (doDebug) {
-                debugLog(" Reset => ", err);
-            }
+
             await new Promise((resolve) => setTimeout(resolve, 500));
             debugLog(boiler.simulation.currentState.toString());
 
@@ -125,9 +133,7 @@ describe("testing monitoring Executable flags on methods", function () {
                 );
             }
             await boiler.simulation.start([]);
-            if (doDebug) {
-                debugLog(" start => ", err);
-            }
+
 
             await new Promise((resolve) => setTimeout(resolve, 500));
 
@@ -151,10 +157,8 @@ describe("testing monitoring Executable flags on methods", function () {
                 "when system is Running, resume method shall NOT be executable"
             );
 
-            boiler.simulation.suspend([]);
-            if (doDebug) {
-                debugLog(" start => ", err);
-            }
+            await boiler.simulation.suspend([]);
+
 
             await proxyManager.stop();
         });
