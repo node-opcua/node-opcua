6b1c38760bc4db41a3869e6779f2b31ae632b391
Alexander Wong <alex@udia.ca>
2025-04-09 22:49:30 -0600

    fix: unencrypted password on empty buffer server certificate

--- packages/node-opcua-client/source/private/opcua_client_impl.ts
+++ packages/node-opcua-client/source/private/opcua_client_impl.ts
@@ -295,7 +295,7 @@ function createUserNameIdentityToken(
     let identityToken;
     let serverCertificate: Buffer | string | null = session.serverCertificate;
     // if server does not provide certificate use unencrypted password
-    if (!serverCertificate) {
+    if (!serverCertificate || serverCertificate.length === 0) {
         identityToken = new UserNameIdentityToken({
             encryptionAlgorithm: null,
             password: Buffer.from(password as string, "utf-8"),
