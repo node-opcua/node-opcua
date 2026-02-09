ab3bd98e69a7d365a40094d35122c4e879d04460
Etienne Rossignon <etienne.rossignon@sterfive.com>
2026-02-09 09:27:21 +0100

    chore: fix companion spec type definition

--- packages/node-opcua-nodeset-machinery-result/source/dt_result.ts
+++ packages/node-opcua-nodeset-machinery-result/source/dt_result.ts
@@ -1,5 +1,5 @@
 // ----- this file has been automatically generated - do not edit
-import { VariantOptions } from "node-opcua-variant"
+import { Variant } from "node-opcua-variant"
 import { ExtensionObject } from "node-opcua-extension-object"
 import { DTStructure } from "node-opcua-nodeset-ua/dist/dt_structure"
 import { DTResultMeta } from "./dt_result_meta"
@@ -18,6 +18,6 @@ export interface DTResult extends DTStructure {
   /** Contains meta data describing the resultContent.*/
   resultMetaData?: DTResultMeta; // ExtensionObject ns=11;i=3007
   /** Abstract data type to be subtyped from to hold result data created by the selected recipe.*/
-  resultContent: VariantOptions[]; // Variant ns=0;i=0
+  resultContent: Variant[]; // Variant ns=0;i=24
 }
 export interface UDTResult extends ExtensionObject, DTResult {};
