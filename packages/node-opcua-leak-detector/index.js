9bef6cdf28c32cba1b5e704f0f43820c6bce5134
Etienne Rossignon <etienne.rossignon@sterfive.com>
2026-02-27 19:39:47 +0100

    wip(leak-detector): add mem_leak_detector and improve resource leak detection

--- packages/node-opcua-leak-detector/index.js
+++ packages/node-opcua-leak-detector/index.js
@@ -1,4 +1,6 @@
 "use strict";
 module.exports = {
-    describeWithLeakDetector: require("./src/resource_leak_detector").describeWithLeakDetector
+    describeWithLeakDetector: require("./src/resource_leak_detector").describeWithLeakDetector,
+    takeMemorySnapshot: require("./src/mem_leak_detector").takeMemorySnapshot,
+    checkForMemoryLeak: require("./src/mem_leak_detector").checkForMemoryLeak
 };
