4064d34a69223e0dbe93c8d13f24775e2c5e7bcf
Etienne Rossignon <etienne.rossignon@sterfive.com>
2026-03-14 15:49:57 +0100

    refactor: replace private getIpAddresses() copies with shared import from node-opcua-hostname
    - install_push_certitifate_management.ts: use shared
      `getIpAddresses()`, remove unused `os` import
    - test_e2e_881.ts: use shared `getIpAddresses()`,
      remove local 20-line function and `os` import

--- packages/node-opcua-end2end-test/test/end_to_end/test_e2e_881.ts
+++ packages/node-opcua-end2end-test/test/end_to_end/test_e2e_881.ts
@@ -1,28 +1,8 @@
-import { networkInterfaces } from "os";
-import { get_mini_nodeset_filename, nodesets, OPCUAClient, OPCUAServer, UserTokenType } from "node-opcua";
+import { get_mini_nodeset_filename, OPCUAClient, OPCUAServer, UserTokenType } from "node-opcua";
+import { getIpAddresses } from "node-opcua-hostname";
 
 const doDebug = false;
 
-function getIpAddresses(): string[] {
-    const nets = networkInterfaces();
-    const results: any = {};
-    for (const name of Object.keys(nets)) {
-        for (const net of nets[name]!) {
-            // skip over non-ipv4 and internal (i.e. 127.0.0.1) addresses
-            if (net.family === "IPv4" && !net.internal) {
-                if (!results[name]) {
-                    results[name] = [];
-                }
-                results[name].push(net.address);
-            }
-        }
-    }
-    if (doDebug) {
-        console.log(results);
-    }
-
-    return [...(Object.values(results) as string[])].flat();
-}
 const port = 2007;
 const ip = getIpAddresses();
 
@@ -53,7 +33,7 @@ async function startServer(): Promise<OPCUAServer> {
     return server;
 }
 // tslint:disable-next-line:no-var-requires
-import { describeWithLeakDetector as describe} from "node-opcua-leak-detector";
+import { describeWithLeakDetector as describe } from "node-opcua-leak-detector";
 describe("building server with an AlternateName", () => {
     let server: OPCUAServer;
     before(async () => {
@@ -65,11 +45,11 @@ describe("building server with an AlternateName", () => {
     });
     it("should not confuse endpoints #881", async () => {
 
-        if (ip.length === 0 ) {
+        if (ip.length === 0) {
             console.log(" cannot run test because no IPV4 address available", ip);
             return;
         }
-        const client = OPCUAClient.create({ endpointMustExist: false, clientName: "1 " + __filename });
+        const client = OPCUAClient.create({ endpointMustExist: false, clientName: `1 ${__filename}` });
         client.on("backoff", () => {
             if (doDebug) {
                 console.log("keep trying", endpointUri);
@@ -89,18 +69,18 @@ describe("building server with an AlternateName", () => {
             console.log("creating session");
             const session = await client.createSession({
                 type: UserTokenType.UserName,
-                password: (()=>"test")(),
+                password: (() => "test")(),
                 userName: "test"
             });
             console.log("session created");
-            
+
             console.log("session closing");
             await session.close();
             console.log("session closed");
         } finally {
             console.log("disconnecting");
             await client.disconnect();
-            console.log ("disconnected");
+            console.log("disconnected");
         }
     });
 });
