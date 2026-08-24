86efd40fb01d2bb45df8f19a2bc12d8b8b52902b
Etienne Rossignon <etienne.rossignon@sterfive.com>
2026-08-24 09:18:00 +0200

    chore(node-opcua): drop stale pragmas, organize root re-export imports
    Package-local `pnpm test` hits the same pre-existing mocha ESM-resolution
    quirk seen in several other packages; deferring to the full suite.

--- packages/node-opcua/source/index.ts
+++ packages/node-opcua/source/index.ts
@@ -9,10 +9,10 @@
  * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
  * the Software, and to permit persons to whom the Software is furnished to do so,
  * subject to the following conditions:
- * 
+ *
  *   The above copyright notice and this permission notice shall be included in all
  *   copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
  * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
@@ -24,10 +24,6 @@
 /**
  * @module node-opcua
  */
-// tslint:disable:max-line-length
-// tslint:disable:no-var-requires
-// tslint:disable:no-console
-// tslint:disable:variable-name
 
 import chalk from "chalk";
 import semver from "semver";
