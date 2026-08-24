8ce7900c4628100f390e9adad39ee3d79d400df2
Etienne Rossignon <etienne.rossignon@sterfive.com>
2026-08-24 23:48:57 +0200

    chore(end2end-test): retype u_test_e2e_write_large_array_range.ts (0 biome findings)
    Switched test param to UmbrellaTestContext.

--- packages/node-opcua-end2end-test/test/end_to_end/u_test_e2e_write_large_array_range.ts
+++ packages/node-opcua-end2end-test/test/end_to_end/u_test_e2e_write_large_array_range.ts
@@ -1,8 +1,9 @@
 import { AttributeIds, type ClientSession, type ClientSubscription, OPCUAClient } from "node-opcua";
 
 import { perform_operation_on_subscription_async } from "../../test_helpers/perform_operation_on_client_session";
+import type { UmbrellaTestContext } from "./_helper_umbrella";
 
-export function t(test: any) {
+export function t(test: UmbrellaTestContext) {
     describe("write multi-dimensional-array", () => {
         let client: OPCUAClient | undefined;
         beforeEach(() => {
@@ -14,7 +15,7 @@ export function t(test: any) {
         it("MDA-1 DA", async () => {
             await perform_operation_on_subscription_async(
                 client!,
-                test.endpointUrl,
+                test.endpointUrl!,
                 async (session: ClientSession, _subscription: ClientSubscription) => {
                     const namespaceArray = await session.readNamespaceArray();
                     const simulationNamespaceIndex = namespaceArray.indexOf("urn://node-opcua-simulator");
